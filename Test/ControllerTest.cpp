016c3484d6b045c88f19847352cce170e814404d
Robert Tangerman <robert.tangerman@deltares.nl>
2020-11-30 11:34:20 +0100

    Renamed DiKErnel to Controller. DIKERNEL-1

--- Test/ControllerTest.cpp
+++ Test/ControllerTest.cpp
@@ -1,13 +1,8 @@
 #define CATCH_CONFIG_MAIN
 #include "catch2/catch.hpp"
-#include "DiKErnel.h"
+#include "Controller.h"
 
 using namespace Application;
-
-SCENARIO("Test DiKErnel")
-{
-    REQUIRE(DiKErnel::Run() == "Run the DiKErnel!");
-}
 
 SCENARIO("SOME TEST")
 {
