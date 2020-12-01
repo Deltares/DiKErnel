f9aff044512ab8498bcd9800ecaeb80823ce8119
Robert Tangerman <robert.tangerman@deltares.nl>
2020-12-01 14:47:16 +0100

    Added main files in test projects. DIKERNEL-1

---
+++ DiKErnel/Test/DiKErnel.Core.Test/CalculatorTest.cpp
@@ -0,0 +1,6 @@
+#include "catch2/catch.hpp"
+
+SCENARIO("CalculatorTest")
+{
+    REQUIRE(true == true);
+}
---
+++ DiKErnel/Test/DiKErnel.Core.Test/ControllerTest.cpp
@@ -0,0 +1,6 @@
+#include "catch2/catch.hpp"
+
+SCENARIO("ControllerTest")
+{
+    REQUIRE(true == true);
+}
