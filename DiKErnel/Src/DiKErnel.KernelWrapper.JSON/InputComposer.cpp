b41df0c7a977928e36caa7aa8e0958eaea404492
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2020-12-08 09:44:48 +0100

    Improved code to read Json file. DIKERNEL-11

--- DiKErnel/Src/DiKErnel.KernelWrapper.JSON/InputComposer.cpp
+++ DiKErnel/Src/DiKErnel.KernelWrapper.JSON/InputComposer.cpp
@@ -22,18 +22,22 @@ namespace DiKErnel::KernelWrapper::Json
 
     void InputComposer::ReadCalculationData(nlohmann::json json, InputData& inputData)
     {
-        inputData.calculationData.time = json["RekenData"]["Tijd"].get<std::vector<int>>();
+        inputData.calculationData.time = json["Rekendata"]["Tijd"].get<std::vector<int>>();
     }
 
     void InputComposer::ReadHydraulicLoads(nlohmann::json json, InputData& inputData)
     {
         inputData.hydraulicLoads.waveAngleMaximum = json["HydraulischeBelastingen"]["GolfhoekMaximum"].get<int>();
 
-        for (auto i = 0; i < json["HydraulischeBelastingen"]["RandvoorwaardenPerTijdstap"].size(); i++)
+        const auto arraySize = json["HydraulischeBelastingen"]["RandvoorwaardenPerTijdstap"].size();
+
+        for (auto i = 0; i < arraySize; i++)
         {
-            inputData.hydraulicLoads.boundaryConditionsPerTimeStep[i].waveHeightHm0 = json["HydraulischeBelastingen"]["RandvoorwaardenPerTijdstap"][i]["GolfhoogteHm0"].get<double>();
-            inputData.hydraulicLoads.boundaryConditionsPerTimeStep[i].wavePeriodTm10 = json["HydraulischeBelastingen"]["RandvoorwaardenPerTijdstap"][i]["GolfperiodeTm10"].get<double>();
-            inputData.hydraulicLoads.boundaryConditionsPerTimeStep[i].waveAngle = json["HydraulischeBelastingen"]["RandvoorwaardenPerTijdstap"][i]["Golfhoek"].get<double>();
+            BoundaryConditionsPerTimeStep boundaryConditionsPerTimeStep;
+            inputData.hydraulicLoads.boundaryConditionsPerTimeStep.push_back(boundaryConditionsPerTimeStep);
+            inputData.hydraulicLoads.boundaryConditionsPerTimeStep.at(i).waveHeightHm0 = json["HydraulischeBelastingen"]["RandvoorwaardenPerTijdstap"][i]["GolfhoogteHm0"].get<double>();
+            inputData.hydraulicLoads.boundaryConditionsPerTimeStep.at(i).wavePeriodTm10 = json["HydraulischeBelastingen"]["RandvoorwaardenPerTijdstap"][i]["GolfperiodeTm10"].get<double>();
+            inputData.hydraulicLoads.boundaryConditionsPerTimeStep.at(i).waveAngle = json["HydraulischeBelastingen"]["RandvoorwaardenPerTijdstap"][i]["Golfhoek"].get<double>();
         }
     }
 
@@ -41,23 +45,26 @@ namespace DiKErnel::KernelWrapper::Json
     {
         for (auto i = 0; i < json["Locaties"].size(); i++)
         {
-            inputData.locations[i].name = json["Locaties"][i]["Naam"].get<std::string>();
+            CalculationLocation location;
+            inputData.locations.push_back(location);
 
-            inputData.locations[i].revetment.typeTopLayer = json["Locaties"][i]["Bekleding"]["TypeToplaag"].get<std::string>();
-            inputData.locations[i].revetment.relativeDensity = json["Locaties"][i]["Bekleding"]["RelatieveDichtheid"].get<double>();
-            inputData.locations[i].revetment.thicknessTopLayer = json["Locaties"][i]["Bekleding"]["DikteToplaag"].get<double>();
-            inputData.locations[i].revetment.initialDamage = json["Locaties"][i]["Bekleding"]["BeginSchade"].get<double>();
-            inputData.locations[i].revetment.similarityParameterThreshold = json["Locaties"][i]["Bekleding"]["BrekerIndexGrenswaarde"].get<double>();
-            inputData.locations[i].revetment.coefficientPlungingAp = json["Locaties"][i]["Bekleding"]["CoefficientPlungingAp"].get<double>();
-            inputData.locations[i].revetment.coefficientPlungingBp = json["Locaties"][i]["Bekleding"]["CoefficientPlungingBp"].get<double>();
-            inputData.locations[i].revetment.coefficientPlungingCp = json["Locaties"][i]["Bekleding"]["CoefficientPlungingCp"].get<double>();
-            inputData.locations[i].revetment.coefficientPlungingNp = json["Locaties"][i]["Bekleding"]["CoefficientPlungingNp"].get<double>();
-            inputData.locations[i].revetment.coefficientSurgingAs = json["Locaties"][i]["Bekleding"]["CoefficientSurgingAs"].get<double>();
-            inputData.locations[i].revetment.coefficientSurgingBs = json["Locaties"][i]["Bekleding"]["CoefficientSurgingBs"].get<double>();
-            inputData.locations[i].revetment.coefficientSurgingCs = json["Locaties"][i]["Bekleding"]["CoefficientSurgingCs"].get<double>();
-            inputData.locations[i].revetment.coefficientSurgingNs = json["Locaties"][i]["Bekleding"]["CoefficientSurgingNs"].get<double>();
+            inputData.locations.at(i).name = json["Locaties"][i]["Naam"].get<std::string>();
 
-            inputData.locations[i].profileSchematization.tanA = json["Locaties"][i]["Profielschematisatie"]["TanA"].get<double>();
+            inputData.locations.at(i).revetment.typeTopLayer = json["Locaties"][i]["Bekleding"]["TypeToplaag"].get<std::string>();
+            inputData.locations.at(i).revetment.relativeDensity = json["Locaties"][i]["Bekleding"]["RelatieveDichtheid"].get<double>();
+            inputData.locations.at(i).revetment.thicknessTopLayer = json["Locaties"][i]["Bekleding"]["DikteToplaag"].get<double>();
+            inputData.locations.at(i).revetment.initialDamage = json["Locaties"][i]["Bekleding"]["BeginSchade"].get<double>();
+            inputData.locations.at(i).revetment.similarityParameterThreshold = json["Locaties"][i]["Bekleding"]["BrekerIndexGrenswaarde"].get<double>();
+            inputData.locations.at(i).revetment.coefficientPlungingAp = json["Locaties"][i]["Bekleding"]["CoefficientPlungingAp"].get<double>();
+            inputData.locations.at(i).revetment.coefficientPlungingBp = json["Locaties"][i]["Bekleding"]["CoefficientPlungingBp"].get<double>();
+            inputData.locations.at(i).revetment.coefficientPlungingCp = json["Locaties"][i]["Bekleding"]["CoefficientPlungingCp"].get<double>();
+            inputData.locations.at(i).revetment.coefficientPlungingNp = json["Locaties"][i]["Bekleding"]["CoefficientPlungingNp"].get<double>();
+            inputData.locations.at(i).revetment.coefficientSurgingAs = json["Locaties"][i]["Bekleding"]["CoefficientSurgingAs"].get<double>();
+            inputData.locations.at(i).revetment.coefficientSurgingBs = json["Locaties"][i]["Bekleding"]["CoefficientSurgingBs"].get<double>();
+            inputData.locations.at(i).revetment.coefficientSurgingCs = json["Locaties"][i]["Bekleding"]["CoefficientSurgingCs"].get<double>();
+            inputData.locations.at(i).revetment.coefficientSurgingNs = json["Locaties"][i]["Bekleding"]["CoefficientSurgingNs"].get<double>();
+
+            inputData.locations.at(i).profileSchematization.tanA = json["Locaties"][i]["Profielschematisatie"]["TanA"].get<double>();
         }
     }
 
--- DiKErnel/Test/DiKErnel.KernelWrapper.JSON.Test/InputComposerTest.cpp
+++ DiKErnel/Test/DiKErnel.KernelWrapper.JSON.Test/InputComposerTest.cpp
@@ -10,18 +10,18 @@ namespace DiKErnel::KernelWrapper::Json::Test
 {
     TEST_CASE("InputComposerTest")
     {
-        // SECTION("GetDomainParametersFromJson_Always_ReturnsTimes")
-        // {
-        //     // Setup
-        //     const auto filePath = TestUtil::TestDataHelper::GetTestDataPath("DiKErnel.KernelWrapper.Json.Test") / "calculation.json";
-        //
-        //     const InputComposer inputComposer;
-        //
-        //     // Call
-        //     const auto times = inputComposer.GetDomainParametersFromJson(filePath.string());
-        //
-        //     // Assert
-        //     REQUIRE_THAT(times, Catch::Equals<int>({0, 100, 500, 800, 1200, 2000 }));
-        // }
+        SECTION("GetDomainParametersFromJson_Always_ReturnsTimes")
+        {
+            // Setup
+            const auto filePath = TestUtil::TestDataHelper::GetTestDataPath("DiKErnel.KernelWrapper.Json.Test") / "calculation.json";
+        
+            const InputComposer inputComposer;
+        
+            // Call
+            inputComposer.GetDomainParametersFromJson(filePath.string());
+        
+            // Assert
+            REQUIRE(filePath == "filepath");
+        }
     }
 }
