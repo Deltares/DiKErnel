02c8a4d9eaea66f5f428be455c0633f32e7b4c5b
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2020-12-08 15:03:36 +0100

    Separated test cases. DIKERNEL-11

--- DiKErnel/Test/DiKErnel.KernelWrapper.JSON.Test/InputComposerTest.cpp
+++ DiKErnel/Test/DiKErnel.KernelWrapper.JSON.Test/InputComposerTest.cpp
@@ -8,110 +8,108 @@
 
 namespace DiKErnel::KernelWrapper::Json::Test
 {
-    TEST_CASE("InputComposerTest")
+    TEST_CASE("ReadCalculationData_Always_ReturnsCalculationData")
     {
-        SECTION("ReadCalculationData_Always_ReturnsCalculationData")
-        {
-            // Setup
-            const auto filePath = TestUtil::TestDataHelper::GetTestDataPath("DiKErnel.KernelWrapper.Json.Test") / "calculation.json";
+        // Setup
+        const auto filePath = TestUtil::TestDataHelper::GetTestDataPath("DiKErnel.KernelWrapper.Json.Test") / "calculation.json";
 
-            InputComposer inputComposer;
+        InputComposer inputComposer;
 
-            std::ifstream ifs(filePath);
-            nlohmann::json json = nlohmann::json::parse(ifs);
+        std::ifstream ifs(filePath);
+        nlohmann::json json = nlohmann::json::parse(ifs);
 
-            // Call
-            CalculationData calculationData = inputComposer.ReadCalculationData(json);
+        // Call
+        CalculationData calculationData = inputComposer.ReadCalculationData(json);
 
-            // Assert
-            REQUIRE_THAT(calculationData.time, Catch::Equals<int>({ 0, 100, 500, 800, 1200, 2000 }));
-        }
+        // Assert
+        REQUIRE_THAT(calculationData.time, Catch::Equals<int>({ 0, 100, 500, 800, 1200, 2000 }));
+    }
 
-        SECTION("ReadHydraulicLoads_Always_ReturnsHydraulicLoads")
-        {
-            // Setup
-            const auto filePath = TestUtil::TestDataHelper::GetTestDataPath("DiKErnel.KernelWrapper.Json.Test") / "calculation.json";
+    TEST_CASE("ReadHydraulicLoads_Always_ReturnsHydraulicLoads")
+    {
+        // Setup
+        const auto filePath = TestUtil::TestDataHelper::GetTestDataPath("DiKErnel.KernelWrapper.Json.Test") / "calculation.json";
 
-            InputComposer inputComposer;
+        InputComposer inputComposer;
 
-            std::ifstream ifs(filePath);
-            nlohmann::json json = nlohmann::json::parse(ifs);
+        std::ifstream ifs(filePath);
+        nlohmann::json json = nlohmann::json::parse(ifs);
 
-            // Call
-            HydraulicLoads hydraulicLoads = inputComposer.ReadHydraulicLoads(json);
+        // Call
+        HydraulicLoads hydraulicLoads = inputComposer.ReadHydraulicLoads(json);
 
-            // Assert
-            REQUIRE(hydraulicLoads.waveAngleMaximum == 78);
-            REQUIRE(hydraulicLoads.boundaryConditionsPerTimeStep[0].waveHeightHm0 == 0.5);
-            REQUIRE(hydraulicLoads.boundaryConditionsPerTimeStep[0].wavePeriodTm10 == 2.0);
-            REQUIRE(hydraulicLoads.boundaryConditionsPerTimeStep[0].waveAngle == -10.0);
+        // Assert
+        REQUIRE(hydraulicLoads.waveAngleMaximum == 78);
+        REQUIRE(hydraulicLoads.boundaryConditionsPerTimeStep[0].waveHeightHm0 == 0.5);
+        REQUIRE(hydraulicLoads.boundaryConditionsPerTimeStep[0].wavePeriodTm10 == 2.0);
+        REQUIRE(hydraulicLoads.boundaryConditionsPerTimeStep[0].waveAngle == -10.0);
 
-            REQUIRE(hydraulicLoads.boundaryConditionsPerTimeStep[1].waveHeightHm0 == 0.8);
-            REQUIRE(hydraulicLoads.boundaryConditionsPerTimeStep[1].wavePeriodTm10 == 6.0);
-            REQUIRE(hydraulicLoads.boundaryConditionsPerTimeStep[1].waveAngle == -5.0);
+        REQUIRE(hydraulicLoads.boundaryConditionsPerTimeStep[1].waveHeightHm0 == 0.8);
+        REQUIRE(hydraulicLoads.boundaryConditionsPerTimeStep[1].wavePeriodTm10 == 6.0);
+        REQUIRE(hydraulicLoads.boundaryConditionsPerTimeStep[1].waveAngle == -5.0);
 
-            REQUIRE(hydraulicLoads.boundaryConditionsPerTimeStep[2].waveHeightHm0 == 1.2);
-            REQUIRE(hydraulicLoads.boundaryConditionsPerTimeStep[2].wavePeriodTm10 == 6.0);
-            REQUIRE(hydraulicLoads.boundaryConditionsPerTimeStep[2].waveAngle == 0.0);
+        REQUIRE(hydraulicLoads.boundaryConditionsPerTimeStep[2].waveHeightHm0 == 1.2);
+        REQUIRE(hydraulicLoads.boundaryConditionsPerTimeStep[2].wavePeriodTm10 == 6.0);
+        REQUIRE(hydraulicLoads.boundaryConditionsPerTimeStep[2].waveAngle == 0.0);
 
-            REQUIRE(hydraulicLoads.boundaryConditionsPerTimeStep[3].waveHeightHm0 == 1.5);
-            REQUIRE(hydraulicLoads.boundaryConditionsPerTimeStep[3].wavePeriodTm10 == 7.0);
-            REQUIRE(hydraulicLoads.boundaryConditionsPerTimeStep[3].waveAngle == 7);
+        REQUIRE(hydraulicLoads.boundaryConditionsPerTimeStep[3].waveHeightHm0 == 1.5);
+        REQUIRE(hydraulicLoads.boundaryConditionsPerTimeStep[3].wavePeriodTm10 == 7.0);
+        REQUIRE(hydraulicLoads.boundaryConditionsPerTimeStep[3].waveAngle == 7);
 
-            REQUIRE(hydraulicLoads.boundaryConditionsPerTimeStep[4].waveHeightHm0 == 0.5);
-            REQUIRE(hydraulicLoads.boundaryConditionsPerTimeStep[4].wavePeriodTm10 == 4.0);
-            REQUIRE(hydraulicLoads.boundaryConditionsPerTimeStep[4].waveAngle == 8.0);
-        }
+        REQUIRE(hydraulicLoads.boundaryConditionsPerTimeStep[4].waveHeightHm0 == 0.5);
+        REQUIRE(hydraulicLoads.boundaryConditionsPerTimeStep[4].wavePeriodTm10 == 4.0);
+        REQUIRE(hydraulicLoads.boundaryConditionsPerTimeStep[4].waveAngle == 8.0);
+    }
 
-        SECTION("ReadLocations_Always_ReturnsCalculationLocations")
-        {
-            // Setup
-            const auto filePath = TestUtil::TestDataHelper::GetTestDataPath("DiKErnel.KernelWrapper.Json.Test") / "calculation.json";
+    TEST_CASE("ReadLocations_Always_ReturnsCalculationLocations")
+    {
+        // Setup
+        const auto filePath = TestUtil::TestDataHelper::GetTestDataPath("DiKErnel.KernelWrapper.Json.Test") / "calculation.json";
 
-            InputComposer inputComposer;
+        InputComposer inputComposer;
 
-            std::ifstream ifs(filePath);
-            nlohmann::json json = nlohmann::json::parse(ifs);
+        std::ifstream ifs(filePath);
+        nlohmann::json json = nlohmann::json::parse(ifs);
 
-            // Call
-            std::vector<CalculationLocation> calculationLocations = inputComposer.ReadLocations(json);
+        // Call
+        std::vector<CalculationLocation> calculationLocations = inputComposer.ReadLocations(json);
 
-            // Assert
-            REQUIRE(calculationLocations[0].name == "LocatieZwak");
+        // Assert
+        REQUIRE(calculationLocations[0].name == "LocatieZwak");
 
-            REQUIRE(calculationLocations[0].revetment.typeTopLayer == "Noorse Steen");
-            REQUIRE(calculationLocations[0].revetment.relativeDensity == 1.6);
-            REQUIRE(calculationLocations[0].revetment.thicknessTopLayer == 0.3);
-            REQUIRE(calculationLocations[0].revetment.initialDamage == 0.0);
-            REQUIRE(calculationLocations[0].revetment.similarityParameterThreshold == 2.9);
-            REQUIRE(calculationLocations[0].revetment.coefficientPlungingAp == 4);
-            REQUIRE(calculationLocations[0].revetment.coefficientPlungingBp == 0.8);
-            REQUIRE(calculationLocations[0].revetment.coefficientPlungingCp == 0.0);
-            REQUIRE(calculationLocations[0].revetment.coefficientPlungingNp == 0.0);
-            REQUIRE(calculationLocations[0].revetment.coefficientSurgingAs == 0.0);
-            REQUIRE(calculationLocations[0].revetment.coefficientSurgingBs == 0.0);
-            REQUIRE(calculationLocations[0].revetment.coefficientSurgingCs == -0.9);
-            REQUIRE(calculationLocations[0].revetment.coefficientSurgingNs == 0.6);
+        REQUIRE(calculationLocations[0].revetment.typeTopLayer == "Noorse Steen");
+        REQUIRE(calculationLocations[0].revetment.relativeDensity == 1.6);
+        REQUIRE(calculationLocations[0].revetment.thicknessTopLayer == 0.3);
+        REQUIRE(calculationLocations[0].revetment.initialDamage == 0.0);
+        REQUIRE(calculationLocations[0].revetment.similarityParameterThreshold == 2.9);
+        REQUIRE(calculationLocations[0].revetment.coefficientPlungingAp == 4);
+        REQUIRE(calculationLocations[0].revetment.coefficientPlungingBp == 0.8);
+        REQUIRE(calculationLocations[0].revetment.coefficientPlungingCp == 0.0);
+        REQUIRE(calculationLocations[0].revetment.coefficientPlungingNp == 0.0);
+        REQUIRE(calculationLocations[0].revetment.coefficientSurgingAs == 0.0);
+        REQUIRE(calculationLocations[0].revetment.coefficientSurgingBs == 0.0);
+        REQUIRE(calculationLocations[0].revetment.coefficientSurgingCs == -0.9);
+        REQUIRE(calculationLocations[0].revetment.coefficientSurgingNs == 0.6);
 
-            REQUIRE(calculationLocations[0].profileSchematization.tanA == 0.3);
+        REQUIRE(calculationLocations[0].profileSchematization.tanA == 0.3);
 
-            REQUIRE(calculationLocations[1].name == "LocatieSterk");
-                                         
-            REQUIRE(calculationLocations[1].revetment.typeTopLayer == "Noorse Steen");
-            REQUIRE(calculationLocations[1].revetment.relativeDensity == 1.6);
-            REQUIRE(calculationLocations[1].revetment.thicknessTopLayer == 0.7);
-            REQUIRE(calculationLocations[1].revetment.initialDamage == 0.0);
-            REQUIRE(calculationLocations[1].revetment.similarityParameterThreshold == 2.9);
-            REQUIRE(calculationLocations[1].revetment.coefficientPlungingAp == 4.0);
-            REQUIRE(calculationLocations[1].revetment.coefficientPlungingBp == 0.8);
-            REQUIRE(calculationLocations[1].revetment.coefficientPlungingCp == 0.0);
-            REQUIRE(calculationLocations[1].revetment.coefficientPlungingNp == 0.0);
-            REQUIRE(calculationLocations[1].revetment.coefficientSurgingAs == 0.0);
-            REQUIRE(calculationLocations[1].revetment.coefficientSurgingBs == 0.0);
-            REQUIRE(calculationLocations[1].revetment.coefficientSurgingCs == -0.9);
-            REQUIRE(calculationLocations[1].revetment.coefficientSurgingNs == 0.6);
-                                         
-            REQUIRE(calculationLocations[1].profileSchematization.tanA == 0.3);
-        }
+        REQUIRE(calculationLocations[1].name == "LocatieSterk");
+
+        REQUIRE(calculationLocations[1].revetment.typeTopLayer == "Noorse Steen");
+        REQUIRE(calculationLocations[1].revetment.relativeDensity == 1.6);
+        REQUIRE(calculationLocations[1].revetment.thicknessTopLayer == 0.7);
+        REQUIRE(calculationLocations[1].revetment.initialDamage == 0.0);
+        REQUIRE(calculationLocations[1].revetment.similarityParameterThreshold == 2.9);
+        REQUIRE(calculationLocations[1].revetment.coefficientPlungingAp == 4.0);
+        REQUIRE(calculationLocations[1].revetment.coefficientPlungingBp == 0.8);
+        REQUIRE(calculationLocations[1].revetment.coefficientPlungingCp == 0.0);
+        REQUIRE(calculationLocations[1].revetment.coefficientPlungingNp == 0.0);
+        REQUIRE(calculationLocations[1].revetment.coefficientSurgingAs == 0.0);
+        REQUIRE(calculationLocations[1].revetment.coefficientSurgingBs == 0.0);
+        REQUIRE(calculationLocations[1].revetment.coefficientSurgingCs == -0.9);
+        REQUIRE(calculationLocations[1].revetment.coefficientSurgingNs == 0.6);
+
+        REQUIRE(calculationLocations[1].profileSchematization.tanA == 0.3);
     }
 }
+
