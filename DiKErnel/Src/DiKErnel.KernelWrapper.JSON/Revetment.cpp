5fd1933c77ee2d7986ede4b4a7ccc6be1812988b
Elmar Kleijn <elmar.kleijn@deltares.nl>
2020-12-11 12:28:26 +0100

    Adapted Revetment. DIKERNEL-11

--- DiKErnel/Src/DiKErnel.KernelWrapper.JSON/Revetment.cpp
+++ DiKErnel/Src/DiKErnel.KernelWrapper.JSON/Revetment.cpp
@@ -18,3 +18,102 @@
 // Stichting Deltares and remain full property of Stichting Deltares at all times.
 // All rights reserved.
 
+#include "Revetment.h"
+
+namespace DiKErnel::KernelWrapper::Json
+{
+    Revetment::Revetment(
+        std::string typeTopLayer,
+        double relativeDensity,
+        double thicknessTopLayer,
+        double initialDamage,
+        double similarityParameterThreshold,
+        double coefficientPlungingAp,
+        double coefficientPlungingBp,
+        double coefficientPlungingCp,
+        double coefficientPlungingNp,
+        double coefficientSurgingAs,
+        double coefficientSurgingBs,
+        double coefficientSurgingCs,
+        double coefficientSurgingNs)
+    {
+        this->typeTopLayer = typeTopLayer;
+        this->relativeDensity = relativeDensity;
+        this->thicknessTopLayer = thicknessTopLayer;
+        this->initialDamage = initialDamage;
+        this->similarityParameterThreshold = similarityParameterThreshold;
+        this->coefficientPlungingAp = coefficientPlungingAp;
+        this->coefficientPlungingBp = coefficientPlungingBp;
+        this->coefficientPlungingCp = coefficientPlungingCp;
+        this->coefficientPlungingNp = coefficientPlungingNp;
+        this->coefficientSurgingAs = coefficientSurgingAs;
+        this->coefficientSurgingBs = coefficientSurgingBs;
+        this->coefficientSurgingCs = coefficientSurgingCs;
+        this->coefficientSurgingNs = coefficientSurgingNs;
+    }
+
+    std::string Revetment::GetTypeTopLayer() const
+    {
+        return typeTopLayer;
+    }
+
+    double Revetment::GetRelativeDensity() const
+    {
+        return relativeDensity;
+    }
+
+    double Revetment::GetThicknessTopLayer() const
+    {
+        return thicknessTopLayer;
+    }
+
+    double Revetment::GetInitialDamage() const
+    {
+        return initialDamage;
+    }
+
+    double Revetment::GetSimilarityParameterThreshold() const
+    {
+        return similarityParameterThreshold;
+    }
+
+    double Revetment::GetCoefficientPlungingAp() const
+    {
+        return coefficientPlungingAp;
+    }
+
+    double Revetment::GetCoefficientPlungingBp() const
+    {
+        return coefficientPlungingBp;
+    }
+
+    double Revetment::GetCoefficientPlungingCp() const
+    {
+        return coefficientPlungingCp;
+    }
+
+    double Revetment::GetCoefficientPlungingNp() const
+    {
+        return coefficientPlungingNp;
+    }
+
+    double Revetment::GetCoefficientSurgingAs() const
+    {
+        return coefficientSurgingAs;
+    }
+
+    double Revetment::GetCoefficientSurgingBs() const
+    {
+        return coefficientSurgingBs;
+    }
+
+    double Revetment::GetCoefficientSurgingCs() const
+    {
+        return coefficientSurgingCs;
+    }
+
+    double Revetment::GetCoefficientSurgingNs() const
+    {
+        return coefficientSurgingNs;
+    }
+}
