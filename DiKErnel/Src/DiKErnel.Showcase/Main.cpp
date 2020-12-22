9cb8915de0935469d100dcaa971785a10be1390c
Elmar Kleijn <elmar.kleijn@deltares.nl>
2020-12-22 14:10:24 +0100

    Improved ShowCase code. DIKERNEL-10

--- DiKErnel/Src/DiKErnel.Showcase/Main.cpp
+++ DiKErnel/Src/DiKErnel.Showcase/Main.cpp
@@ -34,7 +34,7 @@ using namespace DiKErnel::FunctionLibrary;
 using namespace DiKErnel::KernelWrapper::Json;
 using namespace std;
 
-int delay = 0;
+int subCalculationDelay = 0;
 
 enum class UserInput
 {
@@ -46,74 +46,80 @@ enum class UserInput
 #pragma region Forward declarations
 
 double CalculateDamageWithDelay(
-    double initialDamage,
-    double slopeAngle,
-    double relativeDensity,
-    double thicknessTopLayer,
-    double spectralWaveHeight,
-    double spectralWavePeriod,
-    double waveAngle,
-    double startTime,
-    double endTime,
-    double ap,
-    double bp,
-    double cp,
-    double np,
-    double as,
-    double bs,
-    double cs,
-    double ns,
-    double waveAngleMaximum,
-    double similarityParameterThreshold);
+    double,
+    double,
+    double,
+    double,
+    double,
+    double,
+    double,
+    double,
+    double,
+    double,
+    double,
+    double,
+    double,
+    double,
+    double,
+    double,
+    double,
+    double,
+    double);
 
 void InputMethod(
-    const atomic<bool>& calculationFinished,
-    atomic<UserInput>& userInput);
+    const atomic<bool>&,
+    atomic<UserInput>&);
 
 #pragma endregion
 
 int main()
 {
     string jsonFilePath;
-    std::atomic<bool> calculationFinished(false);
     std::atomic<UserInput> userInput;
+    std::atomic<bool> calculationFinished(false);
 
+    // Obtain user input
     cout << "|===================|" << endl;
     cout << "| Calculation input |" << endl;
     cout << "|===================|" << endl;
     cout << "-> Enter the path to the JSON file: ";
     getline(cin, jsonFilePath);
-    cout << "-> Enter the time to wait in seconds: ";
-    cin >> delay;
+    cout << "-> Enter any additional sub-calculation delay (in seconds): ";
+    cin >> subCalculationDelay;
     cout << endl;
 
+    // Read input JSON file
     const auto inputData = InputComposer::GetDomainParametersFromJson(jsonFilePath);
 
+    // Write user feedback
     cout << "|===========|" << endl;
     cout << "| Read data |" << endl;
     cout << "|===========|" << endl;
-    cout << "-> Number of time steps: " << inputData->GetCalculationData().GetTimes().size() - 1 << endl;
-    cout << "-> Number of locations: " << inputData->GetLocations().size() << endl << endl;
+    cout << "-> Number of read time steps: " << inputData->GetCalculationData().GetTimes().size() - 1 << endl;
+    cout << "-> Number of read locations: " << inputData->GetLocations().size() << endl << endl;
 
     // Start stopwatch
     const auto start = std::chrono::high_resolution_clock::now();
 
-    // Write start message
+    // Write start message for the calculation
     cout << "|=====================|" << endl;
     cout << "| Calculation started |" << endl;
     cout << "|=====================|" << endl;
     cout << "-> Enter 'p' to show the current progress" << endl;
     cout << "-> Enter 'c' to cancel the calculation" << endl << endl;
 
+    // Start calculation on separate thread
     Calculator calculator(
         *inputData,
         CalculateDamageWithDelay);
 
+    // Start obtaining user input on separate thread
     thread inputThread(
         InputMethod,
         ref(calculationFinished),
         ref(userInput));
 
+    // Handle user input until the calculation is finished
     while (!calculator.IsFinished())
     {
         if (userInput == UserInput::Cancel)
@@ -129,9 +135,10 @@ int main()
         }
     }
 
+    // Wait for actual completion of the calculation thread
     calculator.WaitForCompletion();
 
-    // Write end message
+    // Write end message for the calculation
     if (calculator.IsCancelled())
     {
         cout << endl;
@@ -163,11 +170,13 @@ int main()
     const std::chrono::duration<double> elapsed = end - start;
     cout << "=> Time elapsed: " << elapsed.count() << endl << endl;
 
-    // Notify thread being finished
+    // Notify calculation thread being finished
     calculationFinished = true;
 
+    // Write final message
     cout << endl << "Press 'Enter' to exit the application.";
 
+    // Wait for actual completion of the user input thread
     inputThread.join();
 
     return 0;
@@ -194,7 +203,7 @@ double CalculateDamageWithDelay(
     const double waveAngleMaximum,
     const double similarityParameterThreshold)
 {
-    std::this_thread::sleep_for(std::chrono::seconds(delay));
+    std::this_thread::sleep_for(std::chrono::seconds(subCalculationDelay));
 
     return NaturalStoneRevetment::CalculateDamage(
         initialDamage,
