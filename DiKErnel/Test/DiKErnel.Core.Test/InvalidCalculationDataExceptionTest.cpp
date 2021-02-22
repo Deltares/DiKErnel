2df89dce62f65ef703b032b50cf897d8f604a71a
Robert Tangerman <robert.tangerman@deltares.nl>
2021-02-22 15:29:02 +0100

    Introduced custom exception. DIKERNEL-31

---
+++ DiKErnel/Test/DiKErnel.Core.Test/InvalidCalculationDataExceptionTest.cpp
@@ -0,0 +1,38 @@
+// Copyright (C) Stichting Deltares 2020. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+#include <gtest/gtest.h>
+
+#include "InvalidCalculationDataException.h"
+
+namespace DiKErnel::Core::Test
+{
+    TEST(InvalidCalculationDataExceptionTest, Constructor_WithMessage_ExpectedValues)
+    {
+        // Setup
+        const std::string message = "Test message";
+
+        // Call
+        const InvalidCalculationDataException exception(message);
+
+        // Assert
+        ASSERT_TRUE(message == exception.what());
+    }
+}
