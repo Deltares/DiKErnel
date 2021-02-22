fe0038d9548448bd5cd5855e67dfdac14f576395
Elmar Kleijn <elmar.kleijn@deltares.nl>
2021-02-22 16:14:05 +0100

    Review related changes. DIKERNEL-62

--- Build/BuildMinGWMakeFiles.ps1
+++ Build/BuildMinGWMakeFiles.ps1
@@ -1,11 +1,11 @@
-# Move to DiKErnel folder
+# Navigate to DiKErnel folder
 cd ..\DiKErnel
 
 # CMake configure
 start-process C:\msys64\mingw64.exe -ArgumentList "cmake -G 'MinGW Makefiles' --config Release . -Wno-dv -B ..\Development\MinGW\" -NoNewWindow -Wait
 
-# Move to MinGW folder
+# Navigate to MinGW folder
 cd ..\Development\MinGW
 
-#CMake build
+# CMake build
 start-process C:\msys64\mingw64.exe -ArgumentList "cmake --build . --config Release" -NoNewWindow -Wait
