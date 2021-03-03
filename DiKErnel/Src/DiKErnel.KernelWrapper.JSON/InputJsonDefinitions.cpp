443721312b704d0ccb16eb5ea2fc818ff5759c60
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2021-03-03 13:21:07 +0100

    Added input- and output json definitions. DIKERNEL-52

---
+++ DiKErnel/Src/DiKErnel.KernelWrapper.JSON/InputJsonDefinitions.cpp
@@ -0,0 +1,65 @@
+// Copyright (C) Stichting Deltares 2020. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+#include "JsonDefinitions.h"
+
+namespace DiKErnel::KernelWrapper::Json
+{
+    using namespace std;
+
+    const string JsonDefinitions::CALCULATION_DATA = "Rekendata";
+    const string JsonDefinitions::HYDRAULIC_LOADS = "HydraulischeBelastingen";
+    const string JsonDefinitions::LOCATIONS = "Locaties";
+
+    const string JsonDefinitions::TIME = "Tijd";
+    const string JsonDefinitions::MAXIMUM_WAVE_ANGLE = "GolfhoekMaximum";
+    const string JsonDefinitions::BOUNDARY_CONDITIONS_PER_TIME_STEP = "RandvoorwaardenPerTijdstap";
+    const string JsonDefinitions::WATER_LEVEL = "Waterstand";
+    const string JsonDefinitions::WAVE_HEIGHT_HM0 = "GolfhoogteHm0";
+    const string JsonDefinitions::WAVE_PERIOD_TM10 = "GolfperiodeTm10";
+    const string JsonDefinitions::WAVE_ANGLE = "Golfhoek";
+
+    const string JsonDefinitions::NAME = "Naam";
+    const string JsonDefinitions::REVETMENT = "Bekleding";
+    const string JsonDefinitions::TYPE_TOP_LAYER = "TypeToplaag";
+    const string JsonDefinitions::RELATIVE_DENSITY = "RelatieveDichtheid";
+    const string JsonDefinitions::THICKNESS_TOP_LAYER = "DikteToplaag";
+    const string JsonDefinitions::INITIAL_DAMAGE = "BeginSchade";
+    const string JsonDefinitions::CRITICAL_DAMAGE = "Faalgetal";
+    const string JsonDefinitions::FAILED = "Faalgebeurtenis";
+    const string JsonDefinitions::TIME_OF_FAILURE = "Faaltijd";
+    const string JsonDefinitions::SIMILARITY_PARAMETER_THRESHOLD = "BrekerIndexGrenswaarde";
+    const string JsonDefinitions::COEFFICIENT_PLUNGING_AP = "CoefficientPlungingAp";
+    const string JsonDefinitions::COEFFICIENT_PLUNGING_BP = "CoefficientPlungingBp";
+    const string JsonDefinitions::COEFFICIENT_PLUNGING_CP = "CoefficientPlungingCp";
+    const string JsonDefinitions::COEFFICIENT_PLUNGING_NP = "CoefficientPlungingNp";
+    const string JsonDefinitions::COEFFICIENT_SURGING_AS = "CoefficientSurgingAs";
+    const string JsonDefinitions::COEFFICIENT_SURGING_BS = "CoefficientSurgingBs";
+    const string JsonDefinitions::COEFFICIENT_SURGING_CS = "CoefficientSurgingCs";
+    const string JsonDefinitions::COEFFICIENT_SURGING_NS = "CoefficientSurgingNs";
+    const string JsonDefinitions::PROFILE_SCHEMATIZATION = "Profielschematisatie";
+    const string JsonDefinitions::TAN_A = "TanA";
+    const string JsonDefinitions::POSITION_Z = "PositieZ";
+
+    const string JsonDefinitions::DAMAGE = "Schade";
+    const string JsonDefinitions::DAMAGE_OVER_TIME = "SchadegetalPerTijd";
+
+    const string JsonDefinitions::OUTPUT_DATA = "Uitvoerdata";
+}
---
+++ DiKErnel/Src/DiKErnel.KernelWrapper.JSON/InputJsonDefinitions.h
@@ -0,0 +1,72 @@
+// Copyright (C) Stichting Deltares 2020. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+#pragma once
+
+#include <string>
+
+namespace DiKErnel::KernelWrapper::Json
+{
+    /*!
+     * \brief Class containing Json related constants.
+     */
+    class JsonDefinitions
+    {
+        public:
+            static const std::string CALCULATION_DATA;
+            static const std::string HYDRAULIC_LOADS;
+            static const std::string LOCATIONS;
+
+            static const std::string TIME;
+            static const std::string MAXIMUM_WAVE_ANGLE;
+            static const std::string BOUNDARY_CONDITIONS_PER_TIME_STEP;
+            static const std::string WATER_LEVEL;
+            static const std::string WAVE_HEIGHT_HM0;
+            static const std::string WAVE_PERIOD_TM10;
+            static const std::string WAVE_ANGLE;
+
+            static const std::string NAME;
+            static const std::string REVETMENT;
+            static const std::string TYPE_TOP_LAYER;
+            static const std::string RELATIVE_DENSITY;
+            static const std::string THICKNESS_TOP_LAYER;
+            static const std::string INITIAL_DAMAGE;
+            static const std::string CRITICAL_DAMAGE;
+            static const std::string FAILED;
+            static const std::string TIME_OF_FAILURE;
+            static const std::string SIMILARITY_PARAMETER_THRESHOLD;
+            static const std::string COEFFICIENT_PLUNGING_AP;
+            static const std::string COEFFICIENT_PLUNGING_BP;
+            static const std::string COEFFICIENT_PLUNGING_CP;
+            static const std::string COEFFICIENT_PLUNGING_NP;
+            static const std::string COEFFICIENT_SURGING_AS;
+            static const std::string COEFFICIENT_SURGING_BS;
+            static const std::string COEFFICIENT_SURGING_CS;
+            static const std::string COEFFICIENT_SURGING_NS;
+            static const std::string PROFILE_SCHEMATIZATION;
+            static const std::string TAN_A;
+            static const std::string POSITION_Z;
+
+            static const std::string DAMAGE;
+            static const std::string DAMAGE_OVER_TIME;
+
+            static const std::string OUTPUT_DATA;
+    };
+}
