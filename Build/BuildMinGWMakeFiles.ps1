ce6fd8e9dfb7cbc3e9b515e6866e3dc497a8a200
Robert Tangerman <robert.tangerman@deltares.nl>
2021-03-08 16:00:11 +0100

    Added flag to powershell script. DIKERNEL-67

--- Build/BuildMinGWMakeFiles.ps1
+++ Build/BuildMinGWMakeFiles.ps1
@@ -2,7 +2,7 @@
 cd ..\DiKErnel
 
 # CMake configure
-start-process C:\msys64\mingw64.exe -ArgumentList "cmake -G 'MinGW Makefiles' --config Release . -Wno-dv -B ..\Development\MinGW\" -NoNewWindow -Wait
+start-process C:\msys64\mingw64.exe -ArgumentList "cmake -G 'MinGW Makefiles' -DBUILD_WITH_TESTS:BOOL=ON --config Release . -Wno-dv -B ..\Development\MinGW\" -NoNewWindow -Wait
 
 # Navigate to MinGW folder
 cd ..\Development\MinGW
