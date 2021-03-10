f279f61dff15bd643bd31ced3da4e935c1f1333b
Robert Tangerman <robert.tangerman@deltares.nl>
2021-03-10 15:58:32 +0100

    Extended builder. DIKERNEL-17

--- DiKErnel/Src/DiKErnel.Integration/Public/RevetmentCalculationInputBuilder.cpp
+++ DiKErnel/Src/DiKErnel.Integration/Public/RevetmentCalculationInputBuilder.cpp
@@ -20,6 +20,7 @@
 
 #include "RevetmentCalculationInputBuilder.h"
 
+#include "Defaults.h"
 #include "InvalidCalculationDataException.h"
 #include "NaturalStoneRevetmentDefaults.h"
 #include "NaturalStoneRevetmentLocationConstructionProperties.h"
@@ -57,32 +58,39 @@ namespace DiKErnel::Integration
     void RevetmentCalculationInputBuilder::AddNaturalStoneLocation(
         const NaturalStoneRevetmentLocationConstructionProperties& constructionProperties)
     {
-        const auto plungingCoefficientA = GetValue(constructionProperties.GetPlungingCoefficientA(),
-                                                   NaturalStoneRevetmentDefaults::PLUNGING_COEFFICIENT_A);
-        const auto plungingCoefficientB = GetValue(constructionProperties.GetPlungingCoefficientB(),
-                                                   NaturalStoneRevetmentDefaults::PLUNGING_COEFFICIENT_B);
-        const auto plungingCoefficientC = GetValue(constructionProperties.GetPlungingCoefficientC(),
-                                                   NaturalStoneRevetmentDefaults::PLUNGING_COEFFICIENT_C);
-        const auto plungingCoefficientN = GetValue(constructionProperties.GetPlungingCoefficientN(),
-                                                   NaturalStoneRevetmentDefaults::PLUNGING_COEFFICIENT_N);
-        const auto surgingCoefficientA = GetValue(constructionProperties.GetSurgingCoefficientA(),
-                                                  NaturalStoneRevetmentDefaults::SURGING_COEFFICIENT_A);
-        const auto surgingCoefficientB = GetValue(constructionProperties.GetSurgingCoefficientB(),
-                                                  NaturalStoneRevetmentDefaults::SURGING_COEFFICIENT_B);
-        const auto surgingCoefficientC = GetValue(constructionProperties.GetSurgingCoefficientC(),
-                                                  NaturalStoneRevetmentDefaults::SURGING_COEFFICIENT_C);
-        const auto surgingCoefficientN = GetValue(constructionProperties.GetSurgingCoefficientN(),
-                                                  NaturalStoneRevetmentDefaults::SURGING_COEFFICIENT_N);
-        const auto similarityParameterThreshold = GetValue(constructionProperties.GetSimilarityParameterThreshold(),
-                                                           NaturalStoneRevetmentDefaults::SIMILARITY_PARAMETER_THRESHOLD);
-
         auto hydraulicLoads = make_unique<NaturalStoneRevetmentHydraulicLoads>(
-            plungingCoefficientA, plungingCoefficientB, plungingCoefficientC, plungingCoefficientN, surgingCoefficientA, surgingCoefficientB,
-            surgingCoefficientC, surgingCoefficientN, similarityParameterThreshold);
+            GetValue(constructionProperties.GetPlungingCoefficientA(), NaturalStoneRevetmentDefaults::PLUNGING_COEFFICIENT_A),
+            GetValue(constructionProperties.GetPlungingCoefficientB(), NaturalStoneRevetmentDefaults::PLUNGING_COEFFICIENT_B),
+            GetValue(constructionProperties.GetPlungingCoefficientC(), NaturalStoneRevetmentDefaults::PLUNGING_COEFFICIENT_C),
+            GetValue(constructionProperties.GetPlungingCoefficientN(), NaturalStoneRevetmentDefaults::PLUNGING_COEFFICIENT_N),
+            GetValue(constructionProperties.GetSurgingCoefficientA(), NaturalStoneRevetmentDefaults::SURGING_COEFFICIENT_A),
+            GetValue(constructionProperties.GetSurgingCoefficientB(), NaturalStoneRevetmentDefaults::SURGING_COEFFICIENT_B),
+            GetValue(constructionProperties.GetSurgingCoefficientC(), NaturalStoneRevetmentDefaults::SURGING_COEFFICIENT_C),
+            GetValue(constructionProperties.GetSurgingCoefficientN(), NaturalStoneRevetmentDefaults::SURGING_COEFFICIENT_N),
+            GetValue(constructionProperties.GetSimilarityParameterThreshold(), NaturalStoneRevetmentDefaults::SIMILARITY_PARAMETER_THRESHOLD));
+
+        auto upperLimitLoading = make_unique<NaturalStoneRevetmentUpperLimitLoading>(
+            GetValue(constructionProperties.GetUpperLimitLoadingAul(), Defaults::UPPER_LIMIT_LOADING_OF_NORDIC_STONE_AUL),
+            GetValue(constructionProperties.GetUpperLimitLoadingBul(), Defaults::UPPER_LIMIT_LOADING_OF_NORDIC_STONE_BUL),
+            GetValue(constructionProperties.GetUpperLimitLoadingCul(), Defaults::UPPER_LIMIT_LOADING_OF_NORDIC_STONE_CUL));
+
+        auto lowerLimitLoading = make_unique<NaturalStoneRevetmentLowerLimitLoading>(
+            GetValue(constructionProperties.GetLowerLimitLoadingAll(), Defaults::LOWER_LIMIT_LOADING_OF_NORDIC_STONE_ALL),
+            GetValue(constructionProperties.GetLowerLimitLoadingBll(), Defaults::LOWER_LIMIT_LOADING_OF_NORDIC_STONE_BLL),
+            GetValue(constructionProperties.GetLowerLimitLoadingCll(), Defaults::LOWER_LIMIT_LOADING_OF_NORDIC_STONE_CLL));
+
+        auto distanceMaximumWaveElevation = make_unique<NaturalStoneRevetmentDistanceMaximumWaveElevation>(
+            GetValue(constructionProperties.GetDistanceMaximumWaveElevationAsmax(), Defaults::DISTANCE_MAXIMUM_WAVE_ELEVATION_NORDIC_STONE_ASMAX),
+            GetValue(constructionProperties.GetDistanceMaximumWaveElevationBsmax(), Defaults::DISTANCE_MAXIMUM_WAVE_ELEVATION_NORDIC_STONE_BSMAX));
+
+        auto normativeWidthOfWaveImpact = make_unique<NaturalStoneRevetmentNormativeWidthOfWaveImpact>(
+            GetValue(constructionProperties.GetNormativeWidthOfWaveImpactAwi(), Defaults::NORMATIVE_WIDTH_OF_WAVE_IMPACT_AWI),
+            GetValue(constructionProperties.GetNormativeWidthOfWaveImpactBwi(), Defaults::NORMATIVE_WIDTH_OF_WAVE_IMPACT_BWI));
 
         _locations.push_back(make_unique<NaturalStoneRevetmentLocationDependentData>(
             constructionProperties.GetInitialDamage(), constructionProperties.GetSlopeAngle(), constructionProperties.GetRelativeDensity(),
-            constructionProperties.GetThicknessTopLayer(), move(hydraulicLoads), nullptr, nullptr, nullptr, nullptr));
+            constructionProperties.GetThicknessTopLayer(), move(hydraulicLoads), move(upperLimitLoading), move(lowerLimitLoading),
+            move(distanceMaximumWaveElevation), move(normativeWidthOfWaveImpact)));
     }
 
     unique_ptr<ICalculationInput> RevetmentCalculationInputBuilder::Build()
