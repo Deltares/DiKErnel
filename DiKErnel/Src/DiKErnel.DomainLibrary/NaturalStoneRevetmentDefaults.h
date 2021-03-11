0ab91c480025569d6e8fdb8af9d8ce268c492c2f
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2021-03-11 08:59:20 +0100

    Added comments. DIKERNEL-68

--- DiKErnel/Src/DiKErnel.DomainLibrary/NaturalStoneRevetmentDefaults.h
+++ DiKErnel/Src/DiKErnel.DomainLibrary/NaturalStoneRevetmentDefaults.h
@@ -82,28 +82,120 @@ namespace DiKErnel::DomainLibrary
              */
             static const double SURGING_COEFFICIENT_N;
 
+            /*!
+             * \brief The default value of the hydraulic load on nordic stone XIb.
+             *        Unit = [-]
+             */
             static const double HYDRAULIC_LOAD_ON_NORDIC_STONE_XIB;
+
+            /*!
+             * \brief The default value of the hydraulic load on nordic stone Ap.
+             *        Unit = [-]
+             */
             static const double HYDRAULIC_LOAD_ON_NORDIC_STONE_AP;
+
+            /*!
+             * \brief The default value of the hydraulic load on nordic stone Bp.
+             *        Unit = [-]
+             */
             static const double HYDRAULIC_LOAD_ON_NORDIC_STONE_BP;
+
+            /*!
+             * \brief The default value of the hydraulic load on nordic stone Cp.
+             *        Unit = [-]
+             */
             static const double HYDRAULIC_LOAD_ON_NORDIC_STONE_CP;
+
+            /*!
+             * \brief The default value of the hydraulic load on nordic stone Np.
+             *        Unit = [-]
+             */
             static const double HYDRAULIC_LOAD_ON_NORDIC_STONE_NP;
+
+            /*!
+             * \brief The default value of the hydraulic load on nordic stone As.
+             *        Unit = [-]
+             */
             static const double HYDRAULIC_LOAD_ON_NORDIC_STONE_AS;
+
+            /*!
+             * \brief The default value of the hydraulic load on nordic stone Bs.
+             *        Unit = [-]
+             */
             static const double HYDRAULIC_LOAD_ON_NORDIC_STONE_BS;
+
+            /*!
+             * \brief The default value of the hydraulic load on nordic stone Cs.
+             *        Unit = [-]
+             */
             static const double HYDRAULIC_LOAD_ON_NORDIC_STONE_CS;
+
+            /*!
+             * \brief The default value of the hydraulic load on nordic stone Ns.
+             *        Unit = [-]
+             */
             static const double HYDRAULIC_LOAD_ON_NORDIC_STONE_NS;
 
+            /*!
+             * \brief The default value of the upper loading limit on nordic stone Aul.
+             *        Unit = [-]
+             */
             static const double UPPER_LIMIT_LOADING_OF_NORDIC_STONE_AUL;
+
+            /*!
+             * \brief The default value of the upper loading limit on nordic stone Bul.
+             *        Unit = [-]
+             */
             static const double UPPER_LIMIT_LOADING_OF_NORDIC_STONE_BUL;
+
+            /*!
+             * \brief The default value of the upper loading limit on nordic stone Cul.
+             *        Unit = [-]
+             */
             static const double UPPER_LIMIT_LOADING_OF_NORDIC_STONE_CUL;
 
+            /*!
+             * \brief The default value of the lower loading limit on nordic stone All.
+             *        Unit = [-]
+             */
             static const double LOWER_LIMIT_LOADING_OF_NORDIC_STONE_ALL;
+
+            /*!
+             * \brief The default value of the lower loading limit on nordic stone Bll.
+             *        Unit = [-]
+             */
             static const double LOWER_LIMIT_LOADING_OF_NORDIC_STONE_BLL;
+
+            /*!
+             * \brief The default value of the lower loading limit on nordic stone Cll.
+             *        Unit = [-]
+             */
             static const double LOWER_LIMIT_LOADING_OF_NORDIC_STONE_CLL;
 
+            /*!
+             * \brief The default value of the distance maximum wave elevation
+             * on nordic stone Asmax.
+             *        Unit = [-]
+             */
             static const double DISTANCE_MAXIMUM_WAVE_ELEVATION_NORDIC_STONE_ASMAX;
+
+            /*!
+             * \brief The default value of the distance maximum wave elevation
+             * on nordic stone Bsmax.
+             *        Unit = [-]
+             */
             static const double DISTANCE_MAXIMUM_WAVE_ELEVATION_NORDIC_STONE_BSMAX;
 
+            /*!
+             * \brief The default value of the normative width of wave impact Awi.
+             *        Unit = [-]
+             */
             static const double NORMATIVE_WIDTH_OF_WAVE_IMPACT_AWI;
+
+            /*!
+             * \brief The default value of the normative width of wave impact Bwi.
+             *        Unit = [-]
+             */
             static const double NORMATIVE_WIDTH_OF_WAVE_IMPACT_BWI;
     };
 }
