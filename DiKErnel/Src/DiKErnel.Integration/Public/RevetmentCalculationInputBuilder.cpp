2eeebdac080dc8818f40a767f3bc62a43d15441b
Ringo van der Velden <ringo.vandervelden@deltares.nl>
2021-03-24 10:47:45 +0100

    Renamings of defaults. DIKERNEL-68

--- DiKErnel/Src/DiKErnel.Integration/Public/RevetmentCalculationInputBuilder.cpp
+++ DiKErnel/Src/DiKErnel.Integration/Public/RevetmentCalculationInputBuilder.cpp
@@ -21,7 +21,7 @@
 #include "RevetmentCalculationInputBuilder.h"
 
 #include "InvalidCalculationDataException.h"
-#include "NordicStoneRevetmentDefaults.h"
+#include "NaturalStoneRevetmentDefaults.h"
 #include "NaturalStoneRevetmentLocationConstructionProperties.h"
 #include "NaturalStoneRevetmentLocationDependentInput.h"
 #include "RevetmentCalculationInputBuilderException.h"
