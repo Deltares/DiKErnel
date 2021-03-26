d46709293dc7a1ac1283b51b2be8931f01165d55
Robert Tangerman <robert.tangerman@deltares.nl>
2021-03-26 13:12:30 +0100

    Implemented grass wave impact parser. DIKERNEL-39

--- DiKErnel/Src/DiKErnel.KernelWrapper.Json.Input/Internal/Parsers/GrassWaveImpactJsonInputParser.cpp
+++ DiKErnel/Src/DiKErnel.KernelWrapper.Json.Input/Internal/Parsers/GrassWaveImpactJsonInputParser.cpp
@@ -21,6 +21,7 @@
 #include "GrassWaveImpactJsonInputParser.h"
 
 #include "GrassWaveImpactJsonInputDefinitions.h"
+#include "JsonInputDefinitions.h"
 #include "JsonInputParserHelper.h"
 
 namespace DiKErnel::KernelWrapper::Json::Input
@@ -45,6 +46,73 @@ namespace DiKErnel::KernelWrapper::Json::Input
         const basic_json<>::value_type& readRevetment,
         const basic_json<>::value_type& readCalculationMethod)
     {
-        return nullptr;
+        auto locationData = make_unique<JsonInputGrassWaveImpactRevetmentLocationData>(
+            readRevetment[JsonInputDefinitions::TYPE_TOP_LAYER].get<JsonInputGrassWaveImpactRevetmentLocationData::TopLayerType>());
+
+        locationData->SetFailureTimeAgwi(
+            forward<unique_ptr<double>>(JsonInputParserHelper::ParseOptionalValue(
+                readCalculationMethod, GrassWaveImpactJsonInputDefinitions::FAILURE_TIME_AGWI)));
+        locationData->SetFailureTimeBgwi(
+            forward<unique_ptr<double>>(JsonInputParserHelper::ParseOptionalValue(
+                readCalculationMethod, GrassWaveImpactJsonInputDefinitions::FAILURE_TIME_BGWI)));
+        locationData->SetFailureTimeCgwi(
+            forward<unique_ptr<double>>(JsonInputParserHelper::ParseOptionalValue(
+                readCalculationMethod, GrassWaveImpactJsonInputDefinitions::FAILURE_TIME_CGWI)));
+
+        if (readCalculationMethod.contains(GrassWaveImpactJsonInputDefinitions::MINIMUM_WAVE_HEIGHT))
+        {
+            const auto& readMinimumWaveHeight = readCalculationMethod[GrassWaveImpactJsonInputDefinitions::MINIMUM_WAVE_HEIGHT];
+
+            locationData->SetMinimumWaveHeightTemax(
+                forward<unique_ptr<double>>(JsonInputParserHelper::ParseOptionalValue(
+                    readMinimumWaveHeight, GrassWaveImpactJsonInputDefinitions::MINIMUM_WAVE_HEIGHT_TEMAX)));
+        }
+
+        if (readCalculationMethod.contains(GrassWaveImpactJsonInputDefinitions::MAXIMUM_WAVE_HEIGHT))
+        {
+            const auto& readMaximumWaveHeight = readCalculationMethod[GrassWaveImpactJsonInputDefinitions::MAXIMUM_WAVE_HEIGHT];
+
+            locationData->SetMaximumWaveHeightTemin(
+                forward<unique_ptr<double>>(JsonInputParserHelper::ParseOptionalValue(
+                    readMaximumWaveHeight, GrassWaveImpactJsonInputDefinitions::MINIMUM_WAVE_HEIGHT_TEMAX)));
+        }
+
+        if (readCalculationMethod.contains(GrassWaveImpactJsonInputDefinitions::WAVE_ANGLE_IMPACT))
+        {
+            const auto& readWaveAngleImpact = readCalculationMethod[GrassWaveImpactJsonInputDefinitions::WAVE_ANGLE_IMPACT];
+
+            locationData->SetWaveAngleImpactNwa(
+                forward<unique_ptr<double>>(JsonInputParserHelper::ParseOptionalValue(
+                    readWaveAngleImpact, GrassWaveImpactJsonInputDefinitions::WAVE_ANGLE_IMPACT_NWA)));
+
+            locationData->SetWaveAngleImpactQwa(
+                forward<unique_ptr<double>>(JsonInputParserHelper::ParseOptionalValue(
+                    readWaveAngleImpact, GrassWaveImpactJsonInputDefinitions::WAVE_ANGLE_IMPACT_QWA)));
+
+            locationData->SetWaveAngleImpactRwa(
+                forward<unique_ptr<double>>(JsonInputParserHelper::ParseOptionalValue(
+                    readWaveAngleImpact, GrassWaveImpactJsonInputDefinitions::WAVE_ANGLE_IMPACT_RWA)));
+        }
+
+        if (readCalculationMethod.contains(GrassWaveImpactJsonInputDefinitions::UPPER_LIMIT_LOADING))
+        {
+            const auto& readUpperLimitLoading = readCalculationMethod[GrassWaveImpactJsonInputDefinitions::UPPER_LIMIT_LOADING];
+
+            locationData->SetUpperLimitLoadingAul(
+                forward<unique_ptr<double>>(JsonInputParserHelper::ParseOptionalValue(
+                    readUpperLimitLoading, GrassWaveImpactJsonInputDefinitions::UPPER_LIMIT_LOADING_AUL)));
+        }
+
+        if (readCalculationMethod.contains(GrassWaveImpactJsonInputDefinitions::LOWER_LIMIT_LOADING))
+        {
+            const auto& readLowerLimitLoading = readCalculationMethod[GrassWaveImpactJsonInputDefinitions::LOWER_LIMIT_LOADING];
+
+            locationData->SetLowerLimitLoadingAll(
+                forward<unique_ptr<double>>(
+                    JsonInputParserHelper::ParseOptionalValue(
+                        readLowerLimitLoading, GrassWaveImpactJsonInputDefinitions::LOWER_LIMIT_LOADING_ALL)));
+        }
+
+        return locationData;
     }
 }
