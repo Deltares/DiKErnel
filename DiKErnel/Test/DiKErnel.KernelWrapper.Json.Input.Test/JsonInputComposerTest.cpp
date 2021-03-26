25b74f54ed1ac412cb3bfab9cb8ad8d4a586a738
Elmar Kleijn <elmar.kleijn@deltares.nl>
2021-03-26 12:57:58 +0100

    Re-implementing natural stone calculation. DIKERNEL-56

--- DiKErnel/Test/DiKErnel.KernelWrapper.Json.Input.Test/JsonInputComposerTest.cpp
+++ DiKErnel/Test/DiKErnel.KernelWrapper.Json.Input.Test/JsonInputComposerTest.cpp
@@ -74,7 +74,7 @@ namespace DiKErnel::KernelWrapper::Json::Input::Test
             &locationDependentInputItems[0].get());
         ASSERT_TRUE(locationDependentInputItem1 != nullptr);
         LocationDependentInputAssertHelper::AssertDamageProperties(0.1, 1.06, *locationDependentInputItem1);
-        NaturalStoneRevetmentLocationDependentInputAssertHelper::AssertMandatoryProperties("LocatieZwak", 0.25, 1.65, 0.3, *locationDependentInputItem1);
+        NaturalStoneRevetmentLocationDependentInputAssertHelper::AssertMandatoryProperties("LocatieZwak", 0.25, 0.9, 1.65, 0.3, *locationDependentInputItem1);
         NaturalStoneRevetmentLocationDependentInputAssertHelper::AssertHydraulicLoads(5, 1.3, 1.7, 0.9, -0.8, 10, -8, 6, 4.9, locationDependentInputItem1->GetHydraulicLoads());
         NaturalStoneRevetmentLocationDependentInputAssertHelper::AssertUpperLimitLoading(6.1, 3.6, 4.5, locationDependentInputItem1->GetUpperLimitLoading());
         NaturalStoneRevetmentLocationDependentInputAssertHelper::AssertLowerLimitLoading(4.1, 8, 0.5, locationDependentInputItem1->GetLowerLimitLoading());
@@ -86,7 +86,7 @@ namespace DiKErnel::KernelWrapper::Json::Input::Test
             &locationDependentInputItems[1].get());
         ASSERT_TRUE(locationDependentInputItem2 != nullptr);
         LocationDependentInputAssertHelper::AssertDamageProperties(0, 1, *locationDependentInputItem2);
-        NaturalStoneRevetmentLocationDependentInputAssertHelper::AssertMandatoryProperties("LocatieSterk", 0.3, 1.65, 0.7, *locationDependentInputItem2);
+        NaturalStoneRevetmentLocationDependentInputAssertHelper::AssertMandatoryProperties("LocatieSterk", 0.3, 1.0, 1.65, 0.7, *locationDependentInputItem2);
         NaturalStoneRevetmentLocationDependentInputAssertHelper::AssertHydraulicLoads(4, 0, 0, -0.9, 0.8, 0, 0, 0.6, 2.9, locationDependentInputItem2->GetHydraulicLoads());
         NaturalStoneRevetmentLocationDependentInputAssertHelper::AssertUpperLimitLoading(0.1, 0.6, 4, locationDependentInputItem2->GetUpperLimitLoading());
         NaturalStoneRevetmentLocationDependentInputAssertHelper::AssertLowerLimitLoading(0.1, 0.2, 4, locationDependentInputItem2->GetLowerLimitLoading());
