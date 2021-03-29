836fa800c3196998438af4cc9d904ccbd36f8b1c
Elmar Kleijn <elmar.kleijn@deltares.nl>
2021-03-29 16:28:07 +0200

    Re-implementing natural stone calculation. DIKERNEL-56

--- DiKErnel/Test/DiKErnel.FunctionLibrary.Test/HydraulicLoadTest.cpp
+++ DiKErnel/Test/DiKErnel.FunctionLibrary.Test/HydraulicLoadTest.cpp
@@ -18,4 +18,79 @@
 // Stichting Deltares and remain full property of Stichting Deltares at all times.
 // All rights reserved.
 
-namespace DiKErnel::FunctionLibrary::Test {}
+#include <gtest/gtest.h>
+
+#include "HydraulicLoad.h"
+
+namespace DiKErnel::FunctionLibrary::Test
+{
+    TEST(HydraulicLoadTest, SlopeAngle_ValidInput_ExpectedValue)
+    {
+        // Setup
+        const auto tanA = 1.1;
+
+        // Call
+        const auto slopeAngle = HydraulicLoad::SlopeAngle(tanA);
+
+        // Assert
+        ASSERT_DOUBLE_EQ(1.1, slopeAngle);
+    }
+
+    TEST(HydraulicLoadTest, LoadingOfRevetment_ValidInput_ExpectedTrue)
+    {
+        // Setup
+        const auto lowerLimitLoadingOfRevetment = 1.1;
+        const auto upperLimitLoadingOfRevetment = 1.1;
+        const auto positionZ = 1.1;
+
+        // Call
+        const auto loadingOfRevetment = HydraulicLoad::LoadingOfRevetment(lowerLimitLoadingOfRevetment, upperLimitLoadingOfRevetment, positionZ);
+
+        // Assert
+        ASSERT_TRUE(loadingOfRevetment);
+    }
+
+    TEST(HydraulicLoadTest, LoadingOfRevetment_ValidInput_ExpectedFalse)
+    {
+        // Setup
+        const auto lowerLimitLoadingOfRevetment = 1.1;
+        const auto upperLimitLoadingOfRevetment = 1.1;
+        const auto positionZ = 1.1;
+
+        // Call
+        const auto loadingOfRevetment = HydraulicLoad::LoadingOfRevetment(lowerLimitLoadingOfRevetment, upperLimitLoadingOfRevetment, positionZ);
+
+        // Assert
+        ASSERT_FALSE(loadingOfRevetment);
+    }
+
+    TEST(HydraulicLoadTest, WaveSteepnessDeepWater_ValidInput_ExpectedValue)
+    {
+        // Setup
+        const auto waveHeightHm0 = 1.1;
+        const auto wavePeriodTm10 = 1.1;
+        const auto gravitationalAcceleration = 1.1;
+
+        // Call
+        const auto waveSteepnessDeepWater = HydraulicLoad::WaveSteepnessDeepWater(waveHeightHm0, wavePeriodTm10, gravitationalAcceleration);
+
+        // Assert
+        ASSERT_DOUBLE_EQ(1.1, waveSteepnessDeepWater);
+    }
+
+    TEST(HydraulicLoadTest, SurfSimilarityParameter_ValidInput_ExpectedValue)
+    {
+        // Setup
+        const auto tanA = 1.1;
+        const auto waveHeightHm0 = 1.1;
+        const auto wavePeriodTm10 = 1.1;
+        const auto gravitationalAcceleration = 1.1;
+
+        // Call
+        const auto surfSimilarityParameter = HydraulicLoad::SurfSimilarityParameter(tanA, waveHeightHm0, wavePeriodTm10,
+                                                                                    gravitationalAcceleration);
+
+        // Assert
+        ASSERT_DOUBLE_EQ(1.1, surfSimilarityParameter);
+    }
+}
