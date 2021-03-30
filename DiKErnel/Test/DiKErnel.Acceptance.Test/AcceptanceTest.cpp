c8353c2ffa7c7e10ed0b272093e1fa22e04ff46d
Robert Tangerman <robert.tangerman@deltares.nl>
2021-03-30 15:12:50 +0200

    Added acceptance test for NaturalStone. DIKERNEL-56

--- DiKErnel/Test/DiKErnel.Acceptance.Test/AcceptanceTest.cpp
+++ DiKErnel/Test/DiKErnel.Acceptance.Test/AcceptanceTest.cpp
@@ -18,4 +18,54 @@
 // Stichting Deltares and remain full property of Stichting Deltares at all times.
 // All rights reserved.
 
-namespace DiKErnel::Acceptance::Test {}
+#include <gtest/gtest.h>
+
+#include "Calculator.h"
+#include "FileAssert.h"
+#include "JsonInputComposer.h"
+#include "JsonOutputComposer.h"
+#include "TestDataPathHelper.h"
+
+namespace DiKErnel::Acceptance::Test
+{
+    using namespace Core;
+    using namespace KernelWrapper::Json::Input;
+    using namespace std;
+    using namespace testing;
+    using namespace TestUtil;    
+
+    struct AcceptanceTest : Test
+    {
+        const string _actualOutputFilePath =
+            (filesystem::temp_directory_path()
+                / "actualOutput.json").string();
+
+        ~AcceptanceTest()
+        {
+            remove(_actualOutputFilePath.c_str());
+        }
+    };
+
+    TEST_F(AcceptanceTest, GivenJsonWithNaturalStoneLocation_WhenCalculating_ThenExpectedOutputJsonCreated)
+    {
+        // Given
+        const auto filePath =
+            (TestDataPathHelper::GetTestDataPath("DiKErnel.Acceptance.Test")
+                / "AcceptanceTest" / "naturalStone.json").string();
+
+        // When
+        const auto calculationInput = JsonInputComposer::GetCalculationInputFromJson(filePath);
+        Calculator calculator(*calculationInput);
+        calculator.WaitForCompletion();
+
+        const auto outputData = calculator.GetCalculationOutput();
+        KernelWrapper::Json::Output::JsonOutputComposer::WriteCalculationOutputToJson(_actualOutputFilePath, *outputData, *calculationInput);
+
+        // Assert
+        const auto expectedOutputFilePath =
+            (TestDataPathHelper::GetTestDataPath("DiKErnel.Acceptance.Test")
+                / "AcceptanceTest"
+                / "expectedOutputNaturalStone.json").string();
+        FileAssert::AssertFileContents(expectedOutputFilePath, _actualOutputFilePath);
+    }
+}
