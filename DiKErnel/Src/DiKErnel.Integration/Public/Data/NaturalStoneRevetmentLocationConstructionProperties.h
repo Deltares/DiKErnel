7a8eedb75cf77eb7b2774d7453ae60edde95e644
Elmar Kleijn <elmar.kleijn@deltares.nl>
2021-03-30 17:12:52 +0200

    Re-implementing natural stone calculation. DIKERNEL-56

--- DiKErnel/Src/DiKErnel.Integration/Public/Data/NaturalStoneRevetmentLocationConstructionProperties.h
+++ DiKErnel/Src/DiKErnel.Integration/Public/Data/NaturalStoneRevetmentLocationConstructionProperties.h
@@ -69,161 +69,161 @@ namespace DiKErnel::Integration
             #pragma region Set methods
 
             /*!
-             * \brief Sets the hydraulic load Ap.
+             * \brief Sets the Ap coefficient.
              * \param hydraulicLoadAp
-             *        The hydraulic load Ap.
+             *        The Ap coefficient.
              */
             void SetHydraulicLoadAp(
                 std::unique_ptr<double> hydraulicLoadAp);
 
             /*!
-             * \brief Sets the hydraulic load Bp.
+             * \brief Sets the Bp coefficient.
              * \param hydraulicLoadBp
-             *        The hydraulic load Bp.
+             *        The Bp coefficient.
              */
             void SetHydraulicLoadBp(
                 std::unique_ptr<double> hydraulicLoadBp);
 
             /*!
-             * \brief Sets the hydraulic load Cp.
+             * \brief Sets the Cp coefficient.
              * \param hydraulicLoadCp
-             *        The hydraulic load Cp.
+             *        The Cp coefficient.
              */
             void SetHydraulicLoadCp(
                 std::unique_ptr<double> hydraulicLoadCp);
 
             /*!
-             * \brief Sets the hydraulic load Np.
+             * \brief Sets the Np coefficient.
              * \param hydraulicLoadNp
-             *        The hydraulic load Np.
+             *        The Np coefficient.
              */
             void SetHydraulicLoadNp(
                 std::unique_ptr<double> hydraulicLoadNp);
 
             /*!
-             * \brief Sets the hydraulic load As.
+             * \brief Sets the As coefficient.
              * \param hydraulicLoadAs
-             *        The hydraulic load As.
+             *        The As coefficient.
              */
             void SetHydraulicLoadAs(
                 std::unique_ptr<double> hydraulicLoadAs);
 
             /*!
-             * \brief Sets the hydraulic load Bs.
+             * \brief Sets the Bs coefficient.
              * \param hydraulicLoadBs
-             *        The hydraulic load Bs.
+             *        The Bs coefficient.
              */
             void SetHydraulicLoadBs(
                 std::unique_ptr<double> hydraulicLoadBs);
 
             /*!
-             * \brief Sets the hydraulic load Cs.
+             * \brief Sets the Cs coefficient.
              * \param hydraulicLoadCs
-             *        The hydraulic load Cs.
+             *        The Cs coefficient.
              */
             void SetHydraulicLoadCs(
                 std::unique_ptr<double> hydraulicLoadCs);
 
             /*!
-             * \brief Sets the hydraulic load Ns.
+             * \brief Sets the Ns coefficient.
              * \param hydraulicLoadNs
-             *        The hydraulic load Ns.
+             *        The Ns coefficient.
              */
             void SetHydraulicLoadNs(
                 std::unique_ptr<double> hydraulicLoadNs);
 
             /*!
-             * \brief Sets the hydraulic load Xib.
+             * \brief Sets the Xib coefficient.
              * \param hydraulicLoadXib
-             *        The hydraulic load Xib.
+             *        The Xib coefficient.
              */
             void SetHydraulicLoadXib(
                 std::unique_ptr<double> hydraulicLoadXib);
 
             /*!
-             * \brief Sets the upper limit loading Aul.
+             * \brief Sets the Aul coefficient.
              * \param upperLimitLoadingAul
-             *        The upper limit loading Aul.
+             *        The Aul coefficient.
              */
             void SetUpperLimitLoadingAul(
                 std::unique_ptr<double> upperLimitLoadingAul);
 
             /*!
-             * \brief Sets the upper limit loading Bul.
+             * \brief Sets the Bul coefficient.
              * \param upperLimitLoadingBul
-             *        The upper limit loading Bul.
+             *        The Bul coefficient.
              */
             void SetUpperLimitLoadingBul(
                 std::unique_ptr<double> upperLimitLoadingBul);
 
             /*!
-             * \brief Sets the upper limit loading Cul.
+             * \brief Sets the Cul coefficient.
              * \param upperLimitLoadingCul
-             *        The upper limit loading Cul.
+             *        The Cul coefficient.
              */
             void SetUpperLimitLoadingCul(
                 std::unique_ptr<double> upperLimitLoadingCul);
 
             /*!
-             * \brief Sets the lower limit loading All.
+             * \brief Sets the All coefficient.
              * \param lowerLimitLoadingAll
-             *        The lower limit loading All.
+             *        The All coefficient.
              */
             void SetLowerLimitLoadingAll(
                 std::unique_ptr<double> lowerLimitLoadingAll);
 
             /*!
-             * \brief Sets the lower limit loading Bll.
+             * \brief Sets the Bll coefficient.
              * \param lowerLimitLoadingBll
-             *        The lower limit loading Bll.
+             *        The Bll coefficient.
              */
             void SetLowerLimitLoadingBll(
                 std::unique_ptr<double> lowerLimitLoadingBll);
 
             /*!
-             * \brief Sets the lower limit loading Cll.
+             * \brief Sets the Cll coefficient.
              * \param lowerLimitLoadingCll
-             *        The lower limit loading Cll.
+             *        The Cll coefficient.
              */
             void SetLowerLimitLoadingCll(
                 std::unique_ptr<double> lowerLimitLoadingCll);
 
             /*!
-             * \brief Sets the distance maximum wave elevation Asmax.
+             * \brief Sets the Asmax coefficient.
              * \param distanceMaximumWaveElevationAsmax
-             *        The distance maximum wave elevation Asmax.
+             *        The Asmax coefficient.
              */
             void SetDistanceMaximumWaveElevationAsmax(
                 std::unique_ptr<double> distanceMaximumWaveElevationAsmax);
 
             /*!
-             * \brief Sets the distance maximum wave elevation Bsmax.
+             * \brief Sets the Bsmax coefficient.
              * \param distanceMaximumWaveElevationBsmax
-             *        The distance maximum wave elevation Bsmax.
+             *        The Bsmax coefficient.
              */
             void SetDistanceMaximumWaveElevationBsmax(
                 std::unique_ptr<double> distanceMaximumWaveElevationBsmax);
 
             /*!
-             * \brief Sets the normative width of wave impact Awi.
+             * \brief Sets the Awi coefficient.
              * \param normativeWidthOfWaveImpactAwi
-             *        The normative width of wave impact Awi.
+             *        The Awi coefficient.
              */
             void SetNormativeWidthOfWaveImpactAwi(
                 std::unique_ptr<double> normativeWidthOfWaveImpactAwi);
 
             /*!
-             * \brief Sets the normative width of wave impact Bwi.
+             * \brief Sets the Bwi coefficient.
              * \param normativeWidthOfWaveImpactBwi
-             *        The normative width of wave impact Bwi.
+             *        The Bwi coefficient.
              */
             void SetNormativeWidthOfWaveImpactBwi(
                 std::unique_ptr<double> normativeWidthOfWaveImpactBwi);
 
             /*!
-             * \brief Sets the wave angle impact beta max
+             * \brief Sets the Betamax coefficient.
              * \param waveAngleImpactBetamax
-             *        The wave angle impact beta max.
+             *        The Betamax coefficient.
              */
             void SetWaveAngleImpactBetamax(
                 std::unique_ptr<double> waveAngleImpactBetamax);
@@ -251,122 +251,122 @@ namespace DiKErnel::Integration
             double GetRelativeDensity() const;
 
             /*!
-             * \brief Gets the hydraulic load Ap.
-             * \return The hydraulic load Ap.
+             * \brief Gets the Ap coefficient.
+             * \return The Ap coefficient.
              */
             const double* GetHydraulicLoadAp() const;
 
             /*!
-             * \brief Gets the hydraulic load Bp.
-             * \return The hydraulic load Bp.
+             * \brief Gets the Bp coefficient.
+             * \return The Bp coefficient.
              */
             const double* GetHydraulicLoadBp() const;
 
             /*!
-             * \brief Gets the hydraulic load Cp.
-             * \return The hydraulic load Cp.
+             * \brief Gets the Cp coefficient.
+             * \return The Cp coefficient.
              */
             const double* GetHydraulicLoadCp() const;
 
             /*!
-             * \brief Gets the hydraulic load Np.
-             * \return The hydraulic load Np.
+             * \brief Gets the Np coefficient.
+             * \return The Np coefficient.
              */
             const double* GetHydraulicLoadNp() const;
 
             /*!
-             * \brief Gets the hydraulic load As.
-             * \return The hydraulic load As.
+             * \brief Gets the As coefficient.
+             * \return The As coefficient.
              */
             const double* GetHydraulicLoadAs() const;
 
             /*!
-             * \brief Gets the hydraulic load Bs.
-             * \return The hydraulic load Bs.
+             * \brief Gets the Bs coefficient.
+             * \return The Bs coefficient.
              */
             const double* GetHydraulicLoadBs() const;
 
             /*!
-             * \brief Gets the hydraulic load Cs.
-             * \return The hydraulic load Cs.
+             * \brief Gets the Cs coefficient.
+             * \return The Cs coefficient.
              */
             const double* GetHydraulicLoadCs() const;
 
             /*!
-             * \brief Gets the hydraulic load Ns.
-             * \return The hydraulic load Ns.
+             * \brief Gets the Ns coefficient.
+             * \return The Ns coefficient.
              */
             const double* GetHydraulicLoadNs() const;
 
             /*!
-             * \brief Gets the hydraulic load Xib.
-             * \return The hydraulic load Xib.
+             * \brief Gets the Xib coefficient.
+             * \return The Xib coefficient.
              */
             const double* GetHydraulicLoadXib() const;
 
             /*!
-             * \brief Gets the upper limit loading Aul.
-             * \return The upper limit loading Aul.
+             * \brief Gets the Aul coefficient.
+             * \return The Aul coefficient.
              */
             const double* GetUpperLimitLoadingAul() const;
 
             /*!
-             * \brief Gets the upper limit loading Bul.
-             * \return The upper limit loading Bul.
+             * \brief Gets the Bul coefficient.
+             * \return The Bul coefficient.
              */
             const double* GetUpperLimitLoadingBul() const;
 
             /*!
-             * \brief Gets the upper limit loading Cul.
-             * \return The upper limit loading Cul.
+             * \brief Gets the Cul coefficient.
+             * \return The Cul coefficient.
              */
             const double* GetUpperLimitLoadingCul() const;
 
             /*!
-             * \brief Gets the lower limit loading All.
-             * \return The lower limit loading All.
+             * \brief Gets the All coefficient.
+             * \return The All coefficient.
              */
             const double* GetLowerLimitLoadingAll() const;
 
             /*!
-             * \brief Gets the lower limit loading Bll.
-             * \return The lower limit loading Bll.
+             * \brief Gets the Bll coefficient.
+             * \return The Bll coefficient.
              */
             const double* GetLowerLimitLoadingBll() const;
 
             /*!
-             * \brief Gets the lower limit loading Cll.
-             * \return The lower limit loading Cll.
+             * \brief Gets the Cll coefficient.
+             * \return The Cll coefficient.
              */
             const double* GetLowerLimitLoadingCll() const;
 
             /*!
-             * \brief Gets the distance maximum wave elevation Asmax.
-             * \return The distance maximum wave elevation Asmax.
+             * \brief Gets the Asmax coefficient.
+             * \return The Asmax coefficient.
              */
             const double* GetDistanceMaximumWaveElevationAsmax() const;
 
             /*!
-             * \brief Gets the distance maximum wave elevation Bsmax.
-             * \return The distance maximum wave elevation Bsmax.
+             * \brief Gets the Bsmax coefficient.
+             * \return The Bsmax coefficient.
              */
             const double* GetDistanceMaximumWaveElevationBsmax() const;
 
             /*!
-             * \brief Gets the normative width of wave impact Awi.
-             * \return The normative width of wave impact Awi.
+             * \brief Gets the Awi coefficient.
+             * \return The Awi coefficient.
              */
             const double* GetNormativeWidthOfWaveImpactAwi() const;
 
             /*!
-             * \brief Gets the normative width of wave impact Bwi.
-             * \return The normative width of wave impact Bwi.
+             * \brief Gets the Bwi coefficient.
+             * \return The Bwi coefficient.
              */
             const double* GetNormativeWidthOfWaveImpactBwi() const;
 
             /*!
-             * \brief Gets the wave angle impact beta max
-             * \return The wave angle impact beta max.
+             * \brief Gets the Betamax coefficient.
+             * \return The Betmax coefficient.
              */
             const double* GetWaveAngleImpactBetamax() const;
 
