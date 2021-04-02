c2cd9b7cc3a02f6d070259217df8fa2c0ada66e1
Elmar Kleijn <elmar.kleijn@deltares.nl>
2021-04-02 09:10:42 +0200

    Implementing grass wave impact calculation. DIKERNEL-38

--- DiKErnel/Src/DiKErnel.FunctionLibrary/Public/GrassRevetmentWaveImpact.h
+++ DiKErnel/Src/DiKErnel.FunctionLibrary/Public/GrassRevetmentWaveImpact.h
@@ -28,45 +28,169 @@ namespace DiKErnel::FunctionLibrary
     class GrassRevetmentWaveImpact
     {
         public:
+            /*!
+             * \brief Calculates the increment of damage.
+             * \param incrementTime
+             *        The increment of time.
+             *        Unit = [s]
+             * \param timeLine
+             *        The time line.
+             *        Unit = [s]
+             * \return The increment of damage.
+             *         Unit = [-]
+             */
             static double IncrementDamage(
                 double incrementTime,
                 double timeLine);
 
+            /*!
+             * \brief Calculates the time line.
+             * \param waveHeight
+             *        The wave height.
+             *        Unit = [m]
+             * \param timeLineAgwi
+             *        The Agwi coefficient.
+             *        Unit = [m]
+             * \param timeLineBgwi
+             *        The Bgwi coefficient.
+             *        Unit = [s^-1]
+             * \param timeLineCgwi
+             *        The Cgwi coefficient.
+             *        Unit = [m]
+             * \return The time line.
+             *         Unit = [s]
+             */
             static double TimeLine(
                 double waveHeight,
                 double timeLineAgwi,
                 double timeLineBgwi,
                 double timeLineCgwi);
 
+            /*!
+             * \brief Calculates the wave height.
+             * \param minimumWaveHeight
+             *        The minimum wave height.
+             *        Unit = [m]
+             * \param maximumWaveHeight
+             *        The maximum wave height.
+             *        Unit = [m]
+             * \param waveAngleImpact
+             *        The wave impact with respect to the wave angle.
+             *        Unit = [-]
+             * \param waveHeightHm0
+             *        The wave height.
+             *        Unit = [m]
+             * \return The wave height.
+             *         Unit = [m]
+             */
             static double WaveHeight(
                 double minimumWaveHeight,
                 double maximumWaveHeight,
                 double waveAngleImpact,
                 double waveHeightHm0);
 
+            /*!
+             * \brief Calculates the minimum wave height.
+             * \param timeLineAgwi
+             *        The Agwi coefficient.
+             *        Unit = [m]
+             * \param timeLineBgwi
+             *        The Bgwi coefficient.
+             *        Unit = [s^-1]
+             * \param timeLineCgwi
+             *        The Cgwi coefficient.
+             *        Unit = [m]
+             * \param minimumWaveHeightTemax
+             *        The Temax coefficient.
+             *        Unit = [s]
+             * \return The minimum wave height.
+             *         Unit = [m]
+             */
             static double MinimumWaveHeight(
                 double timeLineAgwi,
                 double timeLineBgwi,
                 double timeLineCgwi,
                 double minimumWaveHeightTemax);
 
+            /*!
+             * \brief Calculates the maximum wave height.
+             * \param timeLineAgwi
+             *        The Agwi coefficient.
+             *        Unit = [m]
+             * \param timeLineBgwi
+             *        The Bgwi coefficient.
+             *        Unit = [s^-1]
+             * \param timeLineCgwi
+             *        The Cgwi coefficient.
+             *        Unit = [m]
+             * \param maximumWaveHeightTemin
+             *        The Temin coefficient.
+             *        Unit = [s]
+             * \return The maximum wave height.
+             *         Unit = [m]
+             */
             static double MaximumWaveHeight(
                 double timeLineAgwi,
                 double timeLineBgwi,
                 double timeLineCgwi,
                 double maximumWaveHeightTemin);
 
+            /*!
+             * \brief Calculates the wave impact with respect to the wave angle.
+             * \param waveAngle
+             *        The wave angle.
+             *        Unit = [deg]
+             * \param waveAngleImpactNwa
+             *        The Nwa coefficient.
+             *        Unit = [-]
+             * \param waveAngleImpactQwa
+             *        The Qwa coefficient.
+             *        Unit = [-]
+             * \param waveAngleImpactRwa
+             *        The Rwa coefficient.
+             *        Unit = [-]
+             * \return The wave impact with respect to the wave angle.
+             *         Unit = [-]
+             */
             static double WaveAngleImpact(
                 double waveAngle,
                 double waveAngleImpactNwa,
                 double waveAngleImpactQwa,
                 double waveAngleImpactRwa);
 
+            /*!
+             * \brief Calculates the upper limit of loading.
+             * \param waterLevel
+             *        The water level.
+             *        Unit = [m]
+             * \param waveHeightHm0
+             *        The wave height.
+             *        Unit = [m]
+             * \param upperLimitLoadingAul
+             *        The Aul coefficient.
+             *        Unit = [-]
+             * \return The upper limit of loading.
+             *         Unit = [m]
+             */
             static double UpperLimitLoading(
                 double waterLevel,
                 double waveHeightHm0,
                 double upperLimitLoadingAul);
 
+            /*!
+             * \brief Calculates the lower limit of loading.
+             * \param waterLevel
+             *        The water level.
+             *        Unit = [m]
+             * \param waveHeightHm0
+             *        The wave height.
+             *        Unit = [m]
+             * \param lowerLimitLoadingAll
+             *        The All coefficient.
+             *        Unit = [-]
+             * \return The lower limit of loading.
+             *         Unit = [m]
+             */
             static double LowerLimitLoading(
                 double waterLevel,
                 double waveHeightHm0,
