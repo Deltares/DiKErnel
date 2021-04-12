f08d39dcbb2123b99d49d291ea93c9c3106529a0
Robert Tangerman <robert.tangerman@deltares.nl>
2021-04-12 09:36:34 +0200

    Added Grass Wave Runup specific Profile schematization data. DIKERNEL-70

---
+++ DiKErnel/Src/DiKErnel.KernelWrapper.Json.Input/Internal/Data/Revetment/JsonInputGrassRevetmentWaveRunupProfileSchematizationData.cpp
@@ -0,0 +1,53 @@
+// Copyright (C) Stichting Deltares 2020. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+#include "JsonInputGrassRevetmentWaveRunupProfileSchematizationData.h"
+
+namespace DiKErnel::KernelWrapper::Json::Input
+{
+    using namespace std;
+
+    JsonInputGrassRevetmentWaveRunupProfileSchematizationData::JsonInputGrassRevetmentWaveRunupProfileSchematizationData(
+        const double tanA,
+        const double positionZ)
+        : JsonInputProfileSchematizationData(tanA, positionZ) { }
+
+    void JsonInputGrassRevetmentWaveRunupProfileSchematizationData::SetRepresentativeWaveRunup2PGammab(
+        std::unique_ptr<double> representativeWaveRunup2PGammab)
+    {
+        _representativeWaveRunup2PGammab = move(representativeWaveRunup2PGammab);
+    }
+
+    void JsonInputGrassRevetmentWaveRunupProfileSchematizationData::SetRepresentativeWaveRunup2PGammaf(
+        std::unique_ptr<double> representativeWaveRunup2PGammaf)
+    {
+        _representativeWaveRunup2PGammaf = move(representativeWaveRunup2PGammaf);
+    }
+
+    const double* JsonInputGrassRevetmentWaveRunupProfileSchematizationData::SetRepresentativeWaveRunup2PGammab() const
+    {
+        return _representativeWaveRunup2PGammab.get();
+    }
+
+    const double* JsonInputGrassRevetmentWaveRunupProfileSchematizationData::SetRepresentativeWaveRunup2PGammaf() const
+    {
+        return _representativeWaveRunup2PGammaf.get();
+    }
+}
---
+++ DiKErnel/Src/DiKErnel.KernelWrapper.Json.Input/Internal/Data/Revetment/JsonInputGrassRevetmentWaveRunupProfileSchematizationData.h
@@ -0,0 +1,49 @@
+// Copyright (C) Stichting Deltares 2020. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+#pragma once
+#include <memory>
+
+#include "JsonInputProfileSchematizationData.h"
+
+namespace DiKErnel::KernelWrapper::Json::Input
+{
+    class JsonInputGrassRevetmentWaveRunupProfileSchematizationData : public JsonInputProfileSchematizationData
+    {
+        public:
+            explicit JsonInputGrassRevetmentWaveRunupProfileSchematizationData(
+                double tanA,
+                double positionZ);
+
+            void SetRepresentativeWaveRunup2PGammab(
+                std::unique_ptr<double> representativeWaveRunup2PGammab);
+
+            void SetRepresentativeWaveRunup2PGammaf(
+                std::unique_ptr<double> representativeWaveRunup2PGammaf);
+
+            const double* SetRepresentativeWaveRunup2PGammab() const;
+
+            const double* SetRepresentativeWaveRunup2PGammaf() const;
+
+        private:
+            std::unique_ptr<double> _representativeWaveRunup2PGammab;
+            std::unique_ptr<double> _representativeWaveRunup2PGammaf;
+    };
+}
