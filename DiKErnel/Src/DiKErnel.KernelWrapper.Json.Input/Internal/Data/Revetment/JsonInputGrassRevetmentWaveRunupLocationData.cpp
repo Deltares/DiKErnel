684f0ed343efe6e80925de2dd52ed46e32e86915
Robert Tangerman <robert.tangerman@deltares.nl>
2021-04-12 08:05:24 +0200

    Added Json input for Grass Wave Runup. DIKERNEL-70

---
+++ DiKErnel/Src/DiKErnel.KernelWrapper.Json.Input/Internal/Data/Revetment/JsonInputGrassRevetmentWaveRunupLocationData.cpp
@@ -0,0 +1,170 @@
+// Copyright (C) Stichting Deltares 2020. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+#include "JsonInputGrassRevetmentWaveRunupLocationData.h"
+
+namespace DiKErnel::KernelWrapper::Json::Input
+{
+    using namespace std;
+
+    JsonInputGrassRevetmentWaveRunupLocationData::JsonInputGrassRevetmentWaveRunupLocationData(
+        const JsonInputGrassRevetmentTopLayerType topLayerType)
+        : _topLayerType(topLayerType) { }
+
+    #pragma region Set methods
+
+    void JsonInputGrassRevetmentWaveRunupLocationData::SetCriticalCumulativeOverload(
+        unique_ptr<double> criticalCumulativeOverload)
+    {
+        _criticalCumulativeOverload = move(criticalCumulativeOverload);
+    }
+
+    void JsonInputGrassRevetmentWaveRunupLocationData::SetCriticalFrontVelocity(
+        unique_ptr<double> criticalFrontVelocity)
+    {
+        _criticalFrontVelocity = move(criticalFrontVelocity);
+    }
+
+    void JsonInputGrassRevetmentWaveRunupLocationData::SetIncreasedLoadTransitionAlphaM(
+        unique_ptr<double> increasedLoadTransitionAlphaM)
+    {
+        _increasedLoadTransitionAlphaM = move(increasedLoadTransitionAlphaM);
+    }
+
+    void JsonInputGrassRevetmentWaveRunupLocationData::SetReducedStrengthTransitionAlphaS(
+        unique_ptr<double> reducedStrengthTransitionAlphaS)
+    {
+        _reducedStrengthTransitionAlphaS = move(reducedStrengthTransitionAlphaS);
+    }
+
+    void JsonInputGrassRevetmentWaveRunupLocationData::SetAverageNumberWaveTimeStepCtm(
+        unique_ptr<double> averageNumberWaveTimeStepCtm)
+    {
+        _averageNumberWaveTimeStepCtm = move(averageNumberWaveTimeStepCtm);
+    }
+
+    void JsonInputGrassRevetmentWaveRunupLocationData::SetRepresentativeWaveRunUp2PAru(
+        unique_ptr<double> representativeWaveRunUp2PAru)
+    {
+        _representativeWaveRunUp2PAru = move(representativeWaveRunUp2PAru);
+    }
+
+    void JsonInputGrassRevetmentWaveRunupLocationData::SetRepresentativeWaveRunUp2PBru(
+        unique_ptr<double> representativeWaveRunUp2PBru)
+    {
+        _representativeWaveRunUp2PBru = move(representativeWaveRunUp2PBru);
+    }
+
+    void JsonInputGrassRevetmentWaveRunupLocationData::SetRepresentativeWaveRunUp2PCru(
+        unique_ptr<double> representativeWaveRunUp2PCru)
+    {
+        _representativeWaveRunUp2PCru = move(representativeWaveRunUp2PCru);
+    }
+
+    void JsonInputGrassRevetmentWaveRunupLocationData::SetWaveAngleImpactAbeta(
+        unique_ptr<double> waveAngleImpactAbeta)
+    {
+        _waveAngleImpactAbeta = move(waveAngleImpactAbeta);
+    }
+
+    void JsonInputGrassRevetmentWaveRunupLocationData::SetWaveAngleImpactBetamax(
+        unique_ptr<double> waveAngleImpactBetamax)
+    {
+        _waveAngleImpactBetamax = move(waveAngleImpactBetamax);
+    }
+
+    void JsonInputGrassRevetmentWaveRunupLocationData::SetCumulativeOverloadTimeStepRDNf(
+        std::unique_ptr<double> cumulativeOverloadTimeStepRDNf)
+    {
+        _cumulativeOverloadTimeStepRDNf = move(cumulativeOverloadTimeStepRDNf);
+    }
+
+    void JsonInputGrassRevetmentWaveRunupLocationData::SetFrontVelocityCu(
+        std::unique_ptr<double> frontVelocityCu)
+    {
+        _frontVelocityCu = move(frontVelocityCu);
+    }
+
+    #pragma endregion
+
+    #pragma region Get methods
+
+    JsonInputGrassRevetmentTopLayerType JsonInputGrassRevetmentWaveRunupLocationData::GetTopLayerType() const
+    {
+        return _topLayerType;
+    }
+
+    const double* JsonInputGrassRevetmentWaveRunupLocationData::GetCriticalCumulativeOverload() const
+    {
+        return _criticalCumulativeOverload.get();
+    }
+
+    const double* JsonInputGrassRevetmentWaveRunupLocationData::GetCriticalFrontVelocity() const
+    {
+        return _criticalFrontVelocity.get();
+    }
+
+    const double* JsonInputGrassRevetmentWaveRunupLocationData::GetIncreasedLoadTransitionAlphaM() const
+    {
+        return _increasedLoadTransitionAlphaM.get();
+    }
+
+    const double* JsonInputGrassRevetmentWaveRunupLocationData::GetReducedStrengthTransitionAlphaS() const
+    {
+        return _reducedStrengthTransitionAlphaS.get();
+    }
+
+    const double* JsonInputGrassRevetmentWaveRunupLocationData::GetAverageNumberWaveTimeStepCtm() const
+    {
+        return _averageNumberWaveTimeStepCtm.get();
+    }
+
+    const double* JsonInputGrassRevetmentWaveRunupLocationData::GetRepresentativeWaveRunUp2PAru() const
+    {
+        return _representativeWaveRunUp2PAru.get();
+    }
+
+    const double* JsonInputGrassRevetmentWaveRunupLocationData::GetRepresentativeWaveRunUp2PBru() const
+    {
+        return _representativeWaveRunUp2PBru.get();
+    }
+
+    const double* JsonInputGrassRevetmentWaveRunupLocationData::GetRepresentativeWaveRunUp2PCru() const
+    {
+        return _representativeWaveRunUp2PCru.get();
+    }
+
+    const double* JsonInputGrassRevetmentWaveRunupLocationData::GetWaveAngleImpactAbeta() const
+    {
+        return _waveAngleImpactAbeta.get();
+    }
+
+    const double* JsonInputGrassRevetmentWaveRunupLocationData::GetWaveAngleImpactBetamax() const
+    {
+        return _waveAngleImpactBetamax.get();
+    }
+
+    const double* JsonInputGrassRevetmentWaveRunupLocationData::GetFrontVelocityCu() const
+    {
+        return _frontVelocityCu.get();
+    }
+
+    #pragma endregion
+}
---
+++ DiKErnel/Src/DiKErnel.KernelWrapper.Json.Input/Internal/Data/Revetment/JsonInputGrassRevetmentWaveRunupLocationData.h
@@ -0,0 +1,119 @@
+// Copyright (C) Stichting Deltares 2020. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+#pragma once
+
+#include <memory>
+
+#include "IJsonInputRevetmentLocationData.h"
+#include "JsonInputGrassRevetmentTopLayerType.h"
+
+namespace DiKErnel::KernelWrapper::Json::Input
+{
+    class JsonInputGrassRevetmentWaveRunupLocationData : public IJsonInputRevetmentLocationData
+    {
+        public:
+            explicit JsonInputGrassRevetmentWaveRunupLocationData(
+                JsonInputGrassRevetmentTopLayerType topLayerType);
+
+            #pragma region Set methods
+
+            void SetCriticalCumulativeOverload(
+                std::unique_ptr<double> criticalCumulativeOverload);
+
+            void SetCriticalFrontVelocity(
+                std::unique_ptr<double> criticalFrontVelocity);
+
+            void SetIncreasedLoadTransitionAlphaM(
+                std::unique_ptr<double> increasedLoadTransitionAlphaM);
+
+            void SetReducedStrengthTransitionAlphaS(
+                std::unique_ptr<double> reducedStrengthTransitionAlphaS);
+
+            void SetAverageNumberWaveTimeStepCtm(
+                std::unique_ptr<double> averageNumberWaveTimeStepCtm);
+
+            void SetRepresentativeWaveRunUp2PAru(
+                std::unique_ptr<double> representativeWaveRunUp2PAru);
+
+            void SetRepresentativeWaveRunUp2PBru(
+                std::unique_ptr<double> representativeWaveRunUp2PBru);
+
+            void SetRepresentativeWaveRunUp2PCru(
+                std::unique_ptr<double> representativeWaveRunUp2PCru);
+
+            void SetWaveAngleImpactAbeta(
+                std::unique_ptr<double> waveAngleImpactAbeta);
+
+            void SetWaveAngleImpactBetamax(
+                std::unique_ptr<double> waveAngleImpactBetamax);
+
+            void SetCumulativeOverloadTimeStepRDNf(
+                std::unique_ptr<double> cumulativeOverloadTimeStepRDNf);
+
+            void SetFrontVelocityCu(
+                std::unique_ptr<double> frontVelocityCu);
+
+            #pragma endregion
+
+            #pragma region Get methods
+
+            JsonInputGrassRevetmentTopLayerType GetTopLayerType() const;
+
+            const double* GetCriticalCumulativeOverload() const;
+
+            const double* GetCriticalFrontVelocity() const;
+
+            const double* GetIncreasedLoadTransitionAlphaM() const;
+
+            const double* GetReducedStrengthTransitionAlphaS() const;
+
+            const double* GetAverageNumberWaveTimeStepCtm() const;
+
+            const double* GetRepresentativeWaveRunUp2PAru() const;
+
+            const double* GetRepresentativeWaveRunUp2PBru() const;
+
+            const double* GetRepresentativeWaveRunUp2PCru() const;
+
+            const double* GetWaveAngleImpactAbeta() const;
+
+            const double* GetWaveAngleImpactBetamax() const;
+
+            const double* GetFrontVelocityCu() const;
+
+            #pragma endregion
+
+        private:
+            JsonInputGrassRevetmentTopLayerType _topLayerType;
+            std::unique_ptr<double> _criticalCumulativeOverload = nullptr;
+            std::unique_ptr<double> _criticalFrontVelocity = nullptr;
+            std::unique_ptr<double> _increasedLoadTransitionAlphaM = nullptr;
+            std::unique_ptr<double> _reducedStrengthTransitionAlphaS = nullptr;
+            std::unique_ptr<double> _averageNumberWaveTimeStepCtm = nullptr;
+            std::unique_ptr<double> _representativeWaveRunUp2PAru = nullptr;
+            std::unique_ptr<double> _representativeWaveRunUp2PBru = nullptr;
+            std::unique_ptr<double> _representativeWaveRunUp2PCru = nullptr;
+            std::unique_ptr<double> _waveAngleImpactAbeta = nullptr;
+            std::unique_ptr<double> _waveAngleImpactBetamax = nullptr;
+            std::unique_ptr<double> _cumulativeOverloadTimeStepRDNf = nullptr;
+            std::unique_ptr<double> _frontVelocityCu = nullptr;
+    };
+}
