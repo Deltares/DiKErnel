8cfccb1a8bdefb85cf7db1df18a95e297290b583
Elmar Kleijn <elmar.kleijn@deltares.nl>
2021-04-12 10:55:27 +0200

    Implementing grass runup Rayleigh. DIKERNEL-69

---
+++ DiKErnel/Test/DiKErnel.FunctionLibrary.Test/GrassRevetmentWaveRunupTest.cpp
@@ -0,0 +1,39 @@
+// Copyright (C) Stichting Deltares 2020. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+#include <gtest/gtest.h>
+
+#include "GrassRevetmentWaveRunup.h"
+
+namespace DiKErnel::FunctionLibrary::Test
+{
+    TEST(GrassRevetmentWaveRunupTest, IncrementDamage_ValidInput_ExpectedValue)
+    {
+        // Setup
+        const auto cumulativeOverloadWaveRunup = 1063.89;
+        const auto criticalCumulativeOverloadWaveRunup = 7000;
+
+        // Call
+        const auto incrementDamage = GrassRevetmentWaveRunup::IncrementDamage(cumulativeOverloadWaveRunup, criticalCumulativeOverloadWaveRunup);
+
+        // Assert
+        ASSERT_DOUBLE_EQ(0.151984285714286, incrementDamage);
+    }
+}
