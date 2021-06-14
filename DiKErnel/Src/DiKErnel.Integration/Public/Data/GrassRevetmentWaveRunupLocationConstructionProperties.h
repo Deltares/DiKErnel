aa899ffb78c5f2acec97c89889334c16ad396e4d
Robert Tangerman <robert.tangerman@deltares.nl>
2021-06-14 15:21:14 +0200

    Added construction properties. DIKERNEL-71

---
+++ DiKErnel/Src/DiKErnel.Integration/Public/Data/GrassRevetmentWaveRunupLocationConstructionProperties.h
@@ -0,0 +1,64 @@
+// Copyright (C) Stichting Deltares 2020. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+#pragma once
+
+#include <string>
+
+#include "GrassRevetmentTopLayerType.h"
+#include "RevetmentLocationConstructionPropertiesBase.h"
+
+namespace DiKErnel::Integration
+{
+    /*!
+     * \brief Construction properties to construct grass revetment wave runup location dependent
+     *        input.
+     */
+    class GrassRevetmentWaveRunupLocationConstructionProperties : public RevetmentLocationConstructionPropertiesBase
+    {
+        public:
+            /*!
+             * \brief Creates a new instance.
+             * \param name
+             *        The name.
+             * \param positionZ
+             *        The vertical position.
+             * \param topLayerType
+             *        The type of the top layer.
+             */
+            explicit GrassRevetmentWaveRunupLocationConstructionProperties(
+                std::string name,
+                double positionZ,
+                GrassRevetmentTopLayerType topLayerType);
+
+            #pragma region Set methods
+
+
+            #pragma endregion
+
+            #pragma region Get methods
+
+
+            #pragma endregion
+
+        private:
+            GrassRevetmentTopLayerType _topLayerType;
+    };
+}
