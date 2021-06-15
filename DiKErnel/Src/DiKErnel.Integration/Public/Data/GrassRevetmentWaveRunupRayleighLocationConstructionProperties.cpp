3c10db5c90b3ace43152bbca46d077c678c6403b
Robert Tangerman <robert.tangerman@deltares.nl>
2021-06-15 11:15:00 +0200

    Added wave runup rayleigh construction properties. DIKERNEL-71

---
+++ DiKErnel/Src/DiKErnel.Integration/Public/Data/GrassRevetmentWaveRunupRayleighLocationConstructionProperties.cpp
@@ -0,0 +1,62 @@
+// Copyright (C) Stichting Deltares 2020. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+#include "GrassRevetmentWaveRunupRayleighLocationConstructionProperties.h"
+
+namespace DiKErnel::Integration
+{
+    using namespace std;
+
+    GrassRevetmentWaveRunupRayleighLocationConstructionProperties::GrassRevetmentWaveRunupRayleighLocationConstructionProperties(
+        string name,
+        const double positionZ,
+        const GrassRevetmentTopLayerType topLayerType)
+        : GrassRevetmentWaveRunupLocationConstructionProperties(move(name), positionZ, topLayerType) {}
+
+    #pragma region Set methods
+
+    void GrassRevetmentWaveRunupRayleighLocationConstructionProperties::SetCumulativeOverloadNf(
+        std::unique_ptr<int> cumulativeOverloadNf)
+    {
+        _cumulativeOverloadNf = move(cumulativeOverloadNf);
+    }
+
+    void GrassRevetmentWaveRunupRayleighLocationConstructionProperties::SetFrontVelocityCu(
+        std::unique_ptr<double> frontVelocityCu)
+    {
+        _frontVelocityCu = move(frontVelocityCu);
+    }
+
+    #pragma endregion
+
+    #pragma region Get methods
+
+    const int* GrassRevetmentWaveRunupRayleighLocationConstructionProperties::GetCumulativeOverloadNf() const
+    {
+        return _cumulativeOverloadNf.get();
+    }
+
+    const double* GrassRevetmentWaveRunupRayleighLocationConstructionProperties::GetFrontVelocityCu() const
+    {
+        return _frontVelocityCu.get();
+    }
+
+    #pragma endregion
+}
---
+++ DiKErnel/Src/DiKErnel.Integration/Public/Data/GrassRevetmentWaveRunupRayleighLocationConstructionProperties.h
@@ -0,0 +1,88 @@
+// Copyright (C) Stichting Deltares 2020. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+#pragma once
+
+#include "GrassRevetmentWaveRunupLocationConstructionProperties.h"
+
+namespace DiKErnel::Integration
+{
+    /*!
+     * \brief Construction properties to construct grass revetment wave runup location dependent
+     *        input for the Rayleigh protocol.
+     */
+    class GrassRevetmentWaveRunupRayleighLocationConstructionProperties : public GrassRevetmentWaveRunupLocationConstructionProperties
+    {
+        public:
+            /*!
+             * \brief Creates a new instance.
+             * \param name
+             *        The name.
+             * \param positionZ
+             *        The vertical position.
+             * \param topLayerType
+             *        The type of the top layer.
+             */
+            explicit GrassRevetmentWaveRunupRayleighLocationConstructionProperties(
+                std::string name,
+                double positionZ,
+                GrassRevetmentTopLayerType topLayerType);
+
+            #pragma region Set methods
+
+            /*!
+             * \brief Sets the Nf coefficient.
+             * \param cumulativeOverloadNf
+             *        The Nf coefficient.
+             */
+            void SetCumulativeOverloadNf(
+                std::unique_ptr<int> cumulativeOverloadNf);
+
+            /*!
+             * \brief Sets the Cu coefficient.
+             * \param frontVelocityCu
+             *        The Cu coefficient.
+             */
+            void SetFrontVelocityCu(
+                std::unique_ptr<double> frontVelocityCu);
+
+            #pragma endregion
+
+            #pragma region Get methods
+
+            /*!
+             * \brief Gets the Nf coefficient.
+             * \return The Nf coefficient.
+             */
+            const int* GetCumulativeOverloadNf() const;
+
+            /*!
+             * \brief Gets the Cu coefficient.
+             * \return The Cu coefficient.
+             */
+            const double* GetFrontVelocityCu() const;
+
+            #pragma endregion
+
+        private:
+            std::unique_ptr<int> _cumulativeOverloadNf = nullptr;
+            std::unique_ptr<double> _frontVelocityCu = nullptr;
+    };
+}
