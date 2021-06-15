58938562ebe445ca334f4b2b925d4aa6ac7dd2ef
Robert Tangerman <robert.tangerman@deltares.nl>
2021-06-15 11:20:46 +0200

    Also upload test code to SIG.

--- Build/CreateSigExport.ps1
+++ Build/CreateSigExport.ps1
@@ -1,2 +1,2 @@
 Remove-Item .\DiKErnel_src_*.zip
-git archive --output ('.\DiKErnel_src_' + (get-date -Format yyyyMMdd) + '.zip') --prefix=DiKErnel_src/ HEAD: DiKErnel/Src
+git archive --output ('.\DiKErnel_src_' + (get-date -Format yyyyMMdd) + '.zip') --prefix=DiKErnel_src/ HEAD: DiKErnel
