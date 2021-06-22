e1910b9d6727286da55cc0ddf13fdfeb7cb677da
Elmar Kleijn <elmar.kleijn@deltares.nl>
2021-06-22 10:05:42 +0200

    Extending FunctionLibrary for asphalt revetments. DIKERNEL-76

--- DiKErnel/Src/DiKErnel.FunctionLibrary/Public/AsphaltRevetment.h
+++ DiKErnel/Src/DiKErnel.FunctionLibrary/Public/AsphaltRevetment.h
@@ -30,6 +30,57 @@ namespace DiKErnel::FunctionLibrary
     class AsphaltRevetment
     {
         public:
+
+            /*!
+             * \brief Calculates the increment of damage.
+             * \param logFailureTension
+             *        The logarithm of the failure tension.
+             *        Unit = [MPa]
+             * \param averageNumberOfWaves
+             *        The average number of waves.
+             *        Unit = [-]
+             * \param maximumPeakStress
+             *        The maximum peak stress.
+             *        Unit = [MPa]
+             * \param stiffnessRelation
+             *        The stiffness relation.
+             *        Unit = [m^-1]
+             * \param computationalThickness
+             *        The computation thickness.
+             *        Unit = [m]
+             * \param tanA
+             *        The tanA.
+             *        Unit = [-]
+             * \param widthFactors
+             *        The width factors.
+             *        Unit = [-]
+             * \param depthFactors
+             *        The depth factors.
+             *        Unit = [-]
+             * \param impactFactors
+             *        The impact factors.
+             *        Unit = [-]
+             * \param positionZ
+             *        The vertical position.
+             *        Unit = [m]
+             * \param waterLevel
+             *        The water level.
+             *        Unit = [m]
+             * \param waveHeightHm0
+             *        The wave height.
+             *        Unit = [m]
+             * \param fatigueAlpha
+             *        The fatigue alpha coefficient.
+             *        Unit = [-]
+             * \param fatigueBeta
+             *        The fatigue beta coefficient.
+             *        Unit = [-]
+             * \param impactNumberC
+             *        The impact number c coefficient.
+             *        Unit = [-]
+             * \return The increment of damage.
+             *         Unit = [-]
+             */
             static double IncrementDamage(
                 double logFailureTension,
                 double averageNumberOfWaves,
@@ -48,16 +99,55 @@ namespace DiKErnel::FunctionLibrary
                 double impactNumberC
             );
 
+            /*!
+             * \brief Calculates the logarithm of the failure tension.
+             * \param failureTension
+             *        The failure tension.
+             *        Unit = [MPa]
+             * \return The logarithm of the failure tension.
+             *         Unit = [MPa]
+             */
             static double LogFailureTension(
                 double failureTension
             );
 
+            /*!
+             * \brief Calculates the maximum peak stress.
+             * \param waveHeightHm0
+             *        The wave height.
+             *        Unit = [m]
+             * \param gravitationalAcceleration
+             *        The gravitational acceleration.
+             *        Unit = [m/s^2]
+             * \param densityOfWater
+             *        The density of water.
+             *        Unit = [kg/m^3]
+             * \return The maximum peak stress.
+             *         Unit = [MPa]
+             */
             static double MaximumPeakStress(
                 double waveHeightHm0,
                 double gravitationalAcceleration,
                 double densityOfWater
             );
 
+            /*!
+             * \brief Calculates the stiffness relation.
+             * \param computationalThickness
+             *        The computation thickness.
+             *        Unit = [m]
+             * \param equivalentElasticModulus
+             *        The equivalent elastic modulus.
+             *        Unit = [MPa]
+             * \param soilElasticity
+             *        The soil elasticity.
+             *        Unit = [MPa/m]
+             * \param stiffnessRelationNu
+             *        The Nu coefficient.
+             *        Unit = [-]
+             * \return The stiffness relation.
+             *         Unit = [m^-1]
+             */
             static double StiffnessRelation(
                 double computationalThickness,
                 double equivalentElasticModulus,
@@ -65,6 +155,23 @@ namespace DiKErnel::FunctionLibrary
                 double stiffnessRelationNu
             );
 
+            /*!
+             * \brief Calculates the computational thickness.
+             * \param thicknessUpperLayer
+             *        The thickness of the upper layer.
+             *        Unit = [m]
+             * \param thicknessSubLayer
+             *        The thickness of the sub layer.
+             *        Unit = [m]
+             * \param elasticModulusUpperLayer
+             *        The elastic modulus of the upper layer.
+             *        Unit = [MPa]
+             * \param elasticModulusSubLayer
+             *        The elastic modulus of the sub layer.
+             *        Unit = [MPa]
+             * \return The computational thickness.
+             *         Unit = [m]
+             */
             static double ComputationalThickness(
                 double thicknessUpperLayer,
                 double thicknessSubLayer,
