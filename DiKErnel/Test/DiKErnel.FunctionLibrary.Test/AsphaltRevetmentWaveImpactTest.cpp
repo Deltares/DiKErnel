9da472333231fc2a2ab75d2f658b679a440eecce
Elmar Kleijn <elmar.kleijn@deltares.nl>
2021-06-23 13:55:06 +0200

    Extending FunctionLibrary for asphalt revetments. DIKERNEL-76

--- DiKErnel/Test/DiKErnel.FunctionLibrary.Test/AsphaltRevetmentWaveImpactTest.cpp
+++ DiKErnel/Test/DiKErnel.FunctionLibrary.Test/AsphaltRevetmentWaveImpactTest.cpp
@@ -36,48 +36,54 @@ namespace DiKErnel::FunctionLibrary::Test
         const auto computationalThickness = 0.16;
         const auto tanA = 0.232914161;
 
-        vector<tuple<double, double>> widthFactors;
-        widthFactors.emplace_back(0.1, 0.0392);
-        widthFactors.emplace_back(0.2, 0.0738);
-        widthFactors.emplace_back(0.3, 0.1002);
-        widthFactors.emplace_back(0.4, 0.1162);
-        widthFactors.emplace_back(0.5, 0.1213);
-        widthFactors.emplace_back(0.6, 0.1168);
-        widthFactors.emplace_back(0.7, 0.1051);
-        widthFactors.emplace_back(0.8, 0.089);
-        widthFactors.emplace_back(0.9, 0.0712);
-        widthFactors.emplace_back(1.0, 0.0541);
-        widthFactors.emplace_back(1.1, 0.0391);
-        widthFactors.emplace_back(1.2, 0.0269);
-        widthFactors.emplace_back(1.3, 0.0216);
-        widthFactors.emplace_back(1.4, 0.015);
-        widthFactors.emplace_back(1.5, 0.0105);
+        const vector<tuple<double, double>> widthFactors
+        {
+            tuple<double, double>(0.1, 0.0392),
+            tuple<double, double>(0.2, 0.0738),
+            tuple<double, double>(0.3, 0.1002),
+            tuple<double, double>(0.4, 0.1162),
+            tuple<double, double>(0.5, 0.1213),
+            tuple<double, double>(0.6, 0.1168),
+            tuple<double, double>(0.7, 0.1051),
+            tuple<double, double>(0.8, 0.089),
+            tuple<double, double>(0.9, 0.0712),
+            tuple<double, double>(1.0, 0.0541),
+            tuple<double, double>(1.1, 0.0391),
+            tuple<double, double>(1.2, 0.0269),
+            tuple<double, double>(1.3, 0.0216),
+            tuple<double, double>(1.4, 0.015),
+            tuple<double, double>(1.5, 0.0105),
+        };
 
-        vector<tuple<double, double>> depthFactors;
-        depthFactors.emplace_back(-1.0, 0.0244);
-        depthFactors.emplace_back(-0.875, 0.0544);
-        depthFactors.emplace_back(-0.75, 0.0938);
-        depthFactors.emplace_back(-0.625, 0.1407);
-        depthFactors.emplace_back(-0.5, 0.1801);
-        depthFactors.emplace_back(-0.375, 0.1632);
-        depthFactors.emplace_back(-0.25, 0.1426);
-        depthFactors.emplace_back(-0.125, 0.0994);
-        depthFactors.emplace_back(0.0, 0.06);
-        depthFactors.emplace_back(0.125, 0.0244);
-        depthFactors.emplace_back(0.25, 0.0169);
+        const vector<tuple<double, double>> depthFactors
+        {
+            tuple<double, double>(-1.0, 0.0244),
+            tuple<double, double>(-0.875, 0.0544),
+            tuple<double, double>(-0.75, 0.0938),
+            tuple<double, double>(-0.625, 0.1407),
+            tuple<double, double>(-0.5, 0.1801),
+            tuple<double, double>(-0.375, 0.1632),
+            tuple<double, double>(-0.25, 0.1426),
+            tuple<double, double>(-0.125, 0.0994),
+            tuple<double, double>(0.0, 0.06),
+            tuple<double, double>(0.125, 0.0244),
+            tuple<double, double>(0.25, 0.0169)
+        };
 
-        vector<tuple<double, double>> impactFactors;
-        impactFactors.emplace_back(2.0, 0.039);
-        impactFactors.emplace_back(2.4, 0.1);
-        impactFactors.emplace_back(2.8, 0.18);
-        impactFactors.emplace_back(3.2, 0.235);
-        impactFactors.emplace_back(3.6, 0.2);
-        impactFactors.emplace_back(4.0, 0.13);
-        impactFactors.emplace_back(4.4, 0.08);
-        impactFactors.emplace_back(4.8, 0.02);
-        impactFactors.emplace_back(5.2, 0.01);
-        impactFactors.emplace_back(5.6, 0.005);
-        impactFactors.emplace_back(6.0, 0.001);
+        const vector<tuple<double, double>> impactFactors
+        {
+            tuple<double, double>(2.0, 0.039),
+            tuple<double, double>(2.4, 0.1),
+            tuple<double, double>(2.8, 0.18),
+            tuple<double, double>(3.2, 0.235),
+            tuple<double, double>(3.6, 0.2),
+            tuple<double, double>(4.0, 0.13),
+            tuple<double, double>(4.4, 0.08),
+            tuple<double, double>(4.8, 0.02),
+            tuple<double, double>(5.2, 0.01),
+            tuple<double, double>(5.6, 0.005),
+            tuple<double, double>(6.0, 0.001)
+        };
 
         const auto positionZ = 2.988;
         const auto waterLevel = 3.8;
@@ -105,6 +111,19 @@ namespace DiKErnel::FunctionLibrary::Test
             impactNumberC);
 
         // Assert
-        ASSERT_DOUBLE_EQ(9.257776511818653, incrementDamage);
+        ASSERT_DOUBLE_EQ(7.94813500019044, incrementDamage);
+    }
+
+    TEST(AsphaltRevetmentWaveImpactTest, LogFailureTension_ValidInput_ExpectedValue)
+    {
+        // Setup
+        const auto failureTension = 1.56;
+
+        // Call
+        const auto logFailureTension = AsphaltRevetmentWaveImpact::LogFailureTension(
+            failureTension);
+
+        // Assert
+        ASSERT_DOUBLE_EQ(0.19312459835446161, logFailureTension);
     }
 }
