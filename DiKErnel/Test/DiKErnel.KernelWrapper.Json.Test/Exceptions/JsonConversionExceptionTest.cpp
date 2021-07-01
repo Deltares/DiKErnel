1fd95176e15210bed2e7c90c9664d44363924565
Robert Tangerman <robert.tangerman@deltares.nl>
2021-07-01 09:38:59 +0200

    Added test project. DIKERNEL-84

---
+++ DiKErnel/Test/DiKErnel.KernelWrapper.Json.Test/Exceptions/JsonConversionExceptionTest.cpp
@@ -0,0 +1,44 @@
+// Copyright (C) Stichting Deltares 2020. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+#include <gtest/gtest.h>
+
+#include "AssertHelper.h"
+#include "JsonConversionException.h"
+
+namespace DiKErnel::KernelWrapper::Json::Test
+{
+    using namespace Input;
+    using namespace std;
+    using namespace TestUtil;
+
+    TEST(JsonConversionExceptionTest, Constructor_WithMessage_ExpectedValues)
+    {
+        // Setup
+        const string message = "Test message";
+
+        // Call
+        const JsonConversionException actualException(message);
+
+        // Assert
+        AssertHelper::AssertIsInstanceOf<exception>(&actualException);
+        ASSERT_EQ(message, actualException.what());
+    }
+}
