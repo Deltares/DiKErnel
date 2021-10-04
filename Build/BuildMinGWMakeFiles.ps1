1de42e13f5439d5ba953a649123c57f86caee598
Robert Tangerman <robert.tangerman@deltares.nl>
2021-10-04 09:33:51 +0200

    Adjusted build script. DIKERNEL-20

--- Build/BuildMinGWMakeFiles.ps1
+++ Build/BuildMinGWMakeFiles.ps1
@@ -2,10 +2,10 @@
 cd ..\DiKErnel
 
 # CMake configure
-start-process C:\msys64\mingw64.exe -ArgumentList "cmake -G 'MinGW Makefiles' -DBUILD_WITH_TESTS:BOOL=ON --config Release . -Wno-dv -B ..\Development\MinGW\" -NoNewWindow -Wait
+start-process C:\msys64\mingw64.exe -ArgumentList "cmake -G 'MinGW Makefiles' -DBUILD_WITH_TESTS:BOOL=ON -Wno-dev -S . -B ./../Development/MinGW/" -NoNewWindow -Wait
 
 # Navigate to MinGW folder
 cd ..\Development\MinGW
 
 # CMake build
-start-process C:\msys64\mingw64.exe -ArgumentList "cmake --build . --config Release" -NoNewWindow -Wait
+start-process C:\msys64\mingw64.exe -ArgumentList "cmake --build ." -NoNewWindow -Wait
