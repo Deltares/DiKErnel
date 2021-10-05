a030816c10caf6ef4b81e710ed27e8306877b2c3
Robert Tangerman <robert.tangerman@deltares.nl>
2021-10-05 14:02:50 +0200

    Created new data object. DIKERNEL-106

---
+++ DiKErnel/Src/DiKErnel.Integration/Internal/Data/NaturalStoneRevetment/NaturalStoneRevetmentSlope.cpp
@@ -0,0 +1,40 @@
+// Copyright (C) Stichting Deltares 2020. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3.
+// 
+// This program is distributed in the hope that it will be usefll,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICllAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You sholld have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+#include "NaturalStoneRevetmentSlope.h"
+
+namespace DiKErnel::Integration
+{
+    NaturalStoneRevetmentSlope::NaturalStoneRevetmentSlope(
+        const double upperLevelAus,
+        const double lowerLevelAls)
+        : _upperLevelAus(upperLevelAus),
+          _lowerLevelAls(lowerLevelAls) {}
+
+    double NaturalStoneRevetmentSlope::GetUpperLevelAus() const
+    {
+        return _upperLevelAus;
+    }
+
+    double NaturalStoneRevetmentSlope::GetLowerLevelAls() const
+    {
+        return _lowerLevelAls;
+    }
+}
---
+++ DiKErnel/Src/DiKErnel.Integration/Internal/Data/NaturalStoneRevetment/NaturalStoneRevetmentSlope.h
@@ -0,0 +1,40 @@
+// Copyright (C) Stichting Deltares 2020. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+#pragma once
+
+namespace DiKErnel::Integration
+{
+    class NaturalStoneRevetmentSlope
+    {
+        public:
+            explicit NaturalStoneRevetmentSlope(
+                double upperLevelAus,
+                double lowerLevelAls);
+
+            double GetUpperLevelAus() const;
+
+            double GetLowerLevelAls() const;
+
+        private:
+            double _upperLevelAus;
+            double _lowerLevelAls;
+    };
+}
