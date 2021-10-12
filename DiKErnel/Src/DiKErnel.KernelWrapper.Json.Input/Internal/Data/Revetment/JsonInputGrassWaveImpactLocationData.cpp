e256fa8aabe50b73e6e6d2f434811d9c22667dc9
Robert Tangerman <robert.tangerman@deltares.nl>
2021-10-12 15:09:24 +0200

    Added grass wave impact json input location data. DIKERNEL-108

---
+++ DiKErnel/Src/DiKErnel.KernelWrapper.Json.Input/Internal/Data/Revetment/JsonInputGrassWaveImpactLocationData.cpp
@@ -0,0 +1,48 @@
+// Copyright (C) Stichting Deltares 2020. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+#include "JsonInputGrassWaveImpactLocationData.h"
+
+#include <utility>
+
+namespace DiKErnel::KernelWrapper::Json::Input
+{
+    using namespace std;
+
+    JsonInputGrassWaveImpactLocationData::JsonInputGrassWaveImpactLocationData(
+        string name,
+        const double x,
+        unique_ptr<JsonInputDamageData> damageData,
+        unique_ptr<JsonInputGrassRevetmentWaveImpactLocationData> revetmentLocationData,
+        unique_ptr<JsonInputProfileSchematizationData> profileSchematizationData)
+        : JsonInputLocationData(move(name), x, move(damageData)),
+          _revetmentLocationData(move(revetmentLocationData)),
+          _profileSchematizationData(move(profileSchematizationData)) {}
+
+    const JsonInputGrassRevetmentWaveImpactLocationData& JsonInputGrassWaveImpactLocationData::GetRevetmentLocationData() const
+    {
+        return *_revetmentLocationData;
+    }
+
+    const JsonInputProfileSchematizationData& JsonInputGrassWaveImpactLocationData::GetProfileSchematizationData() const
+    {
+        return *_profileSchematizationData;
+    }
+}
