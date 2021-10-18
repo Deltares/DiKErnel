7d8346ada38997bbf777b233cc1e71c13e4efca2
Elmar Kleijn <elmar.kleijn@deltares.nl>
2021-10-18 17:02:30 +0200

    Removed input data from output data. DIKERNEL-118

--- DiKErnel/Test/DiKErnel.KernelWrapper.Json.Integration.Test/IntegrationTest.cpp
+++ DiKErnel/Test/DiKErnel.KernelWrapper.Json.Integration.Test/IntegrationTest.cpp
@@ -64,9 +64,6 @@ namespace DiKErnel::KernelWrapper::Json::Integration::Test
             const json& expectedLocation,
             const json& actualLocation) const
         {
-            ASSERT_EQ(expectedLocation["Naam"], actualLocation["Naam"]);
-            ASSERT_DOUBLE_EQ(expectedLocation["PositieLocatie"], actualLocation["PositieLocatie"]);
-
             const auto& expectedFailure = expectedLocation["FalenBekleding"];
             const auto& actualFailure = actualLocation["FalenBekleding"];
 
