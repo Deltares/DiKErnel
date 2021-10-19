17e738550e0c5e369683681b30823274e176ef5e
Robert Tangerman <robert.tangerman@deltares.nl>
2021-10-19 13:31:07 +0200

    Fixed documentation. DIKERNEL-101

--- DiKErnel/Src/DiKErnel.KernelWrapper.Json.Input/Public/JsonInputComposer.h
+++ DiKErnel/Src/DiKErnel.KernelWrapper.Json.Input/Public/JsonInputComposer.h
@@ -39,7 +39,8 @@ namespace DiKErnel::KernelWrapper::Json::Input
              * \brief Gets input data based on Json.
              * \param filePath
              *        The path to the Json file.
-             * \return A result with the created ICalculationInput and the JsonInputProcessType.
+             * \return A result with the created ICalculationInput and the JsonInputProcessType;
+             *         or with nullptr when an error occurred.
             */
             static std::unique_ptr<Util::Result<std::tuple<std::unique_ptr<Core::ICalculationInput>, JsonInputProcessType>>> GetInputDataFromJson(
                 const std::string& filePath);
