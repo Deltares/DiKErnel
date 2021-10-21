98a42a0921f075a98700f745bc9c61b466b88cc2
Robert Tangerman <robert.tangerman@deltares.nl>
2021-10-21 14:41:58 +0200

    Created RevetmentValidator. DIKERNEL-135

---
+++ DiKErnel/Src/DiKErnel.DomainLibrary/Public/Validators/RevetmentValidator.h
@@ -0,0 +1,56 @@
+// Copyright (C) Stichting Deltares 2020. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+#pragma once
+
+#include <memory>
+
+#include "ValidationIssue.h"
+
+namespace DiKErnel::DomainLibrary
+{
+    /*!
+     * \brief Class that holds all generic revetment validation routines.
+     */
+    class RevetmentValidator
+    {
+        public:
+            /*!
+             * \brief Validates the initial damage.
+             * \param initialDamage
+             *        The initial damage.
+             * \return A validation issue when initial damage is not valid; nullptr otherwise.
+             */
+            static std::unique_ptr<ValidationIssue> InitialDamage(
+                double initialDamage);
+
+            /*!
+             * \brief Validates the failure number.
+             * \param failureNumber
+             *        The failure number.
+             * \param initialDamage
+             *        The initial damage.
+             * \return A validation issue when failure number is not valid; nullptr otherwise.
+             */
+            static std::unique_ptr<ValidationIssue> FailureNumber(
+                double failureNumber,
+                double initialDamage);
+    };
+}
---
+++ DiKErnel/Test/DiKErnel.DomainLibrary.Test/Validators/RevetmentValidatorTest.cpp
@@ -0,0 +1,108 @@
+// Copyright (C) Stichting Deltares 2020. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+#include <gtest/gtest.h>
+
+#include "RevetmentValidator.h"
+
+namespace DiKErnel::DomainLibrary::Test
+{
+    using namespace std;
+
+    TEST(RevetmentValidatorTest, InitialDamage_BelowZero_ReturnValidationIssueWithError)
+    {
+        // Call
+        const auto validationIssue = RevetmentValidator::InitialDamage(-0.0000000001);
+
+        // Assert
+        ASSERT_NE(nullptr, validationIssue);
+        ASSERT_EQ(ValidationIssueType::Error, validationIssue->GetValidationIssueType());
+        ASSERT_EQ("InitialDamage must be a positive number.", validationIssue->GetMessage());
+    }
+
+    TEST(RevetmentValidatorTest, InitialDamage_One_ReturnValidationIssueWithWarning)
+    {
+        // Call
+        const auto validationIssue = RevetmentValidator::InitialDamage(1);
+
+        // Assert
+        ASSERT_NE(nullptr, validationIssue);
+        ASSERT_EQ(ValidationIssueType::Warning, validationIssue->GetValidationIssueType());
+        ASSERT_EQ("InitialDamage should be smaller than 1.", validationIssue->GetMessage());
+    }
+
+    TEST(RevetmentValidatorTest, InitialDamage_LargerThanOne_ReturnValidationIssueWithWarning)
+    {
+        // Call
+        const auto validationIssue = RevetmentValidator::InitialDamage(1.1);
+
+        // Assert
+        ASSERT_NE(nullptr, validationIssue);
+        ASSERT_EQ(ValidationIssueType::Warning, validationIssue->GetValidationIssueType());
+        ASSERT_EQ("InitialDamage should be smaller than 1.", validationIssue->GetMessage());
+    }
+
+    TEST(RevetmentValidatorTest, InitialDamage_Zero_ReturnNullPtr)
+    {
+        // Call
+        const auto validationIssue = RevetmentValidator::InitialDamage(0);
+
+        // Assert
+        ASSERT_EQ(nullptr, validationIssue);
+    }
+
+    TEST(RevetmentValidatorTest, InitialDamage_SmallerThanOne_ReturnNullPtr)
+    {
+        // Call
+        const auto validationIssue = RevetmentValidator::InitialDamage(0.99999999999);
+
+        // Assert
+        ASSERT_EQ(nullptr, validationIssue);
+    }
+
+    TEST(RevetmentValidatorTest, FailureNumber_SmallerThanInitialDamage_ReturnValidationIssueWithError)
+    {
+        // Call
+        const auto validationIssue = RevetmentValidator::FailureNumber(0.199999999999, 0.2);
+
+        // Assert
+        ASSERT_NE(nullptr, validationIssue);
+        ASSERT_EQ(ValidationIssueType::Error, validationIssue->GetValidationIssueType());
+        ASSERT_EQ("FailureNumber must be larger than InitialDamage.", validationIssue->GetMessage());
+    }
+
+    TEST(RevetmentValidatorTest, FailureNumber_EqualToInitialDamage_ReturnNullPtr)
+    {
+        // Call
+        const auto validationIssue = RevetmentValidator::FailureNumber(0, 0);
+
+        // Assert
+        ASSERT_EQ(nullptr, validationIssue);
+    }
+
+    TEST(RevetmentValidatorTest, FailureNumber_LargerThanInitialDamage_ReturnNullPtr)
+    {
+        // Call
+        const auto validationIssue = RevetmentValidator::FailureNumber(0.0000000000001, 0);
+
+        // Assert
+        ASSERT_EQ(nullptr, validationIssue);
+    }
+}
