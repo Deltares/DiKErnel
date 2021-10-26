74d0e164fb3234d12388e7b5af373cf2c9c7c1a8
Robert Tangerman <robert.tangerman@deltares.nl>
2021-10-26 14:43:18 +0200

    Added system test. DIKERNEL-140

---
+++ DiKErnel/Test/DiKErnel.System.Test/ValidationSystemTest.cpp
@@ -0,0 +1,63 @@
+// Copyright (C) Stichting Deltares 2020. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+#include <gtest/gtest.h>
+
+#include "RevetmentCalculationInputBuilder.h"
+#include "Validator.h"
+
+namespace DiKErnel::System::Test
+{
+    using namespace Core;
+    using namespace Integration;
+    using namespace std;
+    using namespace testing;
+    using namespace Util;
+
+    struct ValidationSystemTest : Test
+    {
+        static void AssertEvent(
+            const EventType expectedEventType,
+            const string& expectedMessage,
+            const Event& actualEvent)
+        {
+            ASSERT_EQ(expectedEventType, actualEvent.GetEventType());
+            ASSERT_EQ(expectedMessage, actualEvent.GetMessage());
+        }
+    };
+
+    TEST_F(ValidationSystemTest, GivenCalculationInputWithoutTimeStepsAndLocations_WhenValidating_ThenReturnsValidationResult)
+    {
+        // Given
+        RevetmentCalculationInputBuilder builder;
+        const auto calculationInput = builder.Build();
+
+        // When
+        const auto validationResult = Validator::Validate(*calculationInput);
+
+        // Then
+        ASSERT_TRUE(validationResult->GetSuccessful());
+        ASSERT_EQ(ValidationResultType::Failed, *validationResult->GetData());
+        const auto& events = validationResult->GetEvents();
+        ASSERT_EQ(2, events.size());
+        AssertEvent(EventType::Error, "At least 1 time step must be defined.", events[0]);
+        AssertEvent(EventType::Error, "At least 1 location must be defined.", events[1]);
+    }
+}
