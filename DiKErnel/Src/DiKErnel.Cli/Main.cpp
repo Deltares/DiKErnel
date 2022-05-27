a7fe7ccee0dee79ec878ebdd90527b2ac0f13cfc
Robert Tangerman <robert.tangerman@deltares.nl>
2022-05-27 09:10:38 +0200

    Implemented calculation. DIKENREL-152

--- DiKErnel/Src/DiKErnel.Cli/Main.cpp
+++ DiKErnel/Src/DiKErnel.Cli/Main.cpp
@@ -19,7 +19,21 @@
 // All rights reserved.
 
 #include <filesystem>
+#include <fstream>
 #include <iostream>
+#include <string>
+#include <thread>
+
+#include "Calculator.h"
+#include "EventRegistry.h"
+#include "JsonInputComposer.h"
+#include "JsonOutputComposer.h"
+#include "Validator.h"
+
+using namespace DiKErnel::Core;
+using namespace DiKErnel::KernelWrapper::Json::Input;
+using namespace DiKErnel::KernelWrapper::Json::Output;
+using namespace DiKErnel::Util;
 using namespace std;
 
 #pragma region Forward declarations
@@ -27,23 +41,102 @@ using namespace std;
 string GetLogOutputFilePath(
     const string& jsonOutputFilePath);
 
+JsonOutputType ConvertProcessType(
+    JsonInputProcessType);
+
+void WriteToLogFile(
+    const vector<reference_wrapper<Event>>& events);
+
+string GetEventTypeString(
+    EventType eventType);
+
+void CloseApplicationAfterUnhandledError();
+
+[[noreturn]]
+void UnhandledErrorHandler();
+
+vector<reference_wrapper<Event>> GetEventReferences(
+    const vector<unique_ptr<Event>>& events
+);
+
 #pragma endregion
+
+string logOutputFilePath;
 
 int main(
     int argc,
     char* argv[])
 {
-    if(argc != 3)
+    set_terminate(UnhandledErrorHandler);
+
+    try
     {
-        cout << "You have entered an invalid number of arguments." << endl;
+        if (argc != 3)
+        {
+            cout << "You have entered an invalid number of arguments." << endl;
+            return 0;
+        }
+
+        const string jsonInputFilePath(argv[1]);
+        const string jsonOutputFilePath(argv[2]);
+        logOutputFilePath = GetLogOutputFilePath(jsonOutputFilePath);
+
+        const auto inputComposerResult = JsonInputComposer::GetInputDataFromJson(jsonInputFilePath);
+        WriteToLogFile(inputComposerResult->GetEvents());
+
+        if (!inputComposerResult->GetSuccessful())
+        {
+            return -1;
+        }
+
+        const auto* inputData = inputComposerResult->GetData();
+        const auto& calculationInput = inputData->GetCalculationInput();
+
+        const auto validationResult = Validator::Validate(calculationInput);
+        WriteToLogFile(validationResult->GetEvents());
+
+        if (!validationResult->GetSuccessful())
+        {
+            return -1;
+        }
+
+        if (*validationResult->GetData() != ValidationResultType::Successful)
+        {
+            return -1;
+        }
+
+        Calculator calculator(calculationInput);
+        calculator.WaitForCompletion();
+
+        const auto calculatorResult = calculator.GetResult();
+        WriteToLogFile(calculatorResult->GetEvents());
+
+        if (calculator.GetCalculationState() == CalculationState::FinishedInError)
+        {
+            return -1;
+        }
+
+        const auto outputComposerResult = JsonOutputComposer::WriteCalculationOutputToJson(
+            jsonOutputFilePath, *calculatorResult->GetData(),
+            ConvertProcessType(inputData->GetProcessType()));
+
+        WriteToLogFile(outputComposerResult->GetEvents());
+
+        if (!outputComposerResult->GetSuccessful())
+        {
+            return -1;
+        }
+
         return 0;
     }
+    catch (const exception& e)
+    {
+        EventRegistry::Register(make_unique<Event>("An unhandled error occurred in the Showcase. See stack trace for more information:\n"
+                                                   + static_cast<string>(e.what()), EventType::Error));
 
-    const string jsonInputFilePath(argv[1]);
-    const string jsonOutputFilePath(argv[2]);
-    const auto logOutputFilePath = GetLogOutputFilePath(jsonOutputFilePath);
-
-    return 0;
+        CloseApplicationAfterUnhandledError();
+        return -1;
+    }
 }
 
 string GetLogOutputFilePath(
@@ -54,4 +147,86 @@ string GetLogOutputFilePath(
     const auto outputFileName = outputFilePath.stem().u8string();
 
     return (outputDirectory / (outputFileName + ".log")).u8string();
+}
+
+JsonOutputType ConvertProcessType(
+    const JsonInputProcessType processType)
+{
+    switch (processType)
+    {
+        case JsonInputProcessType::Failure:
+            return JsonOutputType::Failure;
+        case JsonInputProcessType::Damage:
+            return JsonOutputType::Damage;
+        case JsonInputProcessType::Physics:
+            return JsonOutputType::Physics;
+        default:
+            throw runtime_error("Unsupported processType");
+    }
+}
+
+void WriteToLogFile(
+    const vector<reference_wrapper<Event>>& events)
+{
+    if (events.empty())
+    {
+        return;
+    }
+
+    ofstream logFile;
+
+    // Open file and append if already exists.
+    logFile.open(logOutputFilePath, ios::app);
+
+    for (const auto& eventReference : events)
+    {
+        const auto& event = eventReference.get();
+
+        logFile << GetEventTypeString(event.GetEventType()) << ": " << event.GetMessage() << endl;
+    }
+}
+
+string GetEventTypeString(
+    const EventType eventType)
+{
+    switch (eventType)
+    {
+        case EventType::Warning:
+            return "Warning";
+        case EventType::Error:
+            return "Error";
+        default:
+            throw runtime_error("Unsupported EventType");
+    }
+}
+
+void CloseApplicationAfterUnhandledError()
+{
+    const auto events = EventRegistry::Flush();
+    WriteToLogFile(GetEventReferences(events));
+}
+
+void UnhandledErrorHandler()
+{
+    EventRegistry::Register(make_unique<Event>("An unhandled error occurred in the Showcase. No stacktrace available.", EventType::Error));
+
+    CloseApplicationAfterUnhandledError();
+
+    _exit(-1);
+}
+
+vector<reference_wrapper<Event>> GetEventReferences(
+    const vector<unique_ptr<Event>>& events
+)
+{
+    vector<reference_wrapper<Event>> eventReferences;
+
+    eventReferences.reserve(events.size());
+
+    for (const auto& event : events)
+    {
+        eventReferences.emplace_back(*event);
+    }
+
+    return eventReferences;
 }
