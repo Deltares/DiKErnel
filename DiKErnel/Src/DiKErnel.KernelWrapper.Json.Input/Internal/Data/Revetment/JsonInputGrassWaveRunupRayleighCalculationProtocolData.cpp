7ed46f1ea4593923cdeb4cf0753ce96d18b1a2ef
Robert Tangerman <robert.tangerman@deltares.nl>
2022-06-16 14:19:17 +0200

    Introduced grass wave runup calculation protocol data. DIKERNEL-170

---
+++ DiKErnel/Src/DiKErnel.KernelWrapper.Json.Input/Internal/Data/Revetment/JsonInputGrassWaveRunupRayleighCalculationProtocolData.cpp
@@ -0,0 +1,56 @@
+// Copyright (C) Stichting Deltares 2022. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+#include "JsonInputGrassWaveRunupRayleighCalculationProtocolData.h"
+
+namespace DiKErnel::KernelWrapper::Json::Input
+{
+    using namespace std;
+
+    #pragma region Set methods
+
+    void JsonInputGrassWaveRunupRayleighCalculationProtocolData::SetFixedNumberOfWaves(
+        unique_ptr<int> fixedNumberOfWaves)
+    {
+        _fixedNumberOfWaves = move(fixedNumberOfWaves);
+    }
+
+    void JsonInputGrassWaveRunupRayleighCalculationProtocolData::SetFrontVelocityCu(
+        unique_ptr<double> frontVelocityCu)
+    {
+        _frontVelocityCu = move(frontVelocityCu);
+    }
+
+    #pragma endregion
+
+    #pragma region Get methods
+
+    const int* JsonInputGrassWaveRunupRayleighCalculationProtocolData::GetFixedNumberOfWaves() const
+    {
+        return _fixedNumberOfWaves.get();
+    }
+
+    const double* JsonInputGrassWaveRunupRayleighCalculationProtocolData::GetFrontVelocityCu() const
+    {
+        return _frontVelocityCu.get();
+    }
+
+    #pragma endregion
+}
