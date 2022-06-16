66930b69ae062dbfe07a97a6a99daba3f347050d
Robert Tangerman <robert.tangerman@deltares.nl>
2022-06-16 08:28:22 +0200

    Implemented parser. DIKERNEL-170

--- DiKErnel/Src/DiKErnel.KernelWrapper.Json.Input/Internal/Parsers/JsonInputGrassWaveImpactCalculationDefinitionParser.cpp
+++ DiKErnel/Src/DiKErnel.KernelWrapper.Json.Input/Internal/Parsers/JsonInputGrassWaveImpactCalculationDefinitionParser.cpp
@@ -21,6 +21,8 @@
 #include "JsonInputGrassWaveImpactCalculationDefinitionParser.h"
 
 #include "JsonInputGrassWaveImpactCalculationDefinitionData.h"
+#include "JsonInputGrassWaveImpactDefinitions.h"
+#include "JsonInputParserHelper.h"
 
 namespace DiKErnel::KernelWrapper::Json::Input
 {
@@ -34,8 +36,73 @@ namespace DiKErnel::KernelWrapper::Json::Input
     unique_ptr<JsonInputCalculationDefinitionData> JsonInputGrassWaveImpactCalculationDefinitionParser::ParseCalculationDefinition(
         unique_ptr<double> failureNumber)
     {
+        const auto& readCalculationMethod = GetReadCalculationMethod();
+
         auto calculationDefinition = make_unique<JsonInputGrassWaveImpactCalculationDefinitionData>(
             move(failureNumber));
+
+        calculationDefinition->SetTimeLineAgwi(
+            forward<unique_ptr<double>>(JsonInputParserHelper::ParseOptionalDouble(
+                readCalculationMethod, JsonInputGrassWaveImpactDefinitions::TIME_LINE_AGWI)));
+        calculationDefinition->SetTimeLineBgwi(
+            forward<unique_ptr<double>>(JsonInputParserHelper::ParseOptionalDouble(
+                readCalculationMethod, JsonInputGrassWaveImpactDefinitions::TIME_LINE_BGWI)));
+        calculationDefinition->SetTimeLineCgwi(
+            forward<unique_ptr<double>>(JsonInputParserHelper::ParseOptionalDouble(
+                readCalculationMethod, JsonInputGrassWaveImpactDefinitions::TIME_LINE_CGWI)));
+
+        if (readCalculationMethod.contains(JsonInputGrassWaveImpactDefinitions::MINIMUM_WAVE_HEIGHT))
+        {
+            const auto& readMinimumWaveHeight = readCalculationMethod.at(JsonInputGrassWaveImpactDefinitions::MINIMUM_WAVE_HEIGHT);
+
+            calculationDefinition->SetMinimumWaveHeightTemax(
+                forward<unique_ptr<double>>(JsonInputParserHelper::ParseOptionalDouble(
+                    readMinimumWaveHeight, JsonInputGrassWaveImpactDefinitions::MINIMUM_WAVE_HEIGHT_TEMAX)));
+        }
+
+        if (readCalculationMethod.contains(JsonInputGrassWaveImpactDefinitions::MAXIMUM_WAVE_HEIGHT))
+        {
+            const auto& readMaximumWaveHeight = readCalculationMethod.at(JsonInputGrassWaveImpactDefinitions::MAXIMUM_WAVE_HEIGHT);
+
+            calculationDefinition->SetMaximumWaveHeightTemin(
+                forward<unique_ptr<double>>(JsonInputParserHelper::ParseOptionalDouble(
+                    readMaximumWaveHeight, JsonInputGrassWaveImpactDefinitions::MAXIMUM_WAVE_HEIGHT_TEMIN)));
+        }
+
+        if (readCalculationMethod.contains(JsonInputGrassWaveImpactDefinitions::WAVE_ANGLE_IMPACT))
+        {
+            const auto& readWaveAngleImpact = readCalculationMethod.at(JsonInputGrassWaveImpactDefinitions::WAVE_ANGLE_IMPACT);
+
+            calculationDefinition->SetWaveAngleImpactNwa(
+                forward<unique_ptr<double>>(JsonInputParserHelper::ParseOptionalDouble(
+                    readWaveAngleImpact, JsonInputGrassWaveImpactDefinitions::WAVE_ANGLE_IMPACT_NWA)));
+
+            calculationDefinition->SetWaveAngleImpactQwa(
+                forward<unique_ptr<double>>(JsonInputParserHelper::ParseOptionalDouble(
+                    readWaveAngleImpact, JsonInputGrassWaveImpactDefinitions::WAVE_ANGLE_IMPACT_QWA)));
+
+            calculationDefinition->SetWaveAngleImpactRwa(
+                forward<unique_ptr<double>>(JsonInputParserHelper::ParseOptionalDouble(
+                    readWaveAngleImpact, JsonInputGrassWaveImpactDefinitions::WAVE_ANGLE_IMPACT_RWA)));
+        }
+
+        if (readCalculationMethod.contains(JsonInputGrassWaveImpactDefinitions::UPPER_LIMIT_LOADING))
+        {
+            const auto& readUpperLimitLoading = readCalculationMethod.at(JsonInputGrassWaveImpactDefinitions::UPPER_LIMIT_LOADING);
+
+            calculationDefinition->SetUpperLimitLoadingAul(
+                forward<unique_ptr<double>>(JsonInputParserHelper::ParseOptionalDouble(
+                    readUpperLimitLoading, JsonInputGrassWaveImpactDefinitions::UPPER_LIMIT_LOADING_AUL)));
+        }
+
+        if (readCalculationMethod.contains(JsonInputGrassWaveImpactDefinitions::LOWER_LIMIT_LOADING))
+        {
+            const auto& readLowerLimitLoading = readCalculationMethod.at(JsonInputGrassWaveImpactDefinitions::LOWER_LIMIT_LOADING);
+
+            calculationDefinition->SetLowerLimitLoadingAll(
+                forward<unique_ptr<double>>(JsonInputParserHelper::ParseOptionalDouble(
+                    readLowerLimitLoading, JsonInputGrassWaveImpactDefinitions::LOWER_LIMIT_LOADING_ALL)));
+        }
 
         return calculationDefinition;
     }
