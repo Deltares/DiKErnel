53e578b52861b2faad6b44139672e64f26f55b3f
Robert Tangerman <robert.tangerman@deltares.nl>
2022-06-16 11:24:13 +0200

    Added enum serialization to calculation definition parser. DIKERNEL-170

--- DiKErnel/Src/DiKErnel.KernelWrapper.Json.Input/Internal/Parsers/JsonInputGrassWaveImpactCalculationDefinitionParser.cpp
+++ DiKErnel/Src/DiKErnel.KernelWrapper.Json.Input/Internal/Parsers/JsonInputGrassWaveImpactCalculationDefinitionParser.cpp
@@ -21,6 +21,7 @@
 #include "JsonInputGrassWaveImpactCalculationDefinitionParser.h"
 
 #include "JsonInputDefinitions.h"
+#include "JsonInputGrassRevetmentDefinitions.h"
 #include "JsonInputGrassWaveImpactCalculationDefinitionData.h"
 #include "JsonInputGrassWaveImpactDefinitions.h"
 #include "JsonInputParserHelper.h"
@@ -29,6 +30,19 @@ namespace DiKErnel::KernelWrapper::Json::Input
 {
     using namespace nlohmann;
     using namespace std;
+
+    NLOHMANN_JSON_SERIALIZE_ENUM(JsonInputGrassRevetmentTopLayerType,
+        {
+            {
+                JsonInputGrassRevetmentTopLayerType::Unknown, nullptr
+            },
+            {
+                JsonInputGrassRevetmentTopLayerType::ClosedSod, JsonInputGrassRevetmentDefinitions::TOP_LAYER_TYPE_CLOSED_SOD
+            },
+            {
+                JsonInputGrassRevetmentTopLayerType::OpenSod, JsonInputGrassRevetmentDefinitions::TOP_LAYER_TYPE_OPEN_SOD
+            }
+        });
 
     JsonInputGrassWaveImpactCalculationDefinitionParser::JsonInputGrassWaveImpactCalculationDefinitionParser(
         const json& readCalculationMethod)
