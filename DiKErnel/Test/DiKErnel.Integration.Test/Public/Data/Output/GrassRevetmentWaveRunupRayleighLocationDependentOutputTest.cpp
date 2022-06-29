f075200e9a150e73eaa2a72b16d0944952035d5f
Elmar Kleijn <elmar.kleijn@deltares.nl>
2022-06-29 11:26:30 +0200

    Introduced remaining LocationDependentOutput derivatives. DIKERNEL-128

---
+++ DiKErnel/Test/DiKErnel.Integration.Test/Public/Data/Output/GrassRevetmentWaveRunupRayleighLocationDependentOutputTest.cpp
@@ -0,0 +1,56 @@
+// Copyright (C) Stichting Deltares 2022. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+#include <gtest/gtest.h>
+
+#include "GrassRevetmentWaveRunupRayleighLocationDependentOutput.h"
+#include "AssertHelper.h"
+#include "LocationDependentOutput.h"
+#include "TimeDependentOutputMock.h"
+
+namespace DiKErnel::Integration::Test
+{
+    using namespace Core;
+    using namespace Core::TestUtil;
+    using namespace DiKErnel::TestUtil;
+    using namespace std;
+
+    TEST(GrassRevetmentWaveRunupRayleighLocationDependentOutputTest, Constructor_ExpectedValues)
+    {
+        // Setup
+        constexpr auto z = 0.1;
+        constexpr auto incrementDamage = 0.2;
+        constexpr auto damage = 0.3;
+        constexpr auto timeOfFailure = 4;
+
+        auto timeDependentOutputItems = vector<unique_ptr<TimeDependentOutput>>();
+        timeDependentOutputItems.push_back(make_unique<TimeDependentOutputMock>(incrementDamage, damage, make_unique<int>(timeOfFailure)));
+
+        // Call
+        const GrassRevetmentWaveRunupRayleighLocationDependentOutput output(z, move(timeDependentOutputItems));
+
+        // Assert
+        AssertHelper::AssertIsInstanceOf<LocationDependentOutput>(&output);
+        ASSERT_DOUBLE_EQ(z, output.GetZ());
+        ASSERT_EQ(vector{ damage }, output.GetDamages());
+        ASSERT_EQ(timeOfFailure, *output.GetTimeOfFailure());
+        ASSERT_EQ(1, output.GetTimeDependentOutputItems().size());
+    }
+}
