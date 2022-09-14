f01461692ee989d0a74c5fef2176fa3d03b6ce03
Robert Tangerman <robert.tangerman@deltares.nl>
2022-09-14 07:39:45 +0200

    Introduced location dependent input factory. DIKERNEL-193

---
+++ DiKErnel/Src/DiKErnel.Integration/Internal/Factories/LocationDependentInputFactory.cpp
@@ -0,0 +1,96 @@
+// Copyright (C) Stichting Deltares 2022. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+#include "LocationDependentInputFactory.h"
+
+#include "AsphaltRevetmentWaveImpactLocationConstructionProperties.h"
+#include "AsphaltRevetmentWaveImpactLocationDependentInputFactory.h"
+#include "GrassRevetmentWaveImpactLocationConstructionProperties.h"
+#include "GrassRevetmentWaveImpactLocationDependentInputFactory.h"
+#include "GrassRevetmentWaveRunupRayleighLocationConstructionProperties.h"
+#include "GrassRevetmentWaveRunupRayleighLocationDependentInputFactory.h"
+#include "NaturalStoneRevetmentLocationConstructionProperties.h"
+#include "NaturalStoneRevetmentLocationDependentInputFactory.h"
+#include "RevetmentLocationConstructionPropertiesBase.h"
+
+namespace DiKErnel::Integration
+{
+    using namespace Core;
+    using namespace std;
+
+    vector<unique_ptr<ILocationDependentInput>> LocationDependentInputFactory::CreateLocationDependentInputs(
+        const vector<reference_wrapper<RevetmentLocationConstructionPropertiesBase>>& locationConstructionPropertiesItems)
+    {
+        if (locationConstructionPropertiesItems.empty())
+        {
+            throw exception();
+        }
+
+        auto locationDependentInputItems = vector<unique_ptr<ILocationDependentInput>>();
+
+        for (const auto& locationConstructionPropertiesReference : locationConstructionPropertiesItems)
+        {
+            const auto& locationConstructionProperties = locationConstructionPropertiesReference.get();
+
+            if (const auto* asphaltWaveImpactLocationConstructionProperties = dynamic_cast<const
+                    AsphaltRevetmentWaveImpactLocationConstructionProperties*>(&locationConstructionProperties);
+                asphaltWaveImpactLocationConstructionProperties != nullptr)
+            {
+                locationDependentInputItems.push_back(
+                    AsphaltRevetmentWaveImpactLocationDependentInputFactory::CreateLocationDependentInput(
+                        *asphaltWaveImpactLocationConstructionProperties));
+                continue;
+            }
+
+            if (const auto* grassWaveImpactLocationConstructionProperties = dynamic_cast<const
+                    GrassRevetmentWaveImpactLocationConstructionProperties*>(&locationConstructionProperties);
+                grassWaveImpactLocationConstructionProperties != nullptr)
+            {
+                locationDependentInputItems.push_back(
+                    GrassRevetmentWaveImpactLocationDependentInputFactory::CreateLocationDependentInput(
+                        *grassWaveImpactLocationConstructionProperties));
+                continue;
+            }
+
+            if (const auto* grassWaveRunupRayleighLocationConstructionProperties = dynamic_cast<const
+                    GrassRevetmentWaveRunupRayleighLocationConstructionProperties*>(&locationConstructionProperties);
+                grassWaveRunupRayleighLocationConstructionProperties != nullptr)
+            {
+                locationDependentInputItems.push_back(
+                    GrassRevetmentWaveRunupRayleighLocationDependentInputFactory::CreateLocationDependentInput(
+                        *grassWaveRunupRayleighLocationConstructionProperties));
+                continue;
+            }
+
+            if (const auto* naturalStoneLocationConstructionProperties = dynamic_cast<const NaturalStoneRevetmentLocationConstructionProperties*>(
+                &locationConstructionProperties); naturalStoneLocationConstructionProperties != nullptr)
+            {
+                locationDependentInputItems.push_back(
+                    NaturalStoneRevetmentLocationDependentInputFactory::CreateLocationDependentInput(*naturalStoneLocationConstructionProperties));
+                continue;
+            }
+
+            // No valid construction properties.
+            throw exception();
+        }
+
+        return locationDependentInputItems;
+    }
+}
