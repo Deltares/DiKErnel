7f2817742236c49530c2eb705dfde1f39ac0adef
Elmar Kleijn <elmar.kleijn@deltares.nl>
2022-09-20 10:31:09 +0200

    Introduced GrassRevetmentOvertoppingLocationDependentInputAssertHelper. DIKERNEL-197

---
+++ DiKErnel/Test/DiKErnel.Integration.TestUtil/GrassRevetmentOvertoppingLocationDependentInputAssertHelper.cpp
@@ -0,0 +1,65 @@
+// Copyright (C) Stichting Deltares 2022. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+#include "GrassRevetmentOvertoppingLocationDependentInputAssertHelper.h"
+
+#include <gtest/gtest.h>
+
+namespace DiKErnel::Integration::TestUtil
+{
+    using namespace std;
+
+    void GrassRevetmentOvertoppingLocationDependentInputAssertHelper::AssertGeneralProperties(
+        const double x,
+        const GrassRevetmentOvertoppingLocationDependentInput& locationDependentInput)
+    {
+        ASSERT_DOUBLE_EQ(x, locationDependentInput.GetX());
+    }
+
+    void GrassRevetmentOvertoppingLocationDependentInputAssertHelper::AssertCumulativeOverload(
+        const double criticalCumulativeOverload,
+        const GrassRevetmentOvertoppingLocationDependentInput& locationDependentInput)
+    {
+        ASSERT_DOUBLE_EQ(criticalCumulativeOverload, locationDependentInput.GetCriticalCumulativeOverload());
+    }
+
+    void GrassRevetmentOvertoppingLocationDependentInputAssertHelper::AssertFrontVelocity(
+        const double criticalFrontVelocity,
+        const GrassRevetmentOvertoppingLocationDependentInput& locationDependentInput)
+    {
+        ASSERT_DOUBLE_EQ(criticalFrontVelocity, locationDependentInput.GetCriticalFrontVelocity());
+    }
+
+    void GrassRevetmentOvertoppingLocationDependentInputAssertHelper::AssertTransitionAlpha(
+        const double increasedLoadTransitionAlphaM,
+        const double reducedStrengthTransitionAlphaS,
+        const GrassRevetmentOvertoppingLocationDependentInput& locationDependentInput)
+    {
+        ASSERT_DOUBLE_EQ(increasedLoadTransitionAlphaM, locationDependentInput.GetIncreasedLoadTransitionAlphaM());
+        ASSERT_DOUBLE_EQ(reducedStrengthTransitionAlphaS, locationDependentInput.GetReducedStrengthTransitionAlphaS());
+    }
+
+    void GrassRevetmentOvertoppingLocationDependentInputAssertHelper::AssertAverageNumberOfWaves(
+        const double averageNumberOfWavesCtm,
+        const GrassRevetmentOvertoppingLocationDependentInput& locationDependentInput)
+    {
+        ASSERT_DOUBLE_EQ(averageNumberOfWavesCtm, locationDependentInput.GetAverageNumberOfWavesCtm());
+    }
+}
---
+++ DiKErnel/Test/DiKErnel.Integration.TestUtil/GrassRevetmentOvertoppingLocationDependentInputAssertHelper.h
@@ -0,0 +1,51 @@
+// Copyright (C) Stichting Deltares 2022. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+#pragma once
+
+#include "GrassRevetmentOvertoppingLocationDependentInput.h"
+
+namespace DiKErnel::Integration::TestUtil
+{
+    class GrassRevetmentOvertoppingLocationDependentInputAssertHelper
+    {
+        public:
+            static void AssertGeneralProperties(
+                double x,
+                const GrassRevetmentOvertoppingLocationDependentInput& locationDependentInput);
+
+            static void AssertCumulativeOverload(
+                double criticalCumulativeOverload,
+                const GrassRevetmentOvertoppingLocationDependentInput& locationDependentInput);
+
+            static void AssertFrontVelocity(
+                double criticalFrontVelocity,
+                const GrassRevetmentOvertoppingLocationDependentInput& locationDependentInput);
+
+            static void AssertTransitionAlpha(
+                double increasedLoadTransitionAlphaM,
+                double reducedStrengthTransitionAlphaS,
+                const GrassRevetmentOvertoppingLocationDependentInput& locationDependentInput);
+
+            static void AssertAverageNumberOfWaves(
+                double averageNumberOfWavesCtm,
+                const GrassRevetmentOvertoppingLocationDependentInput& locationDependentInput);
+    };
+}
