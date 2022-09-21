598ff7c4b8ebb63704b758ba2b1683bc313a43dd
Elmar Kleijn <elmar.kleijn@deltares.nl>
2022-09-21 14:49:22 +0200

    Extended GrassRevetmentOvertoppingCalculationTest. DIKERNEL-206

--- DiKErnel/Test/DiKErnel.System.Test/GrassRevetmentOvertoppingCalculationTest.cpp
+++ DiKErnel/Test/DiKErnel.System.Test/GrassRevetmentOvertoppingCalculationTest.cpp
@@ -29,30 +29,11 @@ namespace DiKErnel::System::Test
     using namespace Core;
     using namespace Integration;
     using namespace std;
-    
-    TEST(GrassRevetmentOvertoppingCalculationTest, GivenValidCalculationInput_WhenCalculating_ThenReturnsExpectedCalculationResult)
+    using namespace testing;
+
+    struct GrassRevetmentOvertoppingCalculationTest : Test
     {
-        // Given
-        CalculationInputBuilder builder;
-
-        builder.AddTimeStep(0, 3600, 5.5, 1.9, 4.7, -10.0);
-        builder.AddTimeStep(3600, 7200, 5.6, 1.95, 4.73, -8.0);
-        builder.AddTimeStep(7200, 10800, 5.7, 2.0, 4.76, -6.0);
-        builder.AddTimeStep(10800, 14400, 5.8, 2.05, 4.79, -4.0);
-        builder.AddTimeStep(14400, 18000, 5.9, 2.1, 4.82, -2.0);
-        builder.AddTimeStep(18000, 21600, 6.0, 2.15, 4.85, 0.0);
-        builder.AddTimeStep(21600, 25200, 6.1, 2.2, 4.88, 2.0);
-        builder.AddTimeStep(25200, 28800, 6.2, 2.25, 4.91, 4.0);
-        builder.AddTimeStep(28800, 32400, 6.2, 2.3, 4.94, 6.0);
-        builder.AddTimeStep(32400, 36000, 6.1, 2.35, 4.97, 8.0);
-        builder.AddTimeStep(36000, 39600, 6.0, 2.4, 5.0, 10.0);
-        builder.AddTimeStep(39600, 43200, 5.9, 2.45, 5.03, 12.0);
-        builder.AddTimeStep(43200, 46800, 5.8, 2.5, 5.06, 14.0);
-        builder.AddTimeStep(46800, 50400, 5.7, 2.55, 5.09, 16.0);
-        builder.AddTimeStep(50400, 54000, 5.6, 2.6, 5.12, 18.0);
-        builder.AddTimeStep(54000, 57600, 5.5, 2.65, 5.15, 20.0);
-
-        constexpr double roughnessCoefficients[] =
+        const vector<double> _roughnessCoefficientsForTestCase1
         {
             1.0,
             1.0,
@@ -66,23 +47,52 @@ namespace DiKErnel::System::Test
             0.8
         };
 
-        builder.AddDikeProfileSegment(0.0, -0.1, 5.0, 0.0, &roughnessCoefficients[0]);
-        builder.AddDikeProfileSegment(5.0, 0.0, 15.0, 3.0, &roughnessCoefficients[1]);
-        builder.AddDikeProfileSegment(15.0, 3.0, 22.0, 3.2, &roughnessCoefficients[2]);
-        builder.AddDikeProfileSegment(22.0, 3.2, 30.0, 7.5, &roughnessCoefficients[3]);
-        builder.AddDikeProfileSegment(30.0, 7.5, 31.0, 7.6, &roughnessCoefficients[4]);
-        builder.AddDikeProfileSegment(31.0, 7.6, 34.0, 7.7, &roughnessCoefficients[5]);
-        builder.AddDikeProfileSegment(34.0, 7.7, 35.0, 7.4, &roughnessCoefficients[6]);
-        builder.AddDikeProfileSegment(35.0, 7.4, 45.0, 5.0, &roughnessCoefficients[7]);
-        builder.AddDikeProfileSegment(45.0, 5.0, 60.0, 0.5, &roughnessCoefficients[8]);
-        builder.AddDikeProfileSegment(60.0, 0.5, 70.0, 0.5, &roughnessCoefficients[9]);
+        void ConfigureBuilderWithSchematizationForTestCase1(
+            CalculationInputBuilder& builder) const
+        {
+            builder.AddTimeStep(0, 3600, 5.5, 1.9, 4.7, -10.0);
+            builder.AddTimeStep(3600, 7200, 5.6, 1.95, 4.73, -8.0);
+            builder.AddTimeStep(7200, 10800, 5.7, 2.0, 4.76, -6.0);
+            builder.AddTimeStep(10800, 14400, 5.8, 2.05, 4.79, -4.0);
+            builder.AddTimeStep(14400, 18000, 5.9, 2.1, 4.82, -2.0);
+            builder.AddTimeStep(18000, 21600, 6.0, 2.15, 4.85, 0.0);
+            builder.AddTimeStep(21600, 25200, 6.1, 2.2, 4.88, 2.0);
+            builder.AddTimeStep(25200, 28800, 6.2, 2.25, 4.91, 4.0);
+            builder.AddTimeStep(28800, 32400, 6.2, 2.3, 4.94, 6.0);
+            builder.AddTimeStep(32400, 36000, 6.1, 2.35, 4.97, 8.0);
+            builder.AddTimeStep(36000, 39600, 6.0, 2.4, 5.0, 10.0);
+            builder.AddTimeStep(39600, 43200, 5.9, 2.45, 5.03, 12.0);
+            builder.AddTimeStep(43200, 46800, 5.8, 2.5, 5.06, 14.0);
+            builder.AddTimeStep(46800, 50400, 5.7, 2.55, 5.09, 16.0);
+            builder.AddTimeStep(50400, 54000, 5.6, 2.6, 5.12, 18.0);
+            builder.AddTimeStep(54000, 57600, 5.5, 2.65, 5.15, 20.0);
 
-        builder.AddDikeProfilePointData(5.0, CharacteristicPointType::OuterToe);
-        builder.AddDikeProfilePointData(15.0, CharacteristicPointType::CrestOuterBerm);
-        builder.AddDikeProfilePointData(22.0, CharacteristicPointType::NotchOuterBerm);
-        builder.AddDikeProfilePointData(30.0, CharacteristicPointType::OuterCrest);
-        builder.AddDikeProfilePointData(35.0, CharacteristicPointType::InnerCrest);
-        builder.AddDikeProfilePointData(60.0, CharacteristicPointType::InnerToe);
+            builder.AddDikeProfileSegment(0.0, -0.1, 5.0, 0.0, &_roughnessCoefficientsForTestCase1.at(0));
+            builder.AddDikeProfileSegment(5.0, 0.0, 15.0, 3.0, &_roughnessCoefficientsForTestCase1.at(1));
+            builder.AddDikeProfileSegment(15.0, 3.0, 22.0, 3.2, &_roughnessCoefficientsForTestCase1.at(2));
+            builder.AddDikeProfileSegment(22.0, 3.2, 30.0, 7.5, &_roughnessCoefficientsForTestCase1.at(3));
+            builder.AddDikeProfileSegment(30.0, 7.5, 31.0, 7.6, &_roughnessCoefficientsForTestCase1.at(4));
+            builder.AddDikeProfileSegment(31.0, 7.6, 34.0, 7.7, &_roughnessCoefficientsForTestCase1.at(5));
+            builder.AddDikeProfileSegment(34.0, 7.7, 35.0, 7.4, &_roughnessCoefficientsForTestCase1.at(6));
+            builder.AddDikeProfileSegment(35.0, 7.4, 45.0, 5.0, &_roughnessCoefficientsForTestCase1.at(7));
+            builder.AddDikeProfileSegment(45.0, 5.0, 60.0, 0.5, &_roughnessCoefficientsForTestCase1.at(8));
+            builder.AddDikeProfileSegment(60.0, 0.5, 70.0, 0.5, &_roughnessCoefficientsForTestCase1.at(9));
+
+            builder.AddDikeProfilePointData(5.0, CharacteristicPointType::OuterToe);
+            builder.AddDikeProfilePointData(15.0, CharacteristicPointType::CrestOuterBerm);
+            builder.AddDikeProfilePointData(22.0, CharacteristicPointType::NotchOuterBerm);
+            builder.AddDikeProfilePointData(30.0, CharacteristicPointType::OuterCrest);
+            builder.AddDikeProfilePointData(35.0, CharacteristicPointType::InnerCrest);
+            builder.AddDikeProfilePointData(60.0, CharacteristicPointType::InnerToe);
+        }
+    };
+
+    TEST_F(GrassRevetmentOvertoppingCalculationTest, GivenValidCalculationInput_WhenCalculating_ThenReturnsExpectedCalculationResult)
+    {
+        // Given
+        CalculationInputBuilder builder;
+
+        ConfigureBuilderWithSchematizationForTestCase1(builder);
 
         auto grassRevetmentOvertoppingLocationConstructionProperties = make_unique<GrassRevetmentOvertoppingLocationConstructionProperties>(
             50, GrassRevetmentTopLayerType::ClosedSod);
