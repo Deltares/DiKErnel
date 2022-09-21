93262cd7b61aea26a4eadbb93cdddbf94aa1d9eb
Dennis Tang <dennis.tang@deltares.nl>
2022-09-21 16:16:48 +0200

    DIKERNEL-195: Added validator to validate the roughness coefficient on the segment data

---
+++ DiKErnel/Src/DiKErnel.DomainLibrary/Public/Validators/ProfileDataValidator.cpp
@@ -0,0 +1,38 @@
+﻿// Copyright (C) Stichting Deltares 2022. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+#include "ProfileDataValidator.h"
+
+namespace DiKErnel::DomainLibrary
+{
+    using namespace std;
+    using namespace Util;
+
+    unique_ptr<ValidationIssue> ProfileDataValidator::ValidateRoughnessCoefficient(
+        const double roughnessCoefficient)
+    {
+        if (roughnessCoefficient < 0.5 || roughnessCoefficient > 1)
+        {
+            return make_unique<ValidationIssue>(ValidationIssueType::Warning, "Roughness coefficient should be in range [0.5, 1].");
+        }
+
+        return nullptr;
+    }
+}
---
+++ DiKErnel/Src/DiKErnel.DomainLibrary/Public/Validators/ProfileDataValidator.h
@@ -0,0 +1,45 @@
+﻿// Copyright (C) Stichting Deltares 2022. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+#pragma once
+#include <memory>
+
+#include "ValidationIssue.h"
+
+namespace DiKErnel::DomainLibrary
+{
+    /*!
+     * \brief Class that holds all time step related validation routines.
+     */
+    class ProfileDataValidator
+    {
+        public:
+            /*!
+             * \brief Validates the roughness coefficient.
+             * \param roughnessCoefficient
+             *        The roughness coefficient.
+             *        Unit = [-]
+             * \return A validation issue when the roughness coefficient is not valid; nullptr
+             *         otherwise.
+             */
+            static std::unique_ptr<Util::ValidationIssue> ValidateRoughnessCoefficient(
+                double roughnessCoefficient);
+    };
+}
