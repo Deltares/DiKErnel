02425c828f52524e56d37997658956123fc91839
Robert Tangerman <robert.tangerman@deltares.nl>
2022-09-23 13:41:56 +0200

    Added tests. DIKERNEL-208

---
+++ DiKErnel/Test/DiKErnel.Util.Test/Helpers/NumericsHelperTest.cpp
@@ -0,0 +1,91 @@
+// Copyright (C) Stichting Deltares 2022. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+#include <gtest/gtest.h>
+
+#include "NumericsHelper.h"
+
+namespace DiKErnel::Util::Test
+{
+    using namespace std;
+
+    TEST(NumericsHelperTest, AreEqual_SameValue_ReturnsTrue)
+    {
+        // Call
+        const auto areEqual = NumericsHelper::AreEqual(0.1, 0.1);
+
+        // Assert
+        ASSERT_TRUE(areEqual);
+    }
+
+    TEST(NumericsHelperTest, AreEqual_SecondValueDiffersWithEpsilon_ReturnsTrue)
+    {
+        // Call
+        const auto areEqual = NumericsHelper::AreEqual(0.1, 0.1 + numeric_limits<double>::epsilon());
+
+        // Assert
+        ASSERT_TRUE(areEqual);
+    }
+
+    TEST(NumericsHelperTest, AreEqual_FirstValueDiffersWithEpsilon_ReturnsTrue)
+    {
+        // Call
+        const auto areEqual = NumericsHelper::AreEqual(0.1 + numeric_limits<double>::epsilon(), 0.1);
+
+        // Assert
+        ASSERT_TRUE(areEqual);
+    }
+
+    TEST(NumericsHelperTest, AreEqual_SecondValueDiffersWithTwoTimesEpsilon_ReturnsFalse)
+    {
+        // Call
+        const auto areEqual = NumericsHelper::AreEqual(0.1, 0.1 + numeric_limits<double>::epsilon() * 2);
+
+        // Assert
+        ASSERT_FALSE(areEqual);
+    }
+
+    TEST(NumericsHelperTest, AreEqual_FirstValueDiffersWithTwoTimesEpsilon_ReturnsFalse)
+    {
+        // Call
+        const auto areEqual = NumericsHelper::AreEqual(0.1 + numeric_limits<double>::epsilon() * 2, 0.1);
+
+        // Assert
+        ASSERT_FALSE(areEqual);
+    }
+
+    TEST(NumericsHelperTest, AreEqual_SecondValueLargerThanFirstValue_ReturnsFalse)
+    {
+        // Call
+        const auto areEqual = NumericsHelper::AreEqual(0.1, 0.11);
+
+        // Assert
+        ASSERT_FALSE(areEqual);
+    }
+
+    TEST(NumericsHelperTest, AreEqual_SecondValueSmallerThanFirstValue_ReturnsFalse)
+    {
+        // Call
+        const auto areEqual = NumericsHelper::AreEqual(0.11, 0.1);
+
+        // Assert
+        ASSERT_FALSE(areEqual);
+    }
+}
