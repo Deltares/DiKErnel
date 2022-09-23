648ab8ae4bf315171531ba9c910612a8e972c794
Dennis Tang <dennis.tang@deltares.nl>
2022-09-23 11:30:40 +0200

    DIKERNEL-200: Added specific overtopping validators

---
+++ DiKErnel/Src/DiKErnel.DomainLibrary/Public/Validators/GrassRevetmentOvertopping/GrassRevetmentOvertoppingValidator.h
@@ -0,0 +1,57 @@
+// Copyright (C) Stichting Deltares 2022. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+#pragma once
+
+#include <memory>
+
+#include "ValidationIssue.h"
+
+namespace DiKErnel::DomainLibrary
+{
+    /*!
+     * \brief Class that holds all grass revetment overtopping validation routines.
+     */
+    class GrassRevetmentOvertoppingValidator
+    {
+        public:
+            /*!
+             * \brief Validates the alpha A coefficient.
+             * \param accelerationAlphaA
+             *        The alpha A coefficient.
+             *        Unit = [-]
+             * \return A validation issue when the alpha A coefficient is not valid; nullptr
+             *         otherwise.
+             */
+            static std::unique_ptr<Util::ValidationIssue> AccelerationAlphaA(
+                double accelerationAlphaA);
+
+            /*!
+     * \brief Validates the Cwo coefficient.
+     * \param frontVelocityCwo
+     *        The Cwo coefficient.
+     *        Unit = [-]
+     * \return A validation issue when the Cwo coefficient is not valid; nullptr
+     *         otherwise.
+     */
+            static std::unique_ptr<Util::ValidationIssue> FrontVelocityCwo(
+                double frontVelocityCwo);
+    };
+}
