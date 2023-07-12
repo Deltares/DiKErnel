9e7795fb841381e28a7557297d03bb444ef84004
Dennis Tang <Dennis.Tang@deltares.nl>
2023-07-12 13:39:56 +0200

    DIKERNEL-234: Converted FunctionLibrary:
    - Converted generic functions

---
+++ Src/DiKErnel.FunctionLibrary/GenericFunctions.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify it under the terms of the
+// GNU Lesser General Public License as published by the Free Software Foundation, either
+// version 3 of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with this
+// program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of Stichting
+// Deltares and remain full property of Stichting Deltares at all times. All rights reserved.
+
+using System;
+
+namespace DiKErnel.FunctionLibrary
+{
+    /// <summary>
+    /// Class that holds all generic calculation routines.
+    /// </summary>
+    public class GenericFunctions
+    {
+        /// <summary>
+        /// Converts a degrees value into radians.
+        /// </summary>
+        /// <param name="degrees">The value to convert. [deg]</param>
+        /// <returns>The converted value. [rad]</returns>
+        public static double Radians(double degrees)
+        {
+            return degrees * (Math.PI / 180);
+        }
+
+        /// <summary>
+        /// Converts a radians value into degrees.
+        /// </summary>
+        /// <param name="radians">The value to convert. [rad]</param>
+        /// <returns>The converted value. [deg]</returns>
+        public static double Degrees(double radians)
+        {
+            return radians / (Math.PI / 180);
+        }
+    }
+}
---
+++ Test/DiKErnel.FunctionLibrary.Test/GenericFunctionsTest.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify it under the terms of the
+// GNU Lesser General Public License as published by the Free Software Foundation, either
+// version 3 of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with this
+// program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of Stichting
+// Deltares and remain full property of Stichting Deltares at all times. All rights reserved.
+
+using NUnit.Framework;
+
+namespace DiKErnel.FunctionLibrary.Test
+{
+    [TestFixture]
+    public class GenericFunctionsTest
+    {
+        [Test]
+        public void Radians_ValidInput_ExpectedValue()
+        {
+            // Setup
+            const double degrees = 123.45;
+
+            // Call
+            double radians = GenericFunctions.Radians(degrees);
+
+            // Assert
+            Assert.AreEqual(2.1546089615869999, radians);
+        }
+
+        [Test]
+        public void Degrees_ValidInput_ExpectedValue()
+        {
+            // Setup
+            const double radians = 1.2345;
+
+            // Call 
+            double degrees = GenericFunctions.Degrees(radians);
+
+            // Assert
+            Assert.AreEqual(70.731639808900127, degrees);
+        }
+    }
+}
