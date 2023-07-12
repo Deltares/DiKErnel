0a67ebcf7180115b18140a9796e0a13cc09b7f03
Elmar Kleijn <Elmar.Kleijn@deltares.nl>
2023-07-12 10:33:31 +0200

    Converting DiKErnel.DomainLibrary. DIKERNEL-232

---
+++ Test/DiKErnel.DomainLibrary.Test/Defaults/GrassRevetmentWaveRunup/GrassRevetmentWaveRunupDefaultsTest.cs
@@ -0,0 +1,76 @@
+﻿// Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify it under the terms of the
+// GNU Lesser General Public License as published by the Free Software Foundation, either
+// version 3 of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with this
+// program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of Stichting
+// Deltares and remain full property of Stichting Deltares at all times. All rights reserved.
+
+using DiKErnel.DomainLibrary.Defaults.GrassRevetmentWaveRunup;
+using NUnit.Framework;
+
+namespace DiKErnel.DomainLibrary.Test.Defaults.GrassRevetmentWaveRunup
+{
+    [TestFixture]
+    public class GrassRevetmentWaveRunupDefaultsTest
+    {
+        [Test]
+        public void RepresentativeWaveRunup2PAru_ExpectedValue()
+        {
+            // Call & Assert
+            Assert.AreEqual(1.65, GrassRevetmentWaveRunupDefaults.RepresentativeWaveRunup2PAru);
+        }
+
+        [Test]
+        public void RepresentativeWaveRunup2PBru_ExpectedValue()
+        {
+            // Call & Assert
+            Assert.AreEqual(4.0, GrassRevetmentWaveRunupDefaults.RepresentativeWaveRunup2PBru);
+        }
+
+        [Test]
+        public void RepresentativeWaveRunup2PCru_ExpectedValue()
+        {
+            // Call & Assert
+            Assert.AreEqual(1.5, GrassRevetmentWaveRunupDefaults.RepresentativeWaveRunup2PCru);
+        }
+
+        [Test]
+        public void RepresentativeWaveRunup2PGammab_ExpectedValue()
+        {
+            // Call & Assert
+            Assert.AreEqual(1.0, GrassRevetmentWaveRunupDefaults.RepresentativeWaveRunup2PGammab);
+        }
+
+        [Test]
+        public void RepresentativeWaveRunup2PGammaf_ExpectedValue()
+        {
+            // Call & Assert
+            Assert.AreEqual(1.0, GrassRevetmentWaveRunupDefaults.RepresentativeWaveRunup2PGammaf);
+        }
+
+        [Test]
+        public void WaveAngleImpactAbeta_ExpectedValue()
+        {
+            // Call & Assert
+            Assert.AreEqual(0.0022, GrassRevetmentWaveRunupDefaults.WaveAngleImpactAbeta);
+        }
+
+        [Test]
+        public void WaveAngleImpactBetamax_ExpectedValue()
+        {
+            // Call & Assert
+            Assert.AreEqual(80.0, GrassRevetmentWaveRunupDefaults.WaveAngleImpactBetamax);
+        }
+    }
+}
