f050dd936ff7a6d3c6032e581243a4b86e97806c
Elmar Kleijn <Elmar.Kleijn@deltares.nl>
2023-07-14 13:23:14 +0200

    Removing redundant files. DIKERNEL-231

--- Build/BuildMinGWMakeFilesAndRunTests.bat
+++
@@ -1,5 +0,0 @@
-Powershell.exe -executionpolicy remotesigned -File  BuildMinGWMakeFiles.ps1
-
-cd ../Development/MinGW/Test
-
-start cmd.exe /k for /F %%G in ('dir /b /s *.exe') do %%G --gtest_output=xml:%%~nG.xml
--- Build/HarvestGcovReports.ps1
+++
@@ -1,8 +0,0 @@
-Get-ChildItem -Path . -Filter *.gcda -Recurse -File -Name| ForEach-Object {
-    Write-Host $_
-    start-process C:\msys64\mingw64.exe -ArgumentList "gcov $_ -p" -NoNewWindow -Wait
-}
-
-New-Item -Path ".\.." -Name "Gcov" -ItemType "directory"
-
-Get-ChildItem -Path ".\*.gcov" -Recurse | Move-Item -Destination ".\..\Gcov"
