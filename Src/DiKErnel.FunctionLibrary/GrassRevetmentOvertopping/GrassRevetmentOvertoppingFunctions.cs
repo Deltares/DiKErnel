86291561a6024363cf71858d905ad2a0fb573752
Dennis Tang <Dennis.Tang@deltares.nl>
2023-07-17 17:13:22 +0200

    DIKERNEL-234: Added grass revetment overtopping function:
    - Added functionality and test to calculate the cumulative overload

---
+++ Src/DiKErnel.FunctionLibrary/GrassRevetmentOvertopping/GrassRevetmentOvertoppingFunctions.cs
@@ -0,0 +1,55 @@
+﻿// // Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
+// //
+// // This file is part of DiKErnel.
+// //
+// // DiKErnel is free software: you can redistribute it and/or modify it under the terms of the
+// // GNU Lesser General Public License as published by the Free Software Foundation, either
+// // version 3 of the License, or (at your option) any later version.
+// //
+// // This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// // without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// // See the GNU Lesser General Public License for more details.
+// //
+// // You should have received a copy of the GNU Lesser General Public License along with this
+// // program. If not, see <http://www.gnu.org/licenses/>.
+// //
+// // All names, logos, and references to "Deltares" are registered trademarks of Stichting
+// // Deltares and remain full property of Stichting Deltares at all times. All rights reserved.
+
+using System;
+using DiKErnel.External.Overtopping;
+using DiKErnel.FunctionLibrary.GrassRevetment;
+
+namespace DiKErnel.FunctionLibrary.GrassRevetmentOvertopping
+{
+    /// <summary>
+    /// Class that holds all grass revetment overtopping specific calculation routines.
+    /// </summary>
+    public static class GrassRevetmentOvertoppingFunctions
+    {
+
+        /// <summary>
+        /// Calculates the cumulative overload.
+        /// </summary>
+        /// <param name="input">The input to use for the calculation.</param>
+        /// <returns>The cumulative overload [m^2/s^2].</returns>
+        public static double CumulativeOverload(GrassRevetmentOvertoppingCumulativeOverloadInput input)
+        {
+            Func<double, double> getFrontVelocityFunc = waveRunup => FrontVelocity(waveRunup,
+                                                                                   input.VerticalDistanceWaterLevelElevation,
+                                                                                   input.AccelerationAlphaA,
+                                                                                   input.FrontVelocityCwo,
+                                                                                   input.GravitationalAcceleration);
+
+            return GrassRevetmentFunctions.CumulativeOverload(input, getFrontVelocityFunc);
+        }
+
+        private static double FrontVelocity(double waveRunup, double verticalDistanceWaterLevelElevation,
+                                            double accelerationAlphaA, double frontVelocityCwo, double gravitationalAcceleration)
+        {
+            return frontVelocityCwo * accelerationAlphaA
+                                    * Math.Sqrt(gravitationalAcceleration
+                                                * Math.Max(0.0, waveRunup - verticalDistanceWaterLevelElevation));
+        }
+    }
+}
---
+++ Test/DiKErnel.FunctionLibrary.Test/GrassRevetmentOvertopping/GrassRevetmentOvertoppingFunctionsTest.cs
@@ -0,0 +1,58 @@
+﻿// // Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
+// //
+// // This file is part of DiKErnel.
+// //
+// // DiKErnel is free software: you can redistribute it and/or modify it under the terms of the
+// // GNU Lesser General Public License as published by the Free Software Foundation, either
+// // version 3 of the License, or (at your option) any later version.
+// //
+// // This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// // without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// // See the GNU Lesser General Public License for more details.
+// //
+// // You should have received a copy of the GNU Lesser General Public License along with this
+// // program. If not, see <http://www.gnu.org/licenses/>.
+// //
+// // All names, logos, and references to "Deltares" are registered trademarks of Stichting
+// // Deltares and remain full property of Stichting Deltares at all times. All rights reserved.
+
+using DiKErnel.FunctionLibrary.GrassRevetmentOvertopping;
+using DiKErnel.TestUtil;
+using NUnit.Framework;
+
+namespace DiKErnel.FunctionLibrary.Test.GrassRevetmentOvertopping
+{
+    [TestFixture]
+    public class GrassRevetmentOvertoppingFunctionsTest
+    {
+        [Test]
+        public void CumulativeOverload_ValidInput_ExpectedValue()
+        {
+            // Setup
+            const double averageNumberOfWaves = 832.56;
+            const double representativeWaveRunup2P = 2.602;
+            const int fixedNumberOfWaves = 10000;
+            const double verticalDistanceWaterLevelElevation = 2.2;
+            const double criticalFrontVelocity = 6.6;
+            const double increasedLoadTransitionAlphaM = 1.0;
+            const double reducedStrengthTransitionAlphaS = 1.0;
+            const double gravitationalAcceleration = 9.81;
+            const double accelerationAlphaA = 1.4;
+            const double frontVelocityCwo = 1.45;
+
+            var input = new GrassRevetmentOvertoppingCumulativeOverloadInput(averageNumberOfWaves, representativeWaveRunup2P,
+                                                                             fixedNumberOfWaves, 
+                                                                             verticalDistanceWaterLevelElevation, 
+                                                                             criticalFrontVelocity, increasedLoadTransitionAlphaM, 
+                                                                             reducedStrengthTransitionAlphaS,
+                                                                             gravitationalAcceleration, accelerationAlphaA,
+                                                                             frontVelocityCwo);
+            
+            // Call
+            double cumulativeOverload = GrassRevetmentOvertoppingFunctions.CumulativeOverload(input);
+
+            // Assert
+            AssertHelper.AssertAreEqualWithinTolerance(14.883904828110145, cumulativeOverload);
+        }
+    }
+}
