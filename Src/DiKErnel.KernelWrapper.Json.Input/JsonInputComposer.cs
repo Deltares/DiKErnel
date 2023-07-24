9f4946b9d132e630e9cc6dd4f39d63ec74ceaf18
Ralf Kok <ralf.kok@deltares.nl>
2023-07-24 13:28:06 +0200

    Feedback related changes. DIKERNEL-237

--- Src/DiKErnel.KernelWrapper.Json.Input/JsonInputComposer.cs
+++ Src/DiKErnel.KernelWrapper.Json.Input/JsonInputComposer.cs
@@ -19,12 +19,14 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using DiKErnel.Core.Data;
 using DiKErnel.KernelWrapper.Json.Input.Data.Generic.Converters;
 using DiKErnel.Util;
 using DiKErnel.Util.Validation;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Schema;
 
 namespace DiKErnel.KernelWrapper.Json.Input
@@ -35,11 +37,11 @@ namespace DiKErnel.KernelWrapper.Json.Input
     public class JsonInputComposer
     {
         /// <summary>
-        /// Gets input data based on Json.
+        /// Performs a Json schema based validation.
         /// </summary>
-        /// <param name="filePath">The path to the Json file to get the input from.</param>
-        /// <returns>The result of the operation.</returns>
-        public static DataResult<ICalculationInput> GetInputDataFromJson(string filePath)
+        /// <param name="filePath">The path to the Json file to validate.</param>
+        /// <returns>Whether the validation was successful.</returns>
+        public static bool ValidateJson(string filePath)
         {
             var validationIssues = new List<ValidationIssue>();
             if (!File.Exists(filePath))
@@ -47,35 +49,47 @@ namespace DiKErnel.KernelWrapper.Json.Input
                 validationIssues.Add(new ValidationIssue(ValidationIssueType.Error, "The provided input file does not exist"));
             }
 
-            using (StreamReader file = File.OpenText(filePath))
-            using (var validatingReader = new JSchemaValidatingReader(new JsonTextReader(file)))
+            JObject jObject = JObject.Parse(File.ReadAllText(filePath));
+            bool valid = jObject.IsValid(GetValidatorSchema("DiKErnel.KernelWrapper.Json.Input.Resources.schema_definition.json"),
+                                         out IList<ValidationError> errors);
+            validationIssues.AddRange(errors.Select(validationError =>
+                                                        new ValidationIssue(ValidationIssueType.Error, validationError.Message)).ToList());
+
+            // validationhelper is part of integration
+            //return ValidationHelper.RegisterValidationIssues(validationIssues);
+            return valid;
+        }
+
+        /// <summary>
+        /// Gets input data based on Json.
+        /// </summary>
+        /// <param name="filePath">The path to the Json file to get the input from.</param>
+        /// <returns>The result of the operation.</returns>
+        public static DataResult<ICalculationInput> GetInputDataFromJson(string filePath)
+        {
+            var jsonSerializer = new JsonSerializer();
+            jsonSerializer.Converters.Add(new JsonInputCalculationDataConverter());
+            jsonSerializer.Converters.Add(new JsonInputLocationDataConverter());
+
+            try
             {
-                validatingReader.Schema = GetValidatorSchema("DiKErnel.KernelWrapper.Json.Input.Resources.schema_definition.json");
-                validatingReader.ValidationEventHandler += (obj, schemaValidationEventArgs) =>
-                    validationIssues.Add(new ValidationIssue(ValidationIssueType.Error, schemaValidationEventArgs.Message));
+                JObject jObject = JObject.Parse(File.ReadAllText(filePath));
+                
+                // json input adapter is dependent on the integration project
 
-                var jsonSerializer = new JsonSerializer();
-                jsonSerializer.Converters.Add(new JsonInputCalculationDataConverter());
-                jsonSerializer.Converters.Add(new JsonInputLocationDataConverter());
-
-                try
-                {
-                    // json input adapter is dependent on the integration project
-
-                    // DataResult<ICalculationInput> calculationInputList =
-                    //     JsonInputAdapter.AdaptJsonInputData(jsonSerializer.Deserialize<JsonInputData>(validatingReader));
-                    // ValidationHelper.RegisterValidationIssues(validationIssues);
-                    // return calculationInputList;
-                }
-                catch (Exception e)
-                {
-                    EventRegistry.Register(new Event("An unhandled error occurred while composing calculation data" +
-                                                     $" from the Json input. See stack trace for more information: {e.Message}",
-                                                     EventType.Error));
-                }
-
-                return new DataResult<ICalculationInput>(null, EventRegistry.Flush());
+                // DataResult<ICalculationInput> calculationInputList =
+                //     JsonInputAdapter.AdaptJsonInputData(jObject.ToObject<JsonInputData>(jsonSerializer));
+                // 
+                // return calculationInputList;
             }
+            catch (Exception e)
+            {
+                EventRegistry.Register(new Event("An unhandled error occurred while composing calculation data" +
+                                                 $" from the Json input. See stack trace for more information: {e.Message}",
+                                                 EventType.Error));
+            }
+
+            return new DataResult<ICalculationInput>(null, EventRegistry.Flush());
         }
 
         private static JSchema GetValidatorSchema(string resourceName)
