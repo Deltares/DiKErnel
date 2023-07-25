59ba129033b723b6794ab19092be97d886154a1d
Elmar Kleijn <Elmar.Kleijn@deltares.nl>
2023-07-25 10:23:02 +0200

    Review related changes. DIKERNEL-234

--- Src/DiKErnel.KernelWrapper.Json.Output/Data/Definitions/JsonOutputNaturalStoneRevetmentDefinitions.cs
+++ Src/DiKErnel.KernelWrapper.Json.Output/Data/Definitions/JsonOutputNaturalStoneRevetmentDefinitions.cs
@@ -1,7 +1,7 @@
 // Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
-// 
+//
 // This file is part of DiKErnel.
-// 
+//
 // DiKErnel is free software: you can redistribute it and/or modify it under the terms of the
 // GNU Lesser General Public License as published by the Free Software Foundation, either
 // version 3 of the License, or (at your option) any later version.
@@ -9,10 +9,10 @@
 // This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 // without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 // See the GNU Lesser General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Lesser General Public License along with this
 // program. If not, see <http://www.gnu.org/licenses/>.
-// 
+//
 // All names, logos, and references to "Deltares" are registered trademarks of Stichting
 // Deltares and remain full property of Stichting Deltares at all times. All rights reserved.
 
