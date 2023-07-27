f4b9dc8d91f4c74a4ad619b5e33e8fa50a236861
Elmar Kleijn <Elmar.Kleijn@deltares.nl>
2023-07-27 15:54:04 +0200

    Converting DiKErnel.Integration. DIKERNEL-236

---
+++ Src/DiKErnel.Integration/Data/RevetmentLocationConstructionPropertiesBase.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify it under the terms of the
+// GNU Lesser General Public License as published by the Free Software Foundation, either
+// version 3 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with this
+// program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of Stichting
+// Deltares and remain full property of Stichting Deltares at all times. All rights reserved.
+
+namespace DiKErnel.Integration.Data
+{
+    /// <summary>
+    /// Base construction properties to construct location dependent input.
+    /// </summary>
+    public abstract class RevetmentLocationConstructionPropertiesBase
+    {
+        /// <summary>
+        /// Creates a new instance.
+        /// </summary>
+        /// <param name="x">The x coordinate.</param>
+        protected RevetmentLocationConstructionPropertiesBase(double x)
+        {
+            X = x;
+        }
+
+        /// <summary>
+        /// Gets or sets the initial damage.
+        /// </summary>
+        public double? InitialDamage { get; set; }
+
+        /// <summary>
+        /// Gets or sets the failure number.
+        /// </summary>
+        public double? FailureNumber { get; set; }
+
+        /// <summary>
+        /// Gets the x coordinate.
+        /// </summary>
+        public double X { get; }
+    }
+}
