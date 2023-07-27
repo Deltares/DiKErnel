bf50f8ed9a5295035b8f55bcef506fbae0d58153
Elmar Kleijn <Elmar.Kleijn@deltares.nl>
2023-07-27 13:53:22 +0200

    Converting DiKErnel.Integration. DIKERNEL-236

---
+++ Src/DiKErnel.Integration/Data/AsphaltRevetmentWaveImpact/AsphaltRevetmentWaveImpactLocationDependentOutput.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify it under the terms of the
+// GNU Lesser General Public License as published by the Free Software Foundation, either
+// version 3 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with this
+// program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of Stichting
+// Deltares and remain full property of Stichting Deltares at all times. All rights reserved.
+
+using System.Collections.Generic;
+using DiKErnel.Core.Data;
+
+namespace DiKErnel.Integration.Data.AsphaltRevetmentWaveImpact
+{
+    /// <summary>
+    /// Location dependent output of an asphalt revetment wave impact location.
+    /// </summary>
+    public class AsphaltRevetmentWaveImpactLocationDependentOutput : LocationDependentOutput
+    {
+        /// <summary>
+        /// Creates a new instance.
+        /// </summary>
+        /// <param name="timeDependentOutputItems">The time dependent output items of the
+        /// location.</param>
+        /// <param name="z">The calculated z.</param>
+        /// <param name="outerSlope">The calculated outer slope.</param>
+        public AsphaltRevetmentWaveImpactLocationDependentOutput(
+            IReadOnlyList<TimeDependentOutput> timeDependentOutputItems, double z, double outerSlope)
+            : base(timeDependentOutputItems)
+        {
+            Z = z;
+            OuterSlope = outerSlope;
+        }
+
+        /// <summary>
+        /// Gets the calculated z.
+        /// </summary>
+        public double Z { get; }
+
+        /// <summary>
+        /// Gets the calculated outer slope.
+        /// </summary>
+        public double OuterSlope { get; }
+    }
+}
