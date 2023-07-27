1d8b4620e4b9c51c6570431c4c86a53d62a3868c
Elmar Kleijn <Elmar.Kleijn@deltares.nl>
2023-07-27 08:45:34 +0200

    Converting DiKErnel.Integration. DIKERNEL-236

---
+++ Src/DiKErnel.Integration/Factories/AsphaltRevetmentWaveImpactDefaultsFactory.cs
@@ -0,0 +1,35 @@
+// Copyright (C) Stichting Deltares 2022. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+#pragma once
+
+#include <memory>
+
+#include "IAsphaltRevetmentWaveImpactTopLayerDefaults.h"
+
+namespace DiKErnel::Integration
+{
+    class AsphaltRevetmentWaveImpactDefaultsFactory
+    {
+        public:
+            [[nodiscard]]
+            static std::unique_ptr<DomainLibrary::IAsphaltRevetmentWaveImpactTopLayerDefaults> CreateTopLayerDefaults();
+    };
+}
---
+++ Src/DiKErnel.Integration/Factories/ProfileDataFactorySegment.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (C) Stichting Deltares 2022. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
+
+#pragma once
+
+#include <memory>
+
+namespace DiKErnel::Integration
+{
+    class ProfileDataFactorySegment
+    {
+        public:
+            explicit ProfileDataFactorySegment(
+                double startPointX,
+                double startPointZ,
+                double endPointX,
+                double endPointZ,
+                std::unique_ptr<double> roughnessCoefficient);
+
+            [[nodiscard]]
+            double GetStartPointX() const;
+
+            [[nodiscard]]
+            double GetStartPointZ() const;
+
+            [[nodiscard]]
+            double GetEndPointX() const;
+
+            [[nodiscard]]
+            double GetEndPointZ() const;
+
+            [[nodiscard]]
+            const double* GetRoughnessCoefficient() const;
+
+        private:
+            double _startPointX;
+            double _startPointZ;
+            double _endPointX;
+            double _endPointZ;
+            std::unique_ptr<double> _roughnessCoefficient;
+    };
+}
