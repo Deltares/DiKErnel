9e168a307d9f18cebaed0b15552a283375cd4a00
Elmar Kleijn <Elmar.Kleijn@deltares.nl>
2023-07-31 10:31:08 +0200

    Converting DiKErnel.Integration. DIKERNEL-236

---
+++ Src/DiKErnel.Integration/Data/GrassRevetmentWaveImpact/GrassRevetmentWaveImpactTimeDependentOutputConstructionProperties.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify it under the terms of the
+// GNU Lesser General Public License as published by the Free Software Foundation, either
+// version 3 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with this
+// program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of Stichting
+// Deltares and remain full property of Stichting Deltares at all times. All rights reserved.
+
+using DiKErnel.Core.Data;
+
+namespace DiKErnel.Integration.Data.GrassRevetmentWaveImpact
+{
+    /// <summary>
+    /// Construction properties to construct grass revetment wave impact time dependent
+    /// output.
+    /// </summary>
+    public class GrassRevetmentWaveImpactTimeDependentOutputConstructionProperties
+        : TimeDependentOutputConstructionProperties
+    {
+        /// <summary>
+        /// Gets or sets the loading revetment.
+        /// </summary>
+        public bool? LoadingRevetment { get; set; }
+
+        /// <summary>
+        /// Gets or sets the upper limit loading.
+        /// </summary>
+        public double? UpperLimitLoading { get; set; }
+
+        /// <summary>
+        /// Gets or sets the lower limit loading.
+        /// </summary>
+        public double? LowerLimitLoading { get; set; }
+
+        /// <summary>
+        /// Gets or sets the minimum wave height.
+        /// </summary>
+        public double? MinimumWaveHeight { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum wave height.
+        /// </summary>
+        public double? MaximumWaveHeight { get; set; }
+
+        /// <summary>
+        /// Gets or sets the wave angle impact.
+        /// </summary>
+        public double? WaveAngleImpact { get; set; }
+
+        /// <summary>
+        /// Gets or sets the wave height impact.
+        /// </summary>
+        public double? WaveHeightImpact { get; set; }
+    }
+}
---
+++ Src/DiKErnel.Integration/Data/NaturalStoneRevetment/NaturalStoneRevetmentTimeDependentOutputConstructionProperties.cs
@@ -0,0 +1,119 @@
+﻿// Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify it under the terms of the
+// GNU Lesser General Public License as published by the Free Software Foundation, either
+// version 3 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with this
+// program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of Stichting
+// Deltares and remain full property of Stichting Deltares at all times. All rights reserved.
+
+using DiKErnel.Core.Data;
+
+namespace DiKErnel.Integration.Data.NaturalStoneRevetment
+{
+    /// <summary>
+    /// Construction properties to construct natural stone revetment time dependent output.
+    /// </summary>
+    public class NaturalStoneRevetmentTimeDependentOutputConstructionProperties
+        : TimeDependentOutputConstructionProperties
+    {
+        /// <summary>
+        /// Gets or sets the outer slope.
+        /// </summary>
+        public double? OuterSlope { get; set; }
+
+        /// <summary>
+        /// Gets or sets the slope upper level.
+        /// </summary>
+        public double? SlopeUpperLevel { get; set; }
+
+        /// <summary>
+        /// Gets or sets the slope upper position.
+        /// </summary>
+        public double? SlopeUpperPosition { get; set; }
+
+        /// <summary>
+        /// Gets or sets the slope lower level.
+        /// </summary>
+        public double? SlopeLowerLevel { get; set; }
+
+        /// <summary>
+        /// Gets or sets the slope lower position.
+        /// </summary>
+        public double? SlopeLowerPosition { get; set; }
+
+        /// <summary>
+        /// Gets or sets the loading revetment.
+        /// </summary>
+        public bool? LoadingRevetment { get; set; }
+
+        /// <summary>
+        /// Gets or sets the surf similarity parameter.
+        /// </summary>
+        public double? SurfSimilarityParameter { get; set; }
+
+        /// <summary>
+        /// Gets or sets the wave steepness deep water.
+        /// </summary>
+        public double? WaveSteepnessDeepWater { get; set; }
+
+        /// <summary>
+        /// Gets or sets the upper limit loading.
+        /// </summary>
+        public double? UpperLimitLoading { get; set; }
+
+        /// <summary>
+        /// Gets or sets the lower limit loading.
+        /// </summary>
+        public double? LowerLimitLoading { get; set; }
+
+        /// <summary>
+        /// Gets or sets the depth maximum wave load.
+        /// </summary>
+        public double? DepthMaximumWaveLoad { get; set; }
+
+        /// <summary>
+        /// Gets or sets the distance maximum wave elevation.
+        /// </summary>
+        public double? DistanceMaximumWaveElevation { get; set; }
+
+        /// <summary>
+        /// Gets or sets the normative width of wave impact.
+        /// </summary>
+        public double? NormativeWidthOfWaveImpact { get; set; }
+
+        /// <summary>
+        /// Gets or sets the hydraulic load.
+        /// </summary>
+        public double? HydraulicLoad { get; set; }
+
+        /// <summary>
+        /// Gets or sets the wave angle impact.
+        /// </summary>
+        public double? WaveAngleImpact { get; set; }
+
+        /// <summary>
+        /// Gets or sets the resistance.
+        /// </summary>
+        public double? Resistance { get; set; }
+
+        /// <summary>
+        /// Gets or sets the reference time degradation.
+        /// </summary>
+        public double? ReferenceTimeDegradation { get; set; }
+
+        /// <summary>
+        /// Gets or sets the reference degradation.
+        /// </summary>
+        public double? ReferenceDegradation { get; set; }
+    }
+}
