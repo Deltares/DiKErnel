bf254126361d0993656d3df319315634a7f79a5e
Elmar Kleijn <Elmar.Kleijn@deltares.nl>
2023-07-31 14:10:19 +0200

    Converting DiKErnel.Integration. DIKERNEL-236

---
+++ Src/DiKErnel.Integration/Data/GrassRevetmentWaveRunup/GrassRevetmentWaveRunupRayleighLocationConstructionProperties.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify it under the terms of the
+// GNU Lesser General Public License as published by the Free Software Foundation, either
+// version 3 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with this
+// program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of Stichting
+// Deltares and remain full property of Stichting Deltares at all times. All rights reserved.
+
+using DiKErnel.Integration.Data.GrassRevetment;
+
+namespace DiKErnel.Integration.Data.GrassRevetmentWaveRunup
+{
+    /// <summary>
+    /// Construction properties to construct grass revetment wave run-up location dependent
+    /// input for the Rayleigh protocol.
+    /// </summary>
+    public class GrassRevetmentWaveRunupRayleighLocationConstructionProperties : GrassRevetmentWaveRunupLocationConstructionProperties
+    {
+        /// <inheritdoc/>
+        public GrassRevetmentWaveRunupRayleighLocationConstructionProperties(double x, double outerSlope,
+                                                                             GrassRevetmentTopLayerType topLayerType)
+            : base(x, outerSlope, topLayerType) {}
+
+        /// <summary>
+        /// Gets or sets the fixed number of waves.
+        /// </summary>
+        public int? FixedNumberOfWaves { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Cu coefficient.
+        /// </summary>
+        public double? FrontVelocityCu { get; set; }
+    }
+}
