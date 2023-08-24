18972a4f4f5799e9a8385893285f9631eb840604
Elmar Kleijn <Elmar.Kleijn@deltares.nl>
2023-08-24 16:22:02 +0200

    Converting DiKErnel.Integration. DIKERNEL-236

--- Test/DiKErnel.Integration.Test/Data/GrassRevetmentWaveRunupRayleighLocationConstructionPropertiesTest.cs
+++ Test/DiKErnel.Integration.Test/Data/GrassRevetmentWaveRunupRayleighLocationConstructionPropertiesTest.cs
@@ -16,6 +16,9 @@
 // All names, logos, and references to "Deltares" are registered trademarks of Stichting
 // Deltares and remain full property of Stichting Deltares at all times. All rights reserved.
 
+using DiKErnel.Integration.Data.GrassRevetment;
+using DiKErnel.Integration.Data.GrassRevetmentWaveRunup;
+using DiKErnel.TestUtil;
 using NUnit.Framework;
 
 namespace DiKErnel.Integration.Test.Data
@@ -23,105 +26,104 @@ namespace DiKErnel.Integration.Test.Data
     [TestFixture]
     public class GrassRevetmentWaveRunupRayleighLocationConstructionPropertiesTest
     {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            var topLayerType = Random.NextEnumValue<GrassRevetmentTopLayerType>();
+            double x = Random.NextDouble();
+            double outerSlope = Random.NextDouble();
+
+            // Call
+            var constructionProperties = new GrassRevetmentWaveRunupRayleighLocationConstructionProperties(x, outerSlope, topLayerType);
+
+            // Assert
+            Assert.IsInstanceOf<GrassRevetmentWaveRunupLocationConstructionProperties>(constructionProperties);
+            Assert.AreEqual(topLayerType, constructionProperties.TopLayerType);
+            Assert.AreEqual(x, constructionProperties.X);
+            Assert.AreEqual(outerSlope, constructionProperties.OuterSlope);
+            Assert.IsNull(constructionProperties.InitialDamage);
+            Assert.IsNull(constructionProperties.FailureNumber);
+            Assert.IsNull(constructionProperties.CriticalCumulativeOverload);
+            Assert.IsNull(constructionProperties.CriticalFrontVelocity);
+            Assert.IsNull(constructionProperties.IncreasedLoadTransitionAlphaM);
+            Assert.IsNull(constructionProperties.ReducedStrengthTransitionAlphaS);
+            Assert.IsNull(constructionProperties.AverageNumberOfWavesCtm);
+            Assert.IsNull(constructionProperties.RepresentativeWaveRunup2PAru);
+            Assert.IsNull(constructionProperties.RepresentativeWaveRunup2PBru);
+            Assert.IsNull(constructionProperties.RepresentativeWaveRunup2PCru);
+            Assert.IsNull(constructionProperties.RepresentativeWaveRunup2PGammab);
+            Assert.IsNull(constructionProperties.RepresentativeWaveRunup2PGammaf);
+            Assert.IsNull(constructionProperties.WaveAngleImpactAbeta);
+            Assert.IsNull(constructionProperties.WaveAngleImpactBetamax);
+            Assert.IsNull(constructionProperties.FixedNumberOfWaves);
+            Assert.IsNull(constructionProperties.FrontVelocityCu);
+        }
 
         [Test]
-    public void Constructor_ExpectedValues()
-    {
-        // Setup
-        var topLayerType = GrassRevetmentTopLayerType::OpenSod;
-        var x = 0.1;
-        var outerSlope = 0.2;
+        public void GivenConstructionProperties_WhenAllInputSet_ThenExpectedValues()
+        {
+            // Given
+            var topLayerType = Random.NextEnumValue<GrassRevetmentTopLayerType>();
+            double x = Random.NextDouble();
+            double outerSlope = Random.NextDouble();
+            double initialDamage = Random.NextDouble();
+            double failureNumber = Random.NextDouble();
+            double criticalCumulativeOverload = Random.NextDouble();
+            double criticalFrontVelocity = Random.NextDouble();
+            double increasedLoadTransitionAlphaM = Random.NextDouble();
+            double reducedStrengthTransitionAlphaS = Random.NextDouble();
+            double averageNumberOfWavesCtm = Random.NextDouble();
+            double representativeWaveRunup2PAru = Random.NextDouble();
+            double representativeWaveRunup2PBru = Random.NextDouble();
+            double representativeWaveRunup2PCru = Random.NextDouble();
+            double representativeWaveRunup2PGammab = Random.NextDouble();
+            double representativeWaveRunup2PGammaf = Random.NextDouble();
+            double waveAngleImpactAbeta = Random.NextDouble();
+            double waveAngleImpactBetamax = Random.NextDouble();
+            int fixedNumberOfWaves = Random.Next();
+            double frontVelocityCu = Random.NextDouble();
 
-        // Call
-        const GrassRevetmentWaveRunupRayleighLocationConstructionProperties constructionProperties(x, outerSlope, topLayerType);
+            var constructionProperties = new GrassRevetmentWaveRunupRayleighLocationConstructionProperties(x, outerSlope, topLayerType);
 
-        // Assert
-        Assert.IsInstanceOf<GrassRevetmentWaveRunupLocationConstructionProperties>(constructionProperties);
-        Assert.AreEqual(topLayerType, constructionProperties.GetTopLayerType());
-        Assert.AreEqual(x, constructionProperties.GetX());
-        Assert.AreEqual(outerSlope, constructionProperties.GetOuterSlope());
-        Assert.IsNull(constructionProperties.GetInitialDamage());
-        Assert.IsNull(constructionProperties.GetFailureNumber());
-        Assert.IsNull(constructionProperties.GetCriticalCumulativeOverload());
-        Assert.IsNull(constructionProperties.GetCriticalFrontVelocity());
-        Assert.IsNull(constructionProperties.GetIncreasedLoadTransitionAlphaM());
-        Assert.IsNull(constructionProperties.GetReducedStrengthTransitionAlphaS());
-        Assert.IsNull(constructionProperties.GetAverageNumberOfWavesCtm());
-        Assert.IsNull(constructionProperties.GetRepresentativeWaveRunup2PAru());
-        Assert.IsNull(constructionProperties.GetRepresentativeWaveRunup2PBru());
-        Assert.IsNull(constructionProperties.GetRepresentativeWaveRunup2PCru());
-        Assert.IsNull(constructionProperties.GetRepresentativeWaveRunup2PGammab());
-        Assert.IsNull(constructionProperties.GetRepresentativeWaveRunup2PGammaf());
-        Assert.IsNull(constructionProperties.GetWaveAngleImpactAbeta());
-        Assert.IsNull(constructionProperties.GetWaveAngleImpactBetamax());
-        Assert.IsNull(constructionProperties.GetFixedNumberOfWaves());
-        Assert.IsNull(constructionProperties.GetFrontVelocityCu());
+            // When
+            constructionProperties.InitialDamage = initialDamage;
+            constructionProperties.FailureNumber = failureNumber;
+            constructionProperties.CriticalCumulativeOverload = criticalCumulativeOverload;
+            constructionProperties.CriticalFrontVelocity = criticalFrontVelocity;
+            constructionProperties.IncreasedLoadTransitionAlphaM = increasedLoadTransitionAlphaM;
+            constructionProperties.ReducedStrengthTransitionAlphaS = reducedStrengthTransitionAlphaS;
+            constructionProperties.AverageNumberOfWavesCtm = averageNumberOfWavesCtm;
+            constructionProperties.RepresentativeWaveRunup2PAru = representativeWaveRunup2PAru;
+            constructionProperties.RepresentativeWaveRunup2PBru = representativeWaveRunup2PBru;
+            constructionProperties.RepresentativeWaveRunup2PCru = representativeWaveRunup2PCru;
+            constructionProperties.RepresentativeWaveRunup2PGammab = representativeWaveRunup2PGammab;
+            constructionProperties.RepresentativeWaveRunup2PGammaf = representativeWaveRunup2PGammaf;
+            constructionProperties.WaveAngleImpactAbeta = waveAngleImpactAbeta;
+            constructionProperties.WaveAngleImpactBetamax = waveAngleImpactBetamax;
+            constructionProperties.FixedNumberOfWaves = fixedNumberOfWaves;
+            constructionProperties.FrontVelocityCu = frontVelocityCu;
+
+            // Then
+            Assert.AreEqual(topLayerType, constructionProperties.TopLayerType);
+            Assert.AreEqual(x, constructionProperties.X);
+            Assert.AreEqual(outerSlope, constructionProperties.OuterSlope);
+            Assert.AreEqual(initialDamage, constructionProperties.InitialDamage);
+            Assert.AreEqual(failureNumber, constructionProperties.FailureNumber);
+            Assert.AreEqual(criticalCumulativeOverload, constructionProperties.CriticalCumulativeOverload);
+            Assert.AreEqual(criticalFrontVelocity, constructionProperties.CriticalFrontVelocity);
+            Assert.AreEqual(increasedLoadTransitionAlphaM, constructionProperties.IncreasedLoadTransitionAlphaM);
+            Assert.AreEqual(reducedStrengthTransitionAlphaS, constructionProperties.ReducedStrengthTransitionAlphaS);
+            Assert.AreEqual(averageNumberOfWavesCtm, constructionProperties.AverageNumberOfWavesCtm);
+            Assert.AreEqual(representativeWaveRunup2PAru, constructionProperties.RepresentativeWaveRunup2PAru);
+            Assert.AreEqual(representativeWaveRunup2PBru, constructionProperties.RepresentativeWaveRunup2PBru);
+            Assert.AreEqual(representativeWaveRunup2PCru, constructionProperties.RepresentativeWaveRunup2PCru);
+            Assert.AreEqual(representativeWaveRunup2PGammab, constructionProperties.RepresentativeWaveRunup2PGammab);
+            Assert.AreEqual(representativeWaveRunup2PGammaf, constructionProperties.RepresentativeWaveRunup2PGammaf);
+            Assert.AreEqual(waveAngleImpactAbeta, constructionProperties.WaveAngleImpactAbeta);
+            Assert.AreEqual(waveAngleImpactBetamax, constructionProperties.WaveAngleImpactBetamax);
+            Assert.AreEqual(fixedNumberOfWaves, constructionProperties.FixedNumberOfWaves);
+            Assert.AreEqual(frontVelocityCu, constructionProperties.FrontVelocityCu);
+        }
     }
-
-        [Test]
-    public void GivenConstructionProperties_WhenAllInputSet_ThenExpectedValues()
-    {
-        // Given
-        var topLayerType = GrassRevetmentTopLayerType::OpenSod;
-        var x = 0.1;
-        var outerSlope = 0.2;
-        var initialDamage = 0.3;
-        var failureNumber = 0.4;
-        var criticalCumulativeOverload = 0.5;
-        var criticalFrontVelocity = 0.6;
-        var increasedLoadTransitionAlphaM = 0.7;
-        var reducedStrengthTransitionAlphaS = 0.8;
-        var averageNumberOfWavesCtm = 0.9;
-        var representativeWaveRunup2PAru = 1.0;
-        var representativeWaveRunup2PBru = 1.1;
-        var representativeWaveRunup2PCru = 1.2;
-        var representativeWaveRunup2PGammab = 1.3;
-        var representativeWaveRunup2PGammaf = 1.4;
-        var waveAngleImpactAbeta = 1.5;
-        var waveAngleImpactBetamax = 1.6;
-        var fixedNumberOfWaves = 17;
-        var frontVelocityCu = 1.8;
-
-        GrassRevetmentWaveRunupRayleighLocationConstructionProperties constructionProperties(x, outerSlope, topLayerType);
-
-        // When
-        constructionProperties.SetInitialDamage(initialDamage));
-        constructionProperties.SetFailureNumber(failureNumber));
-        constructionProperties.SetCriticalCumulativeOverload(criticalCumulativeOverload));
-        constructionProperties.SetCriticalFrontVelocity(criticalFrontVelocity));
-        constructionProperties.SetIncreasedLoadTransitionAlphaM(increasedLoadTransitionAlphaM));
-        constructionProperties.SetReducedStrengthTransitionAlphaS(reducedStrengthTransitionAlphaS));
-        constructionProperties.SetAverageNumberOfWavesCtm(averageNumberOfWavesCtm));
-        constructionProperties.SetRepresentativeWaveRunup2PAru(representativeWaveRunup2PAru));
-        constructionProperties.SetRepresentativeWaveRunup2PBru(representativeWaveRunup2PBru));
-        constructionProperties.SetRepresentativeWaveRunup2PCru(representativeWaveRunup2PCru));
-        constructionProperties.SetRepresentativeWaveRunup2PGammab(representativeWaveRunup2PGammab));
-        constructionProperties.SetRepresentativeWaveRunup2PGammaf(representativeWaveRunup2PGammaf));
-        constructionProperties.SetWaveAngleImpactAbeta(waveAngleImpactAbeta));
-        constructionProperties.SetWaveAngleImpactBetamax(waveAngleImpactBetamax));
-        constructionProperties.SetFixedNumberOfWaves(fixedNumberOfWaves));
-        constructionProperties.SetFrontVelocityCu(frontVelocityCu));
-
-        // Then
-        Assert.AreEqual(topLayerType, constructionProperties.GetTopLayerType());
-        Assert.AreEqual(x, constructionProperties.GetX());
-        Assert.AreEqual(outerSlope, constructionProperties.GetOuterSlope());
-        Assert.AreEqual(initialDamage, constructionProperties.GetInitialDamage());
-        Assert.AreEqual(failureNumber, constructionProperties.GetFailureNumber());
-        Assert.AreEqual(criticalCumulativeOverload, constructionProperties.GetCriticalCumulativeOverload());
-        Assert.AreEqual(criticalFrontVelocity, constructionProperties.GetCriticalFrontVelocity());
-        Assert.AreEqual(increasedLoadTransitionAlphaM, constructionProperties.GetIncreasedLoadTransitionAlphaM());
-        Assert.AreEqual(reducedStrengthTransitionAlphaS, constructionProperties.GetReducedStrengthTransitionAlphaS());
-        Assert.AreEqual(averageNumberOfWavesCtm, constructionProperties.GetAverageNumberOfWavesCtm());
-        Assert.AreEqual(representativeWaveRunup2PAru, constructionProperties.GetRepresentativeWaveRunup2PAru());
-        Assert.AreEqual(representativeWaveRunup2PBru, constructionProperties.GetRepresentativeWaveRunup2PBru());
-        Assert.AreEqual(representativeWaveRunup2PCru, constructionProperties.GetRepresentativeWaveRunup2PCru());
-        Assert.AreEqual(representativeWaveRunup2PGammab, constructionProperties.GetRepresentativeWaveRunup2PGammab());
-        Assert.AreEqual(representativeWaveRunup2PGammaf, constructionProperties.GetRepresentativeWaveRunup2PGammaf());
-        Assert.AreEqual(waveAngleImpactAbeta, constructionProperties.GetWaveAngleImpactAbeta());
-        Assert.AreEqual(waveAngleImpactBetamax, constructionProperties.GetWaveAngleImpactBetamax());
-        ASSERT_EQ(fixedNumberOfWaves, constructionProperties.GetFixedNumberOfWaves());
-        Assert.AreEqual(frontVelocityCu, constructionProperties.GetFrontVelocityCu());
-    }
-}
 }
