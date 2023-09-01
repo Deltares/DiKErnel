3215361856b73f96fdb6990689d9ab23dce0c81a
Elmar Kleijn <Elmar.Kleijn@deltares.nl>
2023-09-01 09:24:07 +0200

    Converting DiKErnel.KernelWrapper.Json.Input. DIKERNEL-241

---
+++ Src/DiKErnel.KernelWrapper.Json.Output/Exceptions/JsonOutputConversionException.cs
@@ -0,0 +1,34 @@
+// Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// DiKErnel is free software: you can redistribute it and/or modify it under the terms of the
+// GNU Lesser General Public License as published by the Free Software Foundation, either
+// version 3 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with this
+// program. If not, see <http://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of Stichting
+// Deltares and remain full property of Stichting Deltares at all times. All rights reserved.
+
+using System;
+
+namespace DiKErnel.KernelWrapper.Json.Output.Exceptions
+{
+    /// <summary>
+    /// Exception that can be thrown when Json output cannot be created.
+    /// </summary>
+    public class JsonOutputCreationException : Exception
+    {
+        /// <summary>
+        /// Creates a new instance.
+        /// </summary>
+        /// <param name="message">The message of the exception.</param>
+        public JsonOutputCreationException(string message) : base(message) {}
+    }
+}
