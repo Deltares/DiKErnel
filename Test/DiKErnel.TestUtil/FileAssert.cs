d9fb82534d370d22d5e5a0231d7d8c312cd287ce
Joël van den Berg <Joel.vandenBerg@deltares.nl>
2023-09-04 11:48:16 +0200

    Update headers with new format of license info DIKERNEL-246

--- Test/DiKErnel.TestUtil/FileAssert.cs
+++ Test/DiKErnel.TestUtil/FileAssert.cs
@@ -2,19 +2,22 @@
 //
 // This file is part of DiKErnel.
 //
-// DiKErnel is free software: you can redistribute it and/or modify it under the terms of the
-// GNU Lesser General Public License as published by the Free Software Foundation, either
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
 // version 3 of the License, or (at your option) any later version.
 //
-// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-// See the GNU Lesser General Public License for more details.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
 //
-// You should have received a copy of the GNU Lesser General Public License along with this
-// program. If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <https://www.gnu.org/licenses/>.
 //
-// All names, logos, and references to "Deltares" are registered trademarks of Stichting
-// Deltares and remain full property of Stichting Deltares at all times. All rights reserved.
+// All names, logos, and references to "Deltares" are registered trademarks of
+// Stichting Deltares and remain full property of Stichting Deltares at all times.
+// All rights reserved.
 
 using System.IO;
 using NUnit.Framework;
