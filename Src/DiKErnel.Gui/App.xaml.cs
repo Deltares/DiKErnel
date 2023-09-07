68fe77946e912dca8a0b4fd134a60e3963a6b8ae
Elmar Kleijn <Elmar.Kleijn@deltares.nl>
2023-09-07 10:07:06 +0200

    Converting DiKErnel.Gui. DIKERNEL-235

--- Src/DiKErnel.Gui/App.xaml.cs
+++ Src/DiKErnel.Gui/App.xaml.cs
@@ -24,5 +24,5 @@ namespace DiKErnel.Gui
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : System.Windows.Application {}
+    public partial class App {}
 }
--- Src/DiKErnel.Gui/View/TextType.cs
+++ Src/DiKErnel.Gui/View/TextType.cs
@@ -19,13 +19,12 @@
 // Stichting Deltares and remain full property of Stichting Deltares at all times.
 // All rights reserved.
 
-using System.Windows;
-
-[assembly: ThemeInfo(
-    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
-    //(used if a resource is not found in the page,
-    // or application resource dictionaries)
-    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-    //(used if a resource is not found in the page,
-    // app, or any theme specific resource dictionaries)
-)]
+namespace DiKErnel.Gui.View
+{
+    public enum TextType
+    {
+        Bold,
+        Italic,
+        Normal
+    }
+}
