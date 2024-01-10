3868a7b9eb3fdc7412eb1f5267bdc3239066873a
Elmar Kleijn <Elmar.Kleijn@deltares.nl>
2024-01-10 10:55:01 +0100

    Revert "Integrating IProtocol. DIKERNEL-392"
    This reverts commit a69315879a9a6ac29ff418a91aaea869fe857b8a.

--- Src/DiKErnel.Integration/Protocols/Protocol.cs
+++ Src/DiKErnel.Integration/Protocols/Protocol.cs
@@ -54,7 +54,7 @@ namespace DiKErnel.Integration.Protocols
                 InitializeDerivedLocationDependentInput(locationDependentInput, profileData);
             }
 
-            return CalculateTimeDependentOutput(initialDamage, locationDependentInput, timeDependentInput, profileData);
+            return CalculateTimeDependentOutput(initialDamage, timeDependentInput, profileData);
         }
 
         public abstract LocationDependentOutput GetLocationDependentOutput(
@@ -67,7 +67,6 @@ namespace DiKErnel.Integration.Protocols
         }
 
         protected abstract TimeDependentOutput CalculateTimeDependentOutput(double initialDamage,
-                                                                            ILocationDependentInput locationDependentInput,
                                                                             ITimeDependentInput timeDependentInput,
                                                                             IProfileData profileData);
     }
