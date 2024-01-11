8f9c25a9da274cd5a22c192b20d9f0bda5dec1ea
Elmar Kleijn <Elmar.Kleijn@deltares.nl>
2024-01-11 10:27:47 +0100

    Removing redundant calculation routines. DIKERNEL-392

--- Src/DiKErnel.FunctionLibrary/GrassRevetmentWaveRunup/GrassRevetmentWaveRunupFunctions.cs
+++ Src/DiKErnel.FunctionLibrary/GrassRevetmentWaveRunup/GrassRevetmentWaveRunupFunctions.cs
@@ -26,45 +26,6 @@ namespace DiKErnel.FunctionLibrary.GrassRevetmentWaveRunup
     public static class GrassRevetmentWaveRunupFunctions
     {
         /// <summary>
-        /// Calculates the representative wave run-up (2 percent).
-        /// </summary>
-        /// <param name="input">The input to use for the calculation.</param>
-        /// <returns>The representative wave run-up (2 percent) [m].</returns>
-        public static double RepresentativeWaveRunup2P(GrassRevetmentWaveRunupRepresentative2PInput input)
-        {
-            double absoluteWaveAngle = Math.Abs(input.WaveAngle);
-
-            if (absoluteWaveAngle >= 110.0)
-            {
-                return 0.0;
-            }
-
-            double effectiveWaveHeightHm0 = input.WaveHeightHm0;
-            double effectiveWavePeriodTm10 = input.WavePeriodTm10;
-
-            if (absoluteWaveAngle > 80.0)
-            {
-                double correctionFactor = WaveCorrectionFactor(absoluteWaveAngle);
-
-                effectiveWaveHeightHm0 *= correctionFactor;
-                effectiveWavePeriodTm10 *= Math.Sqrt(correctionFactor);
-            }
-
-            double surfSimilarityParameter = HydraulicLoadFunctions.SurfSimilarityParameter(
-                input.OuterSlope, effectiveWaveHeightHm0, effectiveWavePeriodTm10,
-                input.GravitationalAcceleration);
-
-            return effectiveWaveHeightHm0
-                   * Math.Min(input.RepresentativeWaveRunup2PAru * input.RepresentativeWaveRunup2PGammab
-                                                                 * input.RepresentativeWaveRunup2PGammaf
-                                                                 * input.WaveAngleImpact * surfSimilarityParameter,
-                              input.RepresentativeWaveRunup2PGammaf
-                              * input.WaveAngleImpact
-                              * (input.RepresentativeWaveRunup2PBru - input.RepresentativeWaveRunup2PCru
-                                 / Math.Sqrt(surfSimilarityParameter)));
-        }
-
-        /// <summary>
         /// Calculates the wave impact with respect to the wave angle.
         /// </summary>
         /// <param name="waveAngle">The wave angle [deg].</param>
@@ -74,11 +35,6 @@ namespace DiKErnel.FunctionLibrary.GrassRevetmentWaveRunup
         public static double WaveAngleImpact(double waveAngle, double waveAngleImpactAbeta, double waveAngleImpactBetamax)
         {
             return 1 - waveAngleImpactAbeta * Math.Min(Math.Abs(waveAngle), waveAngleImpactBetamax);
-        }
-
-        private static double WaveCorrectionFactor(double absoluteWaveAngle)
-        {
-            return (110.0 - absoluteWaveAngle) / 30.0;
         }
     }
 }
