babca7cc32fcaa63dcb9dc4fe97b65f777107db5
Dennis Tang <Dennis.Tang@deltares.nl>
2024-01-16 09:18:57 +0100

    DIKERNEL-381: Added lamda functions for Ru1 and Ru2

--- Src/DiKErnel.FunctionLibrary/GrassRevetmentWaveRunup/GrassRevetmentWaveRunupBattjesGroenendijkAnalyticalFunctions.cs
+++ Src/DiKErnel.FunctionLibrary/GrassRevetmentWaveRunup/GrassRevetmentWaveRunupBattjesGroenendijkAnalyticalFunctions.cs
@@ -17,8 +17,11 @@
 // Deltares and remain full property of Stichting Deltares at all times. All rights reserved.
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using MathNet.Numerics;
 using MathNet.Numerics.Distributions;
+using MathNet.Numerics.Interpolation;
 
 namespace DiKErnel.FunctionLibrary.GrassRevetmentWaveRunup
 {
@@ -28,6 +31,134 @@ namespace DiKErnel.FunctionLibrary.GrassRevetmentWaveRunup
     /// </summary>
     public static class GrassRevetmentWaveRunupBattjesGroenendijkAnalyticalFunctions
     {
+        private static readonly IReadOnlyList<(double, double)> lambdasRu1 = new[]
+        {
+            (0.05, 12.193),
+            (0.10, 7.003),
+            (0.15, 5.063),
+            (0.20, 4.022),
+            (0.25, 3.365),
+            (0.30, 2.908),
+            (0.35, 2.571),
+            (0.40, 2.311),
+            (0.45, 2.104),
+            (0.50, 1.936),
+            (0.55, 1.796),
+            (0.60, 1.678),
+            (0.65, 1.578),
+            (0.70, 1.492),
+            (0.75, 1.419),
+            (0.80, 1.356),
+            (0.85, 1.302),
+            (0.90, 1.256),
+            (0.95, 1.216),
+            (1.00, 1.182),
+            (1.05, 1.153),
+            (1.10, 1.128),
+            (1.15, 1.108),
+            (1.20, 1.090),
+            (1.25, 1.075),
+            (1.30, 1.063),
+            (1.35, 1.052),
+            (1.40, 1.043),
+            (1.45, 1.036),
+            (1.50, 1.030),
+            (1.55, 1.024),
+            (1.60, 1.020),
+            (1.65, 1.016),
+            (1.70, 1.013),
+            (1.75, 1.011),
+            (1.80, 1.009),
+            (1.85, 1.007),
+            (1.90, 1.006),
+            (1.95, 1.004),
+            (2.00, 1.004),
+            (2.05, 1.003),
+            (2.10, 1.002),
+            (2.15, 1.002),
+            (2.20, 1.001),
+            (2.25, 1.001),
+            (2.30, 1.001),
+            (2.35, 1.001),
+            (2.40, 1.000),
+            (2.45, 1.000),
+            (2.50, 1.000),
+            (2.55, 1.000),
+            (2.60, 1.000),
+            (2.65, 1.000),
+            (2.70, 1.000),
+            (2.75, 1.000),
+            (2.80, 1.000),
+            (2.85, 1.000),
+            (2.90, 1.000),
+            (2.95, 1.000),
+            (3.00, 1.000)
+        };
+
+        private static readonly IReadOnlyList<(double, double)> lambdasRu2 = new[]
+        {
+            (0.05, 1.060),
+            (0.10, 1.060),
+            (0.15, 1.060),
+            (0.20, 1.060),
+            (0.25, 1.060),
+            (0.30, 1.060),
+            (0.35, 1.060),
+            (0.40, 1.060),
+            (0.45, 1.060),
+            (0.50, 1.061),
+            (0.55, 1.061),
+            (0.60, 1.062),
+            (0.65, 1.064),
+            (0.70, 1.066),
+            (0.75, 1.069),
+            (0.80, 1.073),
+            (0.85, 1.077),
+            (0.90, 1.083),
+            (0.95, 1.090),
+            (1.00, 1.097),
+            (1.05, 1.106),
+            (1.10, 1.116),
+            (1.15, 1.126),
+            (1.20, 1.138),
+            (1.25, 1.150),
+            (1.30, 1.162),
+            (1.35, 1.175),
+            (1.40, 1.189),
+            (1.45, 1.203),
+            (1.50, 1.217),
+            (1.55, 1.231),
+            (1.60, 1.246),
+            (1.65, 1.261),
+            (1.70, 1.275),
+            (1.75, 1.290),
+            (1.80, 1.305),
+            (1.85, 1.320),
+            (1.90, 1.334),
+            (1.95, 1.349),
+            (2.00, 1.363),
+            (2.05, 1.378),
+            (2.10, 1.392),
+            (2.15, 1.407),
+            (2.20, 1.421),
+            (2.25, 1.435),
+            (2.30, 1.449),
+            (2.35, 1.462),
+            (2.40, 1.476),
+            (2.45, 1.490),
+            (2.50, 1.503),
+            (2.55, 1.516),
+            (2.60, 1.529),
+            (2.65, 1.542),
+            (2.70, 1.555),
+            (2.75, 1.568),
+            (2.80, 1.580),
+            (2.85, 1.593),
+            (2.90, 1.605),
+            (2.95, 1.617),
+            (3.00, 1.630)
+        };
+
         public static double CumulativeOverload()
         {
             return double.NaN;
@@ -38,6 +169,28 @@ namespace DiKErnel.FunctionLibrary.GrassRevetmentWaveRunup
             double probability = Gamma.CDF(1 + 1 / xi, 1, Math.Pow(eta, xi));
             return (SpecialFunctions.Gamma(1 + 1 / xi) * probability) / SpecialFunctions.Gamma(2 + 1 / xi) +
                    (SpecialFunctions.Gamma(1 + 1 / xi) * probability) / (xi * SpecialFunctions.Gamma(2 + 1 / xi));
+        }
+
+        private static double LambdaRu1(double kappa)
+        {
+            if (kappa > 3.0)
+            {
+                return 1.0;
+            }
+
+            IInterpolation interpolator = Interpolate.Linear(lambdasRu1.Select(x => x.Item1), lambdasRu1.Select(x => x.Item2));
+            return interpolator.Interpolate(kappa);
+        }
+
+        private static double LambdaRu2(double kappa)
+        {
+            if (kappa > 3.0)
+            {
+                return Math.Pow(kappa, 4.0 / 9.0);
+            }
+
+            IInterpolation interpolator = Interpolate.Linear(lambdasRu2.Select(x => x.Item1), lambdasRu2.Select(x => x.Item2));
+            return interpolator.Interpolate(kappa);
         }
     }
 }
