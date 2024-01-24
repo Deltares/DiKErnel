56e4dea637a0fe59f951b4bb6869d3c18e48a2f2
Sylvia <sylvia.vanduijn@deltares.nl>
2024-01-24 14:42:24 +0100

    Created step definitions. DIKERNEL-382

---
+++ Test/DiKErnel.SpecFlow.Test/Steps/BattjesGroenendijkAnalyticalStepDefinitions.cs
@@ -0,0 +1,50 @@
+﻿using TechTalk.SpecFlow;
+
+namespace DiKErnel.SpecFlow.Test.Steps
+{
+    [Binding]
+    public class BattjesGroenendijkAnalyticalStepDefinitions
+    {
+        [Given(@"the following tijdstippen:")]
+        public void GivenTheFollowingTijdstippen(Table table)
+        {
+            ScenarioContext.StepIsPending();
+        }
+
+        [Given(@"the following hydraulischeBelastingen:")]
+        public void GivenTheFollowingHydraulischeBelastingen(Table table)
+        {
+            ScenarioContext.StepIsPending();
+        }
+
+        [Given(@"the following dijkprofiel:")]
+        public void GivenTheFollowingDijkprofiel(Table table)
+        {
+            ScenarioContext.StepIsPending();
+        }
+
+        [Given(@"the following locaties:")]
+        public void GivenTheFollowingLocaties(Table table)
+        {
+            ScenarioContext.StepIsPending();
+        }
+
+        [Given(@"the following rekenmethoden:")]
+        public void GivenTheFollowingRekenmethoden(Table table)
+        {
+            ScenarioContext.StepIsPending();
+        }
+
+        [When(@"I run the calculation")]
+        public void WhenIRunTheCalculation()
+        {
+            ScenarioContext.StepIsPending();
+        }
+
+        [Given(@"the following is adjusted:")]
+        public void GivenTheFollowingIsAdjusted(Table table)
+        {
+            ScenarioContext.StepIsPending();
+        }
+    }
+}
