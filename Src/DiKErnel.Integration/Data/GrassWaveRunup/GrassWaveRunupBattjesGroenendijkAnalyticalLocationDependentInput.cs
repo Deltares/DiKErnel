46cd9be0a7346f54d2341d70b96c052c0a0bd70c
Dennis Tang <Dennis.Tang@deltares.nl>
2024-01-24 14:22:31 +0100

    DIKERNEL-384: Added initial setup for the calculation input

---
+++ Src/DiKErnel.Integration/Data/GrassWaveRunup/GrassWaveRunupBattjesGroenendijkAnalyticalLocationDependentInput.cs
@@ -0,0 +1,193 @@
+// Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
+// 
+// This file is part of DiKErnel.
+// 
+// DiKErnel is free software: you can redistribute it and/or modify it under the terms of the
+// GNU Lesser General Public License as published by the Free Software Foundation, either
+// version 3 of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License along with this
+// program. If not, see <http://www.gnu.org/licenses/>.
+// 
+// All names, logos, and references to "Deltares" are registered trademarks of Stichting
+// Deltares and remain full property of Stichting Deltares at all times. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using DiKErnel.Core.Data;
+using DiKErnel.DomainLibrary.Defaults.GrassWaveRunup;
+using DiKErnel.DomainLibrary.Validators;
+using DiKErnel.DomainLibrary.Validators.Grass;
+using DiKErnel.DomainLibrary.Validators.GrassWaveRunup;
+using DiKErnel.FunctionLibrary;
+using DiKErnel.FunctionLibrary.Grass;
+using DiKErnel.FunctionLibrary.GrassWaveRunup;
+using DiKErnel.Integration.Helpers;
+using DiKErnel.Util.Validation;
+using MathNet.Numerics;
+
+namespace DiKErnel.Integration.Data.GrassWaveRunup
+{
+    internal class GrassWaveRunupBattjesGroenendijkAnalyticalLocationDependentInput : LocationDependentInput
+    {
+        private readonly List<double> xValuesProfile = new List<double>();
+        private readonly List<double> zValuesProfile = new List<double>();
+        private readonly List<double> roughnessCoefficients = new List<double>();
+
+        private double verticalDistanceWaterLevelElevation = double.NaN;
+        private double averageNumberOfWaves = double.NaN;
+        private double representativeWaveRunup2P = double.NaN;
+        private double cumulativeOverload = double.NaN;
+
+        public GrassWaveRunupBattjesGroenendijkAnalyticalLocationDependentInput(double x, double initialDamage,
+                                                                                double failureNumber,
+                                                                                double criticalCumulativeOverload,
+                                                                                double frontVelocityCu,
+                                                                                double criticalFrontVelocity,
+                                                                                double increasedLoadTransitionAlphaM,
+                                                                                double reducedStrengthTransitionAlphaS,
+                                                                                double averageNumberOfWavesCtm)
+            : base(x, initialDamage, failureNumber)
+        {
+            CriticalCumulativeOverload = criticalCumulativeOverload;
+            FrontVelocityCu = frontVelocityCu;
+            CriticalFrontVelocity = criticalFrontVelocity;
+            IncreasedLoadTransitionAlphaM = increasedLoadTransitionAlphaM;
+            ReducedStrengthTransitionAlphaS = reducedStrengthTransitionAlphaS;
+            AverageNumberOfWavesCtm = averageNumberOfWavesCtm;
+        }
+
+        public double CriticalCumulativeOverload { get; }
+        public double FrontVelocityCu { get; }
+        public double CriticalFrontVelocity { get; }
+
+        public double IncreasedLoadTransitionAlphaM { get; }
+
+        public double ReducedStrengthTransitionAlphaS { get; }
+
+        public double AverageNumberOfWavesCtm { get; }
+
+        public override bool Validate(IReadOnlyList<ITimeDependentInput> timeDependentInputItems,
+                                      IProfileData profileData)
+        {
+            bool baseValidationSuccessful = base.Validate(timeDependentInputItems, profileData);
+
+            var validationIssues = new List<ValidationIssue>
+            {
+                GrassRayleighDiscreteValidator.CriticalCumulativeOverload(CriticalCumulativeOverload),
+                GrassWaveRunupRayleighDiscreteValidator.FrontVelocityCu(FrontVelocityCu),
+                GrassRayleighDiscreteValidator.CriticalFrontVelocity(CriticalFrontVelocity),
+                GrassRayleighDiscreteValidator.IncreasedLoadTransitionAlphaM(IncreasedLoadTransitionAlphaM),
+                GrassRayleighDiscreteValidator.ReducedStrengthTransitionAlphaS(ReducedStrengthTransitionAlphaS),
+                RevetmentValidator.AverageNumberOfWavesCtm(AverageNumberOfWavesCtm)
+            };
+
+            return ValidationHelper.RegisterValidationIssues(validationIssues) && baseValidationSuccessful;
+        }
+
+        public override LocationDependentOutput GetLocationDependentOutput(
+            IReadOnlyList<TimeDependentOutput> timeDependentOutputItems)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void InitializeDerivedLocationDependentInput(IProfileData profileData)
+        {
+            base.InitializeDerivedLocationDependentInput(profileData);
+
+            InitializeCalculationProfile(profileData);
+        }
+
+        protected override TimeDependentOutput CalculateTimeDependentOutput(double initialDamage,
+                                                                            ITimeDependentInput timeDependentInput,
+                                                                            IProfileData profileData)
+        {
+            var incrementDamage = 0.0;
+            double damage = initialDamage;
+            double? timeOfFailure = null;
+
+            verticalDistanceWaterLevelElevation = HydraulicLoadFunctions.VerticalDistanceWaterLevelElevation(
+                Z, timeDependentInput.WaterLevel);
+
+            if (verticalDistanceWaterLevelElevation >= 0.0)
+            {
+                double incrementTime = RevetmentFunctions.IncrementTime(timeDependentInput.BeginTime,
+                                                                        timeDependentInput.EndTime);
+
+                averageNumberOfWaves = RevetmentFunctions.AverageNumberOfWaves(incrementTime,
+                                                                               timeDependentInput.WavePeriodTm10,
+                                                                               AverageNumberOfWavesCtm);
+
+                representativeWaveRunup2P = CalculateRepresentativeWaveRunup2P(timeDependentInput, profileData);
+
+                cumulativeOverload = CalculateCumulativeOverload(timeDependentInput, profileData);
+
+                incrementDamage = GrassRevetmentFunctions.IncrementDamage(cumulativeOverload,
+                                                                          CriticalCumulativeOverload);
+
+                damage = RevetmentFunctions.Damage(incrementDamage, initialDamage);
+
+                if (RevetmentFunctions.FailureRevetment(damage, initialDamage, FailureNumber))
+                {
+                    double durationInTimeStepFailure = RevetmentFunctions.DurationInTimeStepFailure(
+                        incrementTime, incrementDamage, FailureNumber, initialDamage);
+
+                    timeOfFailure = RevetmentFunctions.TimeOfFailure(durationInTimeStepFailure,
+                                                                     timeDependentInput.BeginTime);
+                }
+            }
+
+            return null;
+        }
+
+        private double CalculateCumulativeOverload(ITimeDependentInput timeDependentInput, IProfileData profileData)
+        {
+            // Get bottom foreshore z and slope foreshore from the IProfileData
+            double bottomForeshoreZ = double.NaN;
+            double slopeForeshore = double.NaN;
+
+            return GrassRevetmentWaveRunupBattjesGroenendijkAnalyticalFunctions.CumulativeOverload(
+                new GrassRevetmentWaveRunupBattjesGroenendijkAnalyticalCumulativeOverloadInput(averageNumberOfWaves,
+                    representativeWaveRunup2P, timeDependentInput.WaterLevel, timeDependentInput.WaveHeightHm0,
+                    verticalDistanceWaterLevelElevation, bottomForeshoreZ, slopeForeshore, CriticalFrontVelocity,
+                    FrontVelocityCu, IncreasedLoadTransitionAlphaM, ReducedStrengthTransitionAlphaS,
+                    Constants.GravitationalConstant, GrassWaveRunupBattjesGroenendijkAnalyticalDefaults.K1,
+                    GrassWaveRunupBattjesGroenendijkAnalyticalDefaults.K2));
+        }
+
+        private void InitializeCalculationProfile(IProfileData profileData)
+        {
+            (double, double) outerToe = CharacteristicPointsHelper.GetCoordinatesForType(
+                profileData.CharacteristicPoints, CharacteristicPointType.OuterToe);
+            (double, double) outerCrest = CharacteristicPointsHelper.GetCoordinatesForType(
+                profileData.CharacteristicPoints, CharacteristicPointType.OuterCrest);
+
+            foreach (ProfileSegment profileSegment in profileData.ProfileSegments)
+            {
+                if (profileSegment.StartPoint.X >= outerToe.Item1 && profileSegment.StartPoint.X < outerCrest.Item1)
+                {
+                    xValuesProfile.Add(profileSegment.StartPoint.X);
+                    zValuesProfile.Add(profileSegment.StartPoint.Z);
+                    roughnessCoefficients.Add(profileSegment.RoughnessCoefficient);
+                }
+            }
+
+            xValuesProfile.Add(outerCrest.Item1);
+            zValuesProfile.Add(outerCrest.Item2);
+        }
+
+        private double CalculateRepresentativeWaveRunup2P(ITimeDependentInput timeDependentInput,
+                                                          IProfileData profileData)
+        {
+            return GrassRevetmentFunctions.RepresentativeWaveRunup2P(
+                new GrassRevetmentRepresentative2PInput(timeDependentInput.WaterLevel, timeDependentInput.WaveHeightHm0,
+                                                        timeDependentInput.WavePeriodTm10, timeDependentInput.WaveDirection,
+                                                        xValuesProfile, zValuesProfile, roughnessCoefficients,
+                                                        Z, profileData.DikeOrientation));
+        }
+    }
+}
