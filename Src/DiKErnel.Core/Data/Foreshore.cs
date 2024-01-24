2d1ffe83d2c3918efac1cc2f3f4687bbde147f47
Dennis Tang <Dennis.Tang@deltares.nl>
2024-01-24 16:35:30 +0100

    DIKERNEL-384: Added data object to contain foreshore data

---
+++ Src/DiKErnel.Core/Data/Foreshore.cs
@@ -0,0 +1,47 @@
+// Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
+// 
+// This file is part of DiKErnel.
+// 
+// DiKErnel is free software: you can redistribute it and/or modify it under the terms of the
+// GNU Lesser General Public License as published by the Free Software Foundation, either
+// version 3 of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License along with this
+// program. If not, see <http://www.gnu.org/licenses/>.
+// 
+// All names, logos, and references to "Deltares" are registered trademarks of Stichting
+// Deltares and remain full property of Stichting Deltares at all times. All rights reserved.
+
+namespace DiKErnel.Core.Data
+{
+    /// <summary>
+    /// Class containing a foreshore that is needed to perform a calculation.
+    /// </summary>
+    public class Foreshore
+    {
+        /// <summary>
+        /// Creates a new instance.
+        /// </summary>
+        /// <param name="slope">The slope.</param>
+        /// <param name="bottomZ">The z coordinate at the bottom.</param>
+        public Foreshore(double slope, double bottomZ)
+        {
+            Slope = slope;
+            BottomZ = bottomZ;
+        }
+
+        /// <summary>
+        /// Gets the slope of the foreshore.
+        /// </summary>
+        public double Slope { get; }
+
+        /// <summary>
+        /// Gets the z coordinate at the bottom of the foreshore.
+        /// </summary>
+        public double BottomZ { get; }
+    }
+}
---
+++ Test/DiKErnel.Core.Test/Data/ForeshoreTest.cs
@@ -0,0 +1,43 @@
+// Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
+// 
+// This file is part of DiKErnel.
+// 
+// DiKErnel is free software: you can redistribute it and/or modify it under the terms of the
+// GNU Lesser General Public License as published by the Free Software Foundation, either
+// version 3 of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License along with this
+// program. If not, see <http://www.gnu.org/licenses/>.
+// 
+// All names, logos, and references to "Deltares" are registered trademarks of Stichting
+// Deltares and remain full property of Stichting Deltares at all times. All rights reserved.
+
+using DiKErnel.Core.Data;
+using DiKErnel.TestUtil;
+using NUnit.Framework;
+
+namespace DiKErnel.Core.Test.Data
+{
+    [TestFixture]
+    public class ForeshoreTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            double slope = Random.NextDouble();
+            double bottomZ = Random.NextDouble();
+
+            // Call
+            var foreshore = new Foreshore(slope, bottomZ);
+
+            // Assert
+            Assert.That(foreshore.Slope, Is.EqualTo(slope));
+            Assert.That(foreshore.BottomZ, Is.EqualTo(bottomZ));
+        }
+    }
+}
