d3929700d4cef7e95de8775a11f4f6e2271ecafa
Elmar Kleijn <Elmar.Kleijn@deltares.nl>
2024-02-01 14:24:21 +0100

    Final cleanups. DIKERNEL-423

---
+++ Src/DiKErnel.FunctionLibrary/GrassWaveRunup/GrassWaveRunupCumulativeOverloadInput.cs
@@ -0,0 +1,50 @@
+// Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
+// 
+// This file is part of DiKErnel.
+// 
+// DiKErnel is free software: you can redistribute it and/or modify it under the terms of the
+// GNU Lesser General Public License as published by the Free Software Foundation, either
+// version 3 of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License along with this
+// program. If not, see <http://www.gnu.org/licenses/>.
+// 
+// All names, logos, and references to "Deltares" are registered trademarks of Stichting
+// Deltares and remain full property of Stichting Deltares at all times. All rights reserved.
+
+using DiKErnel.FunctionLibrary.Grass;
+
+namespace DiKErnel.FunctionLibrary.GrassWaveRunup
+{
+    /// <summary>
+    /// Input for grass wave run-up cumulative overload calculations.
+    /// </summary>
+    public class GrassWaveRunupCumulativeOverloadInput : GrassCumulativeOverloadInput
+    {
+        /// <inheritdoc/>
+        /// <param name="frontVelocityCu">The Cu coefficient [-].</param>
+        public GrassWaveRunupCumulativeOverloadInput(double averageNumberOfWaves,
+                                                     double representativeWaveRunup2P,
+                                                     double verticalDistanceWaterLevelElevation,
+                                                     double criticalFrontVelocity,
+                                                     double increasedLoadTransitionAlphaM,
+                                                     double reducedStrengthTransitionAlphaS,
+                                                     double gravitationalAcceleration,
+                                                     double frontVelocityCu)
+            : base(averageNumberOfWaves, representativeWaveRunup2P, verticalDistanceWaterLevelElevation,
+                   criticalFrontVelocity, increasedLoadTransitionAlphaM, reducedStrengthTransitionAlphaS,
+                   gravitationalAcceleration)
+        {
+            FrontVelocityCu = frontVelocityCu;
+        }
+
+        /// <summary>
+        /// Gets the Cu coefficient [-].
+        /// </summary>
+        public double FrontVelocityCu { get; }
+    }
+}
---
+++ Test/DiKErnel.FunctionLibrary.Test/GrassWaveRunup/GrassWaveRunupCumulativeOverloadInputTest.cs
@@ -0,0 +1,60 @@
+// Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
+// 
+// This file is part of DiKErnel.
+// 
+// DiKErnel is free software: you can redistribute it and/or modify it under the terms of the
+// GNU Lesser General Public License as published by the Free Software Foundation, either
+// version 3 of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License along with this
+// program. If not, see <http://www.gnu.org/licenses/>.
+// 
+// All names, logos, and references to "Deltares" are registered trademarks of Stichting
+// Deltares and remain full property of Stichting Deltares at all times. All rights reserved.
+
+using DiKErnel.FunctionLibrary.Grass;
+using DiKErnel.FunctionLibrary.GrassWaveRunup;
+using DiKErnel.TestUtil;
+using NUnit.Framework;
+
+namespace DiKErnel.FunctionLibrary.Test.GrassWaveRunup
+{
+    [TestFixture]
+    public class GrassWaveRunupCumulativeOverloadInputTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Setup
+            double averageNumberOfWaves = Random.NextDouble();
+            double representativeWaveRunup2P = Random.NextDouble();
+            double verticalDistanceWaterLevelElevation = Random.NextDouble();
+            double criticalFrontVelocity = Random.NextDouble();
+            double increasedLoadTransitionAlphaM = Random.NextDouble();
+            double reducedStrengthTransitionAlphaS = Random.NextDouble();
+            double gravitationalAcceleration = Random.NextDouble();
+            double frontVelocityCu = Random.NextDouble();
+
+            // Call
+            var input = new GrassWaveRunupCumulativeOverloadInput(
+                averageNumberOfWaves, representativeWaveRunup2P, verticalDistanceWaterLevelElevation,
+                criticalFrontVelocity, increasedLoadTransitionAlphaM, reducedStrengthTransitionAlphaS,
+                gravitationalAcceleration, frontVelocityCu);
+
+            // Assert
+            Assert.That(input, Is.InstanceOf<GrassCumulativeOverloadInput>());
+            Assert.That(input.AverageNumberOfWaves, Is.EqualTo(averageNumberOfWaves));
+            Assert.That(input.RepresentativeWaveRunup2P, Is.EqualTo(representativeWaveRunup2P));
+            Assert.That(input.VerticalDistanceWaterLevelElevation, Is.EqualTo(verticalDistanceWaterLevelElevation));
+            Assert.That(input.CriticalFrontVelocity, Is.EqualTo(criticalFrontVelocity));
+            Assert.That(input.IncreasedLoadTransitionAlphaM, Is.EqualTo(increasedLoadTransitionAlphaM));
+            Assert.That(input.ReducedStrengthTransitionAlphaS, Is.EqualTo(reducedStrengthTransitionAlphaS));
+            Assert.That(input.GravitationalAcceleration, Is.EqualTo(gravitationalAcceleration));
+            Assert.That(input.FrontVelocityCu, Is.EqualTo(frontVelocityCu));
+        }
+    }
+}
