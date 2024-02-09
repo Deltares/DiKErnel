de7ea9664c981288ae72ab9d58ad2104f8ad4ffd
Elmar Kleijn <Elmar.Kleijn@deltares.nl>
2024-02-09 15:44:40 +0100

    Introducing GrassWaveOvertoppingRayleighAnalyticalLocationDependentInput. DIKERNEL-432

---
+++ Src/DiKErnel.Integration/Data/GrassWaveOvertopping/GrassWaveOvertoppingRayleighAnalyticalLocationDependentInput.cs
@@ -0,0 +1,55 @@
+// Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// This program is free software; you can redistribute it and/or modify it under the terms of
+// the GNU Lesser General Public License as published by the Free Software Foundation; either
+// version 3 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with this
+// program; if not, see <https://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of Stichting
+// Deltares and remain full property of Stichting Deltares at all times. All rights reserved.
+
+using DiKErnel.Core.Data;
+using DiKErnel.DomainLibrary.Constants;
+using DiKErnel.FunctionLibrary.GrassWaveOvertopping;
+
+namespace DiKErnel.Integration.Data.GrassWaveOvertopping
+{
+    internal class GrassWaveOvertoppingRayleighAnalyticalLocationDependentInput
+        : GrassWaveOvertoppingRayleighLocationDependentInput
+    {
+        public GrassWaveOvertoppingRayleighAnalyticalLocationDependentInput(
+            double x, double initialDamage, double failureNumber, double criticalCumulativeOverload,
+            double criticalFrontVelocity, double increasedLoadTransitionAlphaM, double reducedStrengthTransitionAlphaS,
+            double averageNumberOfWavesCtm, double frontVelocityCwo,
+            GrassWaveOvertoppingRayleighAccelerationAlphaA accelerationAlphaA, double? enforcedDikeHeight)
+            : base(x, initialDamage, failureNumber, criticalCumulativeOverload, criticalFrontVelocity,
+                   increasedLoadTransitionAlphaM, reducedStrengthTransitionAlphaS, averageNumberOfWavesCtm,
+                   frontVelocityCwo, accelerationAlphaA, enforcedDikeHeight) {}
+
+        protected override double CalculateCumulativeOverload(double averageNumberOfWaves,
+                                                              double representativeWaveRunup2P,
+                                                              double verticalDistanceWaterLevelElevation,
+                                                              ITimeDependentInput timeDependentInput,
+                                                              IProfileData profileData)
+        {
+            return GrassWaveOvertoppingRayleighAnalyticalFunctions.CumulativeOverload(
+                new GrassWaveOvertoppingRayleighCumulativeOverloadInput(averageNumberOfWaves,
+                                                                        representativeWaveRunup2P,
+                                                                        verticalDistanceWaterLevelElevation,
+                                                                        CriticalFrontVelocity,
+                                                                        IncreasedLoadTransitionAlphaM,
+                                                                        ReducedStrengthTransitionAlphaS,
+                                                                        NaturalConstants.GravitationalAcceleration,
+                                                                        FrontVelocityCwo,
+                                                                        AccelerationAlphaAValue));
+        }
+    }
+}
