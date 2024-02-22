c8f1fe049cc180028a1aa53bed44cfe2ce000f10
Dennis Tang <dennis.tang@deltares.nl>
2024-02-22 11:57:01 +0100

    DIKERNEL-431: Introduced cumulative overload definitions:
    - Wired step definitions for Rayleigh analytical definitions

--- Test/DiKErnel.SpecFlow.Test/Steps/GrassWaveOvertoppingRayleighAnalyticalStepDefinitions.cs
+++ Test/DiKErnel.SpecFlow.Test/Steps/GrassWaveOvertoppingRayleighAnalyticalStepDefinitions.cs
@@ -1,5 +1,6 @@
 using DiKErnel.Integration;
 using DiKErnel.Integration.Data.GrassWaveOvertopping;
+using DiKErnel.SpecFlow.Test.Steps.Definitions;
 using TechTalk.SpecFlow;
 
 namespace DiKErnel.SpecFlow.Test.Steps
@@ -7,17 +8,6 @@ namespace DiKErnel.SpecFlow.Test.Steps
     [Binding]
     public class GrassWaveOvertoppingRayleighAnalyticalStepDefinitions : StepDefinitionsBase
     {
-        private const string calculationPositionKey = "Position";
-        private const string topLayerTypeKey = "Top layer type";
-        private const string damageKey = "Failure number";
-        private const string initialDamageKey = "Initial damage";
-        private const string averageNumberOfWavesKey = "Average number of waves";
-        private const string criticalFrontVelocityKey = "Critical front velocity";
-        private const string frontVelocityKey = "Front velocity";
-        private const string criticalCumulativeOverloadKey = "Critical cumulative overload";
-        private const string increasedLoadTransitionKey = "Increased load transition";
-        private const string reducedStrengthTransitionKey = "Reduced strength transition";
-
         public GrassWaveOvertoppingRayleighAnalyticalStepDefinitions(ScenarioContext context) : base(context) {}
 
         [When(@"I run the grass wave overtopping Rayleigh analytical calculation")]
@@ -29,16 +19,16 @@ namespace DiKErnel.SpecFlow.Test.Steps
         protected override void AddLocation(CalculationInputBuilder builder)
         {
             var constructionProperties = new GrassWaveOvertoppingRayleighLocationConstructionProperties(
-                GetDouble(calculationPositionKey), GetGrassTopLayerType(topLayerTypeKey))
+                GetDouble(GeneralDefinitions.CalculationPositionKey), GetGrassTopLayerType(GeneralDefinitions.TopLayerTypeKey))
             {
-                FailureNumber = GetNullableDouble(damageKey),
-                InitialDamage = GetNullableDouble(initialDamageKey),
-                AverageNumberOfWavesCtm = GetNullableDouble(averageNumberOfWavesKey),
-                CriticalFrontVelocity = GetNullableDouble(criticalFrontVelocityKey),
-                FrontVelocityCwo = GetNullableDouble(frontVelocityKey),
-                CriticalCumulativeOverload = GetNullableDouble(criticalCumulativeOverloadKey),
-                IncreasedLoadTransitionAlphaM = GetNullableDouble(increasedLoadTransitionKey),
-                ReducedStrengthTransitionAlphaS = GetNullableDouble(reducedStrengthTransitionKey),
+                FailureNumber = GetNullableDouble(GeneralDefinitions.DamageKey),
+                InitialDamage = GetNullableDouble(GeneralDefinitions.InitialDamageKey),
+                AverageNumberOfWavesCtm = GetNullableDouble(GrassCumulativeOverloadDefinitions.AverageNumberOfWavesKey),
+                CriticalFrontVelocity = GetNullableDouble(GrassCumulativeOverloadDefinitions.CriticalFrontVelocityKey),
+                FrontVelocityCwo = GetNullableDouble(GrassCumulativeOverloadDefinitions.FrontVelocityKey),
+                CriticalCumulativeOverload = GetNullableDouble(GrassCumulativeOverloadDefinitions.CriticalCumulativeOverloadKey),
+                IncreasedLoadTransitionAlphaM = GetNullableDouble(GrassCumulativeOverloadDefinitions.IncreasedLoadTransitionKey),
+                ReducedStrengthTransitionAlphaS = GetNullableDouble(GrassCumulativeOverloadDefinitions.ReducedStrengthTransitionKey),
                 AccelerationAlphaAForCrest = GetNullableDouble("Acceleration crest"),
                 AccelerationAlphaAForInnerSlope = GetNullableDouble("Acceleration inner slope"),
                 DikeHeight = GetNullableDouble("Dike height")
