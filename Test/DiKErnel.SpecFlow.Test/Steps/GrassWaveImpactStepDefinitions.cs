ea90b12f02f8d90246dbbd35cea4b265d812e469
Elmar Kleijn <Elmar.Kleijn@deltares.nl>
2024-06-11 16:08:41 +0200

    Added definition files for grass wave impact. DIKERNEL-441

---
+++ Test/DiKErnel.SpecFlow.Test/Steps/GrassWaveImpactStepDefinitions.cs
@@ -0,0 +1,48 @@
+// Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
+// 
+// This file is part of DiKErnel.
+// 
+// DiKErnel is free software: you can redistribute it and/or modify it under the terms of the
+// GNU Lesser General Public License as published by the Free Software Foundation, either
+// version 3 of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License along with this
+// program. If not, see <http://www.gnu.org/licenses/>.
+// 
+// All names, logos, and references to "Deltares" are registered trademarks of Stichting
+// Deltares and remain full property of Stichting Deltares at all times. All rights reserved.
+
+using DiKErnel.Integration;
+using DiKErnel.Integration.Data.GrassWaveImpact;
+using DiKErnel.SpecFlow.Test.Steps.Definitions;
+using TechTalk.SpecFlow;
+
+namespace DiKErnel.SpecFlow.Test.Steps
+{
+    public class GrassWaveImpactStepDefinitions : StepDefinitionsBase
+    {
+        public GrassWaveImpactStepDefinitions(ScenarioContext context) : base(context) {}
+
+        [When(@"I run the grass wave impact calculation")]
+        public void WhenIRunTheGrassWaveImpactCalculation()
+        {
+            RunCalculation();
+        }
+
+        protected override void AddLocation(CalculationInputBuilder builder)
+        {
+            var constructionProperties = new GrassWaveImpactLocationConstructionProperties(
+                Context.GetDouble(GeneralDefinitions.CalculationPosition), Context.GetGrassTopLayerType())
+            {
+                FailureNumber = Context.GetNullableDouble(GeneralDefinitions.FailureNumber),
+                InitialDamage = Context.GetNullableDouble(GeneralDefinitions.InitialDamage)
+            };
+
+            builder.AddGrassWaveImpactLocation(constructionProperties);
+        }
+    }
+}
