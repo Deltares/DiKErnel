5b87a0745cd6919c7bcc48a2d11ebafa6315106f
Elmar Kleijn <Elmar.Kleijn@deltares.nl>
2024-06-14 09:03:41 +0200

    Added definition files for natural stone wave impact. DIKERNEL-438

---
+++ Test/DiKErnel.SpecFlow.Test/Steps/Definitions/NaturalStoneWaveImpactDefinitions.cs
@@ -0,0 +1,38 @@
+// Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
+// 
+// This file is part of DiKErnel.
+// 
+// DiKErnel is free software: you can redistribute it and/or modify it under the terms of the
+// GNU Lesser General Public License as published by the Free Software Foundation, either
+// version 3 of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License along with this
+// program. If not, see <http://www.gnu.org/licenses/>.
+// 
+// All names, logos, and references to "Deltares" are registered trademarks of Stichting
+// Deltares and remain full property of Stichting Deltares at all times. All rights reserved.
+
+namespace DiKErnel.SpecFlow.Test.Steps.Definitions
+{
+    internal static class AsphaltWaveImpactDefinitions
+    {
+        public const string FlexuralStrength = "Flexural strength";
+        public const string SoilElasticity = "Soil elasticity";
+        public const string ThicknessUpperLayer = "Thickness upper layer";
+        public const string ElasticModulusUpperLayer = "Elastic modulus upper layer";
+        public const string DensityOfWater = "Density of water";
+        public const string ThicknessSubLayer = "Thickness sub layer";
+        public const string ElasticModulusSubLayer = "Elastic modulus sub layer";
+        public const string FatigueAlpha = "Fatigue alpha";
+        public const string FatigueBeta = "Fatigue beta";
+        public const string ImpactNumberC = "Impact number C";
+        public const string StiffnessRelation = "Stiffness relation";
+        public const string WidthFactors = "Width factors";
+        public const string DepthFactors = "Depth factors";
+        public const string ImpactFactors = "Impact factors";
+    }
+}
