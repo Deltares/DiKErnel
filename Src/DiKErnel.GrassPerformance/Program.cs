1d8b81a8e4f86405adda7064a15d7803535548aa
Elmar Kleijn <Elmar.Kleijn@deltares.nl>
2024-09-20 11:36:01 +0200

    Implementing simple console application for performance benchmarking.

--- Src/DiKErnel.GrassPerformance/Program.cs
+++ Src/DiKErnel.GrassPerformance/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using DiKErnel.Integration;
@@ -11,11 +12,79 @@ namespace DiKErnel.GrassPerformance
 {
     internal static class Program
     {
+        private const double dikeOrientation = 0;
+        private const double xStartCalculationZoneOuterSlope = 5;
+        private const double xEndCalculationZoneOuterSlope = 15;
+        private const double xStartCalculationZoneInnerSlope = 60;
+        private const double xEndCalculationZoneInnerSlope = 70;
+        private const string grassWaveImpactIdentifier = "GrassWaveImpact";
+        private const string grassWaveOvertoppingRayleighAnalyticalIdentifier = "GrassWaveOvertoppingRayleighAnalytical";
+        private const string grassWaveOvertoppingRayleighDiscreteIdentifier = "GrassWaveOvertoppingRayleighDiscrete";
+        private const string grassWaveRunupBattjesGroenendijkAnalyticalIdentifier = "GrassWaveRunupBattjesGroenendijkAnalytical";
+        private const string grassWaveRunupRayleighDiscreteIdentifier = "GrassWaveRunupRayleighDiscrete";
+
         private static void Main(string[] args)
         {
-            var builder = new CalculationInputBuilder(0);
+            var builder = new CalculationInputBuilder(dikeOrientation);
 
-            switch (args[0])
+            Action<double> addGrassLocationAction = args[0] switch
+            {
+                grassWaveImpactIdentifier => x => builder.AddGrassWaveImpactLocation(
+                    new GrassWaveImpactLocationConstructionProperties(x, GrassTopLayerType.OpenSod)),
+                grassWaveOvertoppingRayleighAnalyticalIdentifier => x => builder.AddGrassWaveOvertoppingRayleighAnalyticalLocation(
+                    new GrassWaveOvertoppingRayleighLocationConstructionProperties(x, GrassTopLayerType.OpenSod)),
+                grassWaveOvertoppingRayleighDiscreteIdentifier => x => builder.AddGrassWaveOvertoppingRayleighDiscreteLocation(
+                    new GrassWaveOvertoppingRayleighDiscreteLocationConstructionProperties(x, GrassTopLayerType.OpenSod)),
+                grassWaveRunupBattjesGroenendijkAnalyticalIdentifier => x => builder.AddGrassWaveRunupBattjesGroenendijkAnalyticalLocation(
+                    new GrassWaveRunupBattjesGroenendijkAnalyticalLocationConstructionProperties(x, GrassTopLayerType.OpenSod)),
+                grassWaveRunupRayleighDiscreteIdentifier => x => builder.AddGrassWaveRunupRayleighDiscreteLocation(
+                    new GrassWaveRunupRayleighDiscreteLocationConstructionProperties(x, GrassTopLayerType.OpenSod)),
+                _ => throw new ArgumentException("Invalid calculation type")
+            };
+
+            int numberOfLocations = int.Parse(args[1]);
+
+            var locations = new List<double>();
+
+            double xStartCalculationZone;
+            double xEndCalculationZone;
+
+            if (args[0] == grassWaveOvertoppingRayleighAnalyticalIdentifier || args[0] == grassWaveOvertoppingRayleighDiscreteIdentifier)
+            {
+                xStartCalculationZone = xStartCalculationZoneInnerSlope;
+                xEndCalculationZone = xEndCalculationZoneInnerSlope;
+            }
+            else
+            {
+                xStartCalculationZone = xStartCalculationZoneOuterSlope;
+                xEndCalculationZone = xEndCalculationZoneOuterSlope;
+            }
+
+            switch (numberOfLocations)
+            {
+                case 1:
+                    locations.Add(xStartCalculationZone + (xEndCalculationZone - xStartCalculationZone) * 1 / 2);
+                    break;
+                case 2:
+                    locations.Add(xStartCalculationZone + (xEndCalculationZone - xStartCalculationZone) * 1 / 3);
+                    locations.Add(xStartCalculationZone + (xEndCalculationZone - xStartCalculationZone) * 2 / 3);
+                    break;
+                default:
+                {
+                    double increment = (xEndCalculationZone - xStartCalculationZone) / numberOfLocations;
+
+                    locations.AddRange(Enumerable.Range(0, numberOfLocations)
+                                                 .Select(i => xStartCalculationZone + i * increment));
+                    break;
+                }
+            }
+
+            foreach (double location in locations)
+            {
+                addGrassLocationAction(location);
+            }
+
+            switch (args[2])
             {
                 case "1h":
                     AddTimeSteps(builder, 1, Resources.htime_1h, Resources.Hm0_1h, Resources.Tmm10_1h, Resources.WDir_1h);
@@ -24,23 +93,6 @@ namespace DiKErnel.GrassPerformance
                     AddTimeSteps(builder, 12, Resources.htime_12h, Resources.Hm0_12h, Resources.Tmm10_12h, Resources.WDir_12h);
                     break;
             }
-
-            Action<double> addGrassLocationAction = args[2] switch
-            {
-                "GrassWaveImpact" => x => builder.AddGrassWaveImpactLocation(
-                    new GrassWaveImpactLocationConstructionProperties(x, GrassTopLayerType.OpenSod)),
-                "GrassWaveOvertoppingRayleighAnalytical" => x => builder.AddGrassWaveOvertoppingRayleighAnalyticalLocation(
-                    new GrassWaveOvertoppingRayleighLocationConstructionProperties(x, GrassTopLayerType.OpenSod)),
-                "GrassWaveOvertoppingRayleighDiscrete" => x => builder.AddGrassWaveOvertoppingRayleighDiscreteLocation(
-                    new GrassWaveOvertoppingRayleighDiscreteLocationConstructionProperties(x, GrassTopLayerType.OpenSod)),
-                "GrassWaveRunupBattjesGroenendijkAnalytical" => x => builder.AddGrassWaveRunupBattjesGroenendijkAnalyticalLocation(
-                    new GrassWaveRunupBattjesGroenendijkAnalyticalLocationConstructionProperties(x, GrassTopLayerType.OpenSod)),
-                "GrassWaveRunupRayleighDiscrete" => x => builder.AddGrassWaveRunupRayleighDiscreteLocation(
-                    new GrassWaveRunupRayleighDiscreteLocationConstructionProperties(x, GrassTopLayerType.OpenSod)),
-                _ => throw new ArgumentException("Invalid calculation type")
-            };
-
-            addGrassLocationAction(5);
         }
 
         private static void AddTimeSteps(CalculationInputBuilder builder, int hours, string commaSeparatedWaterLevels,
