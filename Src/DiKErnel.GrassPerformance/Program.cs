e1bd353f0c52fddd7a2b7371019421f22686f15e
Elmar Kleijn <Elmar.Kleijn@deltares.nl>
2024-09-20 11:03:07 +0200

    Implementing simple console application for performance benchmarking.

--- Src/DiKErnel.GrassPerformance/Program.cs
+++ Src/DiKErnel.GrassPerformance/Program.cs
@@ -1,80 +1,81 @@
 ﻿using System;
 using System.Globalization;
 using System.Linq;
-using DiKErnel.GrassPerformance;
 using DiKErnel.Integration;
+using DiKErnel.Integration.Data.Grass;
+using DiKErnel.Integration.Data.GrassWaveImpact;
+using DiKErnel.Integration.Data.GrassWaveOvertopping;
+using DiKErnel.Integration.Data.GrassWaveRunup;
 
-internal class Program
+namespace DiKErnel.GrassPerformance
 {
-    private static void Main(string[] args)
+    internal static class Program
     {
-        var builder = new CalculationInputBuilder(0);
-
-        switch (args[0])
+        private static void Main(string[] args)
         {
-            case "1h":
-                AddTimeSteps(builder, 1, Resources.htime_1h, Resources.Hm0_1h, Resources.Tmm10_1h, Resources.WDir_1h);
-                break;
-            case "12h":
-                AddTimeSteps(builder, 12, Resources.htime_12h, Resources.Hm0_12h, Resources.Tmm10_12h, Resources.WDir_12h);
-                break;
+            var builder = new CalculationInputBuilder(0);
+
+            switch (args[0])
+            {
+                case "1h":
+                    AddTimeSteps(builder, 1, Resources.htime_1h, Resources.Hm0_1h, Resources.Tmm10_1h, Resources.WDir_1h);
+                    break;
+                case "12h":
+                    AddTimeSteps(builder, 12, Resources.htime_12h, Resources.Hm0_12h, Resources.Tmm10_12h, Resources.WDir_12h);
+                    break;
+            }
+
+            Action<double> addGrassLocationAction = args[2] switch
+            {
+                "GrassWaveImpact" => x => builder.AddGrassWaveImpactLocation(
+                    new GrassWaveImpactLocationConstructionProperties(x, GrassTopLayerType.OpenSod)),
+                "GrassWaveOvertoppingRayleighAnalytical" => x => builder.AddGrassWaveOvertoppingRayleighAnalyticalLocation(
+                    new GrassWaveOvertoppingRayleighLocationConstructionProperties(x, GrassTopLayerType.OpenSod)),
+                "GrassWaveOvertoppingRayleighDiscrete" => x => builder.AddGrassWaveOvertoppingRayleighDiscreteLocation(
+                    new GrassWaveOvertoppingRayleighDiscreteLocationConstructionProperties(x, GrassTopLayerType.OpenSod)),
+                "GrassWaveRunupBattjesGroenendijkAnalytical" => x => builder.AddGrassWaveRunupBattjesGroenendijkAnalyticalLocation(
+                    new GrassWaveRunupBattjesGroenendijkAnalyticalLocationConstructionProperties(x, GrassTopLayerType.OpenSod)),
+                "GrassWaveRunupRayleighDiscrete" => x => builder.AddGrassWaveRunupRayleighDiscreteLocation(
+                    new GrassWaveRunupRayleighDiscreteLocationConstructionProperties(x, GrassTopLayerType.OpenSod)),
+                _ => throw new ArgumentException("Invalid calculation type")
+            };
+
+            addGrassLocationAction(5);
         }
 
-        switch (args[2])
+        private static void AddTimeSteps(CalculationInputBuilder builder, int hours, string commaSeparatedWaterLevels,
+                                         string commaSeparatedWaveHeights, string commaSeparatedWavePeriods,
+                                         string commaSeparatedWaveDirections)
         {
-            case "GrassWaveImpact":
-                // Todo: implement
-                break;
-            case "GrassWaveOvertoppingRayleighAnalytical":
-                // Todo: implement
-                break;
-            case "GrassWaveOvertoppingRayleighDiscrete":
-                // Todo: implement
-                break;
-            case "GrassWaveRunupBattjesGroenendijkAnalytical":
-                // Todo: implement
-                break;
-            case "GrassWaveRunupRayleighDiscrete":
-                // Todo: implement
-                break;
-            case "GrassWaveRunupRayleighAnalytical":
-                // Todo: implement
-                break;
+            double[] waterLevels = commaSeparatedWaterLevels.Split(',')
+                                                            .Select(s => double.Parse(s, CultureInfo.InvariantCulture))
+                                                            .ToArray();
+
+            double[] waveHeights = commaSeparatedWaveHeights.Split(',')
+                                                            .Select(s => double.Parse(s, CultureInfo.InvariantCulture))
+                                                            .Select(d => d <= 0 ? 0.01 : d)
+                                                            .ToArray();
+
+            double[] wavePeriods = commaSeparatedWavePeriods.Split(',')
+                                                            .Select(s => double.Parse(s, CultureInfo.InvariantCulture))
+                                                            .Select(d => d <= 0 ? 0.01 : d)
+                                                            .ToArray();
+
+            double[] waveDirections = commaSeparatedWaveDirections.Split(',')
+                                                                  .Select(s => double.Parse(s, CultureInfo.InvariantCulture))
+                                                                  .ToArray();
+
+            double[] times = Enumerable.Range(0, waterLevels.Length + 1)
+                                       .Select(i => hours * 3600d * i)
+                                       .ToArray();
+
+            for (var i = 0; i <= waterLevels.Length - 1; i++)
+            {
+                builder.AddTimeStep(times[i], times[i + 1], waterLevels[i], waveHeights[i], wavePeriods[i], waveDirections[i]);
+            }
+
+            Console.WriteLine($"Duration of single time step: {hours * 3600} seconds");
+            Console.WriteLine($"Number of time steps: {waterLevels.Length}");
         }
-    }
-
-    private static void AddTimeSteps(CalculationInputBuilder builder, int hours, string commaSeparatedWaterLevels,
-                                     string commaSeparatedWaveHeights, string commaSeparatedWavePeriods,
-                                     string commaSeparatedWaveDirections)
-    {
-        double[] waterLevels = commaSeparatedWaterLevels.Split(',')
-                                                        .Select(s => double.Parse(s, CultureInfo.InvariantCulture))
-                                                        .ToArray();
-
-        double[] waveHeights = commaSeparatedWaveHeights.Split(',')
-                                                        .Select(s => double.Parse(s, CultureInfo.InvariantCulture))
-                                                        .Select(d => d <= 0 ? 0.01 : d)
-                                                        .ToArray();
-
-        double[] wavePeriods = commaSeparatedWavePeriods.Split(',')
-                                                        .Select(s => double.Parse(s, CultureInfo.InvariantCulture))
-                                                        .Select(d => d <= 0 ? 0.01 : d)
-                                                        .ToArray();
-
-        double[] waveDirections = commaSeparatedWaveDirections.Split(',')
-                                                              .Select(s => double.Parse(s, CultureInfo.InvariantCulture))
-                                                              .ToArray();
-
-        double[] times = Enumerable.Range(0, waterLevels.Length + 1)
-                                   .Select(i => hours * 3600d * i)
-                                   .ToArray();
-
-        for (var i = 0; i <= waterLevels.Length - 1; i++)
-        {
-            builder.AddTimeStep(times[i], times[i + 1], waterLevels[i], waveHeights[i], wavePeriods[i], waveDirections[i]);
-        }
-
-        Console.WriteLine($"Duration of single time step: {hours * 3600} seconds");
-        Console.WriteLine($"Number of time steps: {waterLevels.Length}");
     }
 }
