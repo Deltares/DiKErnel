7dd47e7fbf4b488d087ab7d6149141a49e7e4544
Elmar Kleijn <Elmar.Kleijn@deltares.nl>
2024-09-24 08:28:56 +0200

    Make calculations per time step parallelizable.

--- Test/DiKErnel.Core.Test/CalculatorTest.cs
+++ Test/DiKErnel.Core.Test/CalculatorTest.cs
@@ -110,12 +110,15 @@ namespace DiKErnel.Core.Test
 
             public double FailureNumber => 1;
 
+            public bool RequiresDamageAtStartOfCalculation => false;
+
             public bool Validate(IReadOnlyList<ITimeDependentInput> timeDependentInputItems, IProfileData profileData)
             {
                 return true;
             }
 
-            public TimeDependentOutput Calculate(ITimeDependentInput timeDependentInput, IProfileData profileData)
+            public TimeDependentOutput Calculate(ITimeDependentInput timeDependentInput, IProfileData profileData,
+                                                 double damageAtStartOfCalculation = double.NaN)
             {
                 if (!string.IsNullOrEmpty(ExceptionMessage))
                 {
