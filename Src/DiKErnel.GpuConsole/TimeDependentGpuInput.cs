b9a07b22fb523ea999b8cfb9c44a5a3bed93ce39
Elmar Kleijn <Elmar.Kleijn@deltares.nl>
2024-10-30 11:25:33 +0100

    Improving performance by using GPU.

---
+++ Src/DiKErnel.GpuConsole/TimeDependentGpuInput.cs
@@ -0,0 +1,46 @@
+// Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// This program is free software; you can redistribute it and/or modify it under the terms of
+// the GNU Lesser General Public License as published by the Free Software Foundation; either
+// version 3 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with this
+// program; if not, see <https://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of Stichting
+// Deltares and remain full property of Stichting Deltares at all times. All rights reserved.
+
+namespace DiKErnel.GpuConsole
+{
+    internal struct TimeDependentGpuInput
+    {
+        public TimeDependentGpuInput(double beginTime, double endTime, double waterLevel, double waveHeightHm0,
+                                     double wavePeriodTm10, double waveDirection)
+        {
+            BeginTime = beginTime;
+            EndTime = endTime;
+            WaterLevel = waterLevel;
+            WaveHeightHm0 = waveHeightHm0;
+            WavePeriodTm10 = wavePeriodTm10;
+            WaveDirection = waveDirection;
+        }
+
+        public double BeginTime { get; }
+
+        public double EndTime { get; }
+
+        public double WaterLevel { get; }
+
+        public double WaveHeightHm0 { get; }
+
+        public double WavePeriodTm10 { get; }
+
+        public double WaveDirection { get; }
+    }
+}
