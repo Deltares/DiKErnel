2642c775e242fbbc3409fe22bcf9f573b9be7ec0
Elmar Kleijn <elmar.kleijn@deltares.nl>
2024-12-24 14:50:17 +0100

    Improving performance by using float.

---
+++ Src/DiKErnel.FunctionLibrary/CMathF.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (C) Stichting Deltares and State of the Netherlands 2023. All rights reserved.
+//
+// This file is part of DiKErnel.
+//
+// This program is free software; you can redistribute it and/or modify it under the terms of
+// the GNU Lesser General Public License as published by the Free Software Foundation; either
+// version 3 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with this
+// program; if not, see <https://www.gnu.org/licenses/>.
+//
+// All names, logos, and references to "Deltares" are registered trademarks of Stichting
+// Deltares and remain full property of Stichting Deltares at all times. All rights reserved.
+
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace DiKErnel.FunctionLibrary
+{
+    internal static partial class CMathF
+    {
+        [LibraryImport("DiKErnel.Optimization.dll", EntryPoint = "PowF")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvStdcall)])]
+        internal static partial float Pow(float x, float y);
+
+        [LibraryImport("DiKErnel.Optimization.dll", EntryPoint = "OptimizedImpactFactorAccumulationF")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvStdcall)])]
+        internal static partial float OptimizedImpactFactorAccumulation(float fatigueAlpha, float fatigueBeta,
+                                                                        float averageNumberOfWaves, float logFailureTension,
+                                                                        float bendingStress, float[] impactFactorProbabilities,
+                                                                        int impactFactorProbabilitiesLength, float[] impactNumberLookup,
+                                                                        int impactNumberLookupLength);
+    }
+}
