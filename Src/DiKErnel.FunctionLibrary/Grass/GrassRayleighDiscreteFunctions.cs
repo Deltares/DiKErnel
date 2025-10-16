70e08869f44ba37db06e6712344cfaef7fb51b44
Elmar Kleijn <elmar.kleijn@deltares.nl>
2025-10-16 23:56:26 +0200

    Revert "Performing calculations only once when possible. DIKERNEL-463"
    This reverts commit fcc93d62bcae71100c54bc9934dd8e184f5c329d.

--- Src/DiKErnel.FunctionLibrary/Grass/GrassRayleighDiscreteFunctions.cs
+++ Src/DiKErnel.FunctionLibrary/Grass/GrassRayleighDiscreteFunctions.cs
@@ -33,14 +33,14 @@ namespace DiKErnel.FunctionLibrary.Grass
         {
             double cumulativeFrontVelocity = 0;
 
-            double cumulativeFrontVelocityPartial = input.CriticalFrontVelocity * input.CriticalFrontVelocity;
+            double cumulativeFrontVelocityPartial = Math.Pow(input.CriticalFrontVelocity, 2);
 
             for (var k = 1; k <= fixedNumberOfWaves; k++)
             {
                 double waveRunup = WaveRunup(input.RepresentativeWaveRunup2P, fixedNumberOfWaves, k);
                 double frontVelocity = getFrontVelocityFunc(waveRunup);
 
-                cumulativeFrontVelocity += Math.Max(0, input.IncreasedLoadTransitionAlphaM * frontVelocity * frontVelocity
+                cumulativeFrontVelocity += Math.Max(0, input.IncreasedLoadTransitionAlphaM * Math.Pow(frontVelocity, 2)
                                                        - input.ReducedStrengthTransitionAlphaS
                                                        * cumulativeFrontVelocityPartial);
             }
